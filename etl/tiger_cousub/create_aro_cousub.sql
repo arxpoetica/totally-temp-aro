044e181e1ca1cc9297c0d47f93da3eb1616684dc
Michael Maloney <mmaloney@altvil.com>
2015-06-17 21:26:05 -0400

    rudimentary ETL for TIGER COUSUB data [finishes #97046900, #97046856]

---
+++ etl/tiger_cousub/create_aro_cousub.sql
@@ -0,0 +1,25 @@
+-- Table: public.aro_cousub
+
+-- DROP TABLE public.aro_cousub;
+
+CREATE TABLE public.aro_cousub AS
+  SELECT
+    gid,
+    statefp,
+    countyfp,
+    geoid,
+    name,
+    aland,
+    awater,
+    intptlat,
+    intptlon,
+    geom
+  FROM tiger_cousub;
+
+ALTER TABLE public.aro_cousub
+  OWNER TO aro;
+
+CREATE INDEX aro_cousub_geom_gist
+  ON public.aro_cousub
+  USING gist
+  (geom);
