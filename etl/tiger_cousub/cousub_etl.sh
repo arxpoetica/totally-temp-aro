474f8686e55b9863ab0a80999d52a173635357db
Michael Maloney <mmaloney@altvil.com>
2015-06-24 15:20:33 -0400

    Change downloads and geographic location of application map to Seattle, WA, since that's where all of our business data is.

--- etl/tiger_cousub/cousub_etl.sh
+++ etl/tiger_cousub/cousub_etl.sh
@@ -10,14 +10,14 @@ export PGDATABASE=aro
 
 # We should download these to an S3 bucket when we load the rest of the country.
 sudo mkdir shapefiles
-sudo mkdir shapefiles/tl_2014_36_cousub
+sudo mkdir shapefiles/tl_2014_53_cousub
 
-sudo wget -P shapefiles/tl_2014_36_cousub/ "ftp://ftp2.census.gov/geo/tiger/TIGER2014/COUSUB/tl_2014_36_cousub.zip"
-unzip shapefiles/tl_2014_36_cousub/tl_2014_36_cousub.zip -d shapefiles/tl_2014_36_cousub/
-sudo rm shapefiles/tl_2014_36_cousub/tl_2014_36_cousub.zip
+sudo wget -P shapefiles/tl_2014_53_cousub/ "ftp://ftp2.census.gov/geo/tiger/TIGER2014/COUSUB/tl_2014_53_cousub.zip"
+unzip shapefiles/tl_2014_53_cousub/tl_2014_53_cousub.zip -d shapefiles/tl_2014_53_cousub/
+sudo rm shapefiles/tl_2014_53_cousub/tl_2014_53_cousub.zip
 
 # Create & load the table for the raw TIGER COUSUB data
-sudo su postgres -c "shp2pgsql -I -s 26918 shapefiles/tl_2014_36_cousub/tl_2014_36_cousub.shp public.tiger_cousub | psql -U postgres -d ${PGDATABASE}"
+sudo su postgres -c "shp2pgsql -I -s 26918 shapefiles/tl_2014_53_cousub/tl_2014_53_cousub.shp public.tiger_cousub | psql -U postgres -d ${PGDATABASE}"
 
 # Reduce the number of columns in tiger_cousub to only those relevant to the app and store result in aro_cousub
 sudo su postgres -c "psql -d ${PGDATABASE} -a -f create_aro_cousub.sql"
--- etl/tiger_edges/edges_etl.sh
+++ etl/tiger_edges/edges_etl.sh
@@ -11,14 +11,14 @@ export PGDATABASE=aro
 
 # We should download these to an S3 bucket when we load the rest of the country.
 sudo mkdir shapefiles
-sudo mkdir shapefiles/tl_2014_36047_edges
+sudo mkdir shapefiles/tl_2014_53033_edges
 
-sudo wget -P shapefiles/tl_2014_36047_edges/ "ftp://ftp2.census.gov/geo/tiger/TIGER2014/EDGES/tl_2014_36047_edges.zip"
-unzip shapefiles/tl_2014_36047_edges/tl_2014_36047_edges.zip -d shapefiles/tl_2014_36047_edges/
-sudo rm shapefiles/tl_2014_36047_edges/tl_2014_36047_edges.zip
+sudo wget -P shapefiles/tl_2014_53033_edges/ "ftp://ftp2.census.gov/geo/tiger/TIGER2014/EDGES/tl_2014_53033_edges.zip"
+unzip shapefiles/tl_2014_53033_edges/tl_2014_53033_edges.zip -d shapefiles/tl_2014_53033_edges/
+sudo rm shapefiles/tl_2014_53033_edges/tl_2014_53033_edges.zip
 
 # Create & load the table for the raw TIGER EDGES data
-sudo su postgres -c "shp2pgsql -I -s 26918 shapefiles/tl_2014_36047_edges/tl_2014_36047_edges.shp public.tiger_edges | psql -U postgres -d ${PGDATABASE}"
+sudo su postgres -c "shp2pgsql -I -s 26918 shapefiles/tl_2014_53033_edges/tl_2014_53033_edges.shp public.tiger_edges | psql -U postgres -d ${PGDATABASE}"
 
 # Reduce tiger_edges by removing features that are not roads, and eliminate unneeded columns
 sudo su postgres -c "psql -d ${PGDATABASE} -a -f create_aro_edges.sql"
