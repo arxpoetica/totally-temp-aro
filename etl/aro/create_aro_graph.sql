670fa13625fea97c9a25d52a0a0a061d726ae8de
Nick Vitucci <nvitucci@altvil.com>
2015-06-30 14:00:43 -0400

    Move routing graph creation to its own ETL task/script
     - Remove graph creation from the aro.graph ETL
     - Create sh and sql scripts for routing graph creation

--- etl/aro/create_aro_graph.sql
+++ etl/aro/create_aro_graph.sql
@@ -55,6 +55,3 @@ SELECT
 	ST_MakeLine(locations.geom, (SELECT road_nodes.geom FROM aro.road_nodes ORDER by locations.geom <-> road_nodes.geom LIMIT 1))
 FROM aro.locations;
 
--- After all edges and vertices have been added to the graph, we use pgRouting to create a topology:
--- Need to figure out how to tune the 'precision' argument here (0.00001 is the suggested value in the pgRouting docs)
--- SELECT pgr_createTopology('aro.graph', 0.00001, 'geom', 'id');
