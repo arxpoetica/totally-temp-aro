a177c05590bc4480546d10ce35606ce61c5d8332
Nick Vitucci <nvitucci@altvil.com>
2015-06-30 14:31:04 -0400

    Get infousa data from S3
     - Obtain compressed csv from S3 bucket
     - remove csv files from repository

--- etl/infousa/infousa_etl.sh
+++ etl/infousa/infousa_etl.sh
@@ -6,12 +6,20 @@ export PGPASSWORD=aro
 export PGHOST=localhost
 export PGBIN=/usr/bin
 PSQL=${PGBIN}/psql
+GISROOT=/gisdata
+TMPDIR=/gisdata/temp/
+UNZIPTOOL=unzip
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
 
-
+# Get infousa source file from S3
+cd $GISROOT;
+rm -f ${TMPDIR}/*.*
+wget https://s3.amazonaws.com/public.aro/infousa_businesses.zip -nd -nc
+unzip infousa_businesses.zip -d ${TMPDIR}
+cd $TMPDIR;
 # Create and load infousa_businesses table
 ${PSQL} -a -f $DIR/create_infousa_businesses.sql
 
 # TOTO: remove hard-coded path and find a better way of loading this data
-cat /vagrant/etl/infousa/sample_data/infousa_businesses.csv | ${PSQL} -c "COPY infousa.businesses FROM STDIN DELIMITER ',' CSV HEADER;"
+cat /$TMPDIR/infousa_businesses.csv | ${PSQL} -a -c "COPY infousa.businesses FROM STDIN DELIMITER ',' CSV HEADER;"
