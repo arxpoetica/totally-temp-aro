621094149b1ceed4f00acb86a87dbc92d87ace10
Nick Vitucci <nvitucci@altvil.com>
2015-07-06 14:27:08 -0400

    ETL reorganization for ARO schema
     - Use standard SRID (4326) in aro.cousub table
     - Limit aro.edges table to selected edge types
     - Limit aro.edges table to county 033 (for now)
     - Limit aro.locations table to county 033

--- etl/aro/create_aro_edges.sql
+++ etl/aro/create_aro_edges.sql
@@ -5,19 +5,29 @@
 
 CREATE TABLE aro.edges AS 
 SELECT
-	gid,
-	statefp,
-	countyfp,
-	'road_segment'::text AS edge_type,
-	ST_Length(Geography(ST_Transform(the_geom, 4326))) as edge_length,
-	ST_Transform(the_geom, 4326) as geom,
-	Geography(ST_Transform(the_geom, 4326)) as geog
+    gid,
+    statefp,
+    countyfp,
+    'road_segment'::text AS edge_type,
+    ST_Length(Geography(ST_Transform(the_geom, 4326))) as edge_length,
+    ST_Transform(the_geom, 4326) as geom,
+    Geography(ST_Transform(the_geom, 4326)) as geog
 FROM
-	tiger.edges;
+    tiger.edges
+WHERE 
+        (mtfcc = 'S1640'
+        OR
+        mtfcc = 'S1400'
+        OR
+        mtfcc = 'S1630'
+        OR
+        mtfcc = 'S1200')
+        AND
+        countyfp = '033';
 
 ALTER TABLE aro.edges
-	ADD CONSTRAINT pkey_aro_edges_gid
-		PRIMARY KEY (gid);
+    ADD CONSTRAINT pkey_aro_edges_gid
+        PRIMARY KEY (gid);
 
 CREATE INDEX idx_aro_edges_geom_gist ON aro.edges USING gist(geom);
 CREATE INDEX idx_aro_edges_geog_gist ON aro.edges USING gist(geog);
