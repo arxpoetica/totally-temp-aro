6a09103320f732a357d4f6bb70be1e8de4e7b4e5
Michael Maloney <mmaloney@altvil.com>
2015-07-29 16:47:55 -0400

    Adding dummy install costs for all businesses [#99907398]

--- etl/client/client_etl.sh
+++ etl/client/client_etl.sh
@@ -13,3 +13,6 @@ ${PSQL} -a -f $DIR/create_client_graph.sql
 
 # Populates a location entry fees table with fake data for 'client'. This should be replaced with real data later.
 ${PSQL} -a -f $DIR/load_location_entry_fees.sql
+
+# Populates a location entry fees table with fake data for 'client'. This should be replaced with real data later.
+${PSQL} -a -f $DIR/load_business_install_costs.sql
