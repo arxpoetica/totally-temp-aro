a41ef529b927acb839b4ff35b9951665935e09b2
Alberto Gimeno <gimenete@gmail.com>
2015-08-05 12:50:53 +0200

    Generalization of tools' visibility

--- app/public/javascripts/controllers/market_size_controller.js
+++ app/public/javascripts/controllers/market_size_controller.js
@@ -1,31 +1,21 @@
 // Market Size Controller
-app.controller('market_size_controller', ['$scope', '$rootScope', '$http', 'selection', 'MapLayer', function($scope, $rootScope, $http, selection, MapLayer) {
+app.controller('market_size_controller', ['$scope', '$rootScope', '$http', 'selection', 'map_tools', function($scope, $rootScope, $http, selection, map_tools) {
   // Controller instance variables
-  $scope.is_visible = false;
+  $scope.map_tools = map_tools;
 
   /************
   * FUNCTIONS *
   *************/
 
   // Listen for visibility toggle to be broadcast through $rootScope from other controller (map_tools_controller)
-  $rootScope.$on('toggle_market_size_visibility', function() {
-
-    $scope.is_visible = !$scope.is_visible;
-
-    $rootScope.area_layers.census_blocks_layer.set_highlighteable($scope.is_visible);
-  });
-
-  // Hide if DBR tool is selected and this is open
-  $rootScope.$on('toggle_tool_visibility', function() {
-
-    if($scope.is_visible){
-
-      $scope.is_visible = !$scope.is_visible;
+  $rootScope.$on('map_tool_changed_visibility', function(e, tool) {
+    if (tool === 'market_size') {
+      $rootScope.area_layers.census_blocks_layer.set_highlighteable(map_tools.is_visible('market_size'));
     }
   });
 
   $rootScope.$on('map_layer_clicked_feature', function(e, event, layer) {
-    if (layer.type === 'census_blocks' && $scope.is_visible) {
+    if (layer.type === 'census_blocks' && map_tools.is_visible('market_size')) {
       console.log('selected census block gid =', event.feature.getProperty('id'));
       // TODO: load data for this census_block
     }
