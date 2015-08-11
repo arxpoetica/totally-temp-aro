7c23e567e35a180f257add6536d9fdfe382cbb16
Nick Vitucci <nvitucci@altvil.com>
2015-08-11 15:23:49 -0400

    Cleanup from earlier resolved merge conflict

--- etl/client/create_client_network_node_types.sql
+++ etl/client/create_client_network_node_types.sql
@@ -8,5 +8,5 @@ CREATE TABLE client.network_node_types
 
 insert into client.network_node_types (name, description) values('central_office','Central Office') ;
 insert into client.network_node_types (name, description) values('splice_point','Splice Point') ;
-insert into client.network_node_types (name, description) values('fiber_distribution_hub','Fiber Deployment Hub') ;
-insert into client.network_node_types (name, description) values('fiber_distribution_terminal','Fiber Deployment Terminal') ;
+insert into client.network_node_types (name, description) values('fiber_distribution_hub','Fiber Distribution Hub') ;
+insert into client.network_node_types (name, description) values('fiber_distribution_terminal','Fiber Distribution Terminal') ;
