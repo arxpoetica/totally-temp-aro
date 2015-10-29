9e8d98be52b3fa1015b3a353f59b1585fb3a83a2
Michael Maloney <mmaloney@altvil.com>
2015-10-28 20:39:01 -0400

    Remove deprecated directory

--- etl-colt/etl/deprecated/infousa/optimize_infousa_businesses.sql
+++
@@ -1,6 +0,0 @@
-
--- DROP INDEX infousa.infousa_businesses_geog_gist;
--- DROP INDEX infousa.infousa_businesses_sic4;
-
-CREATE INDEX infousa_businesses_geog_gist ON infousa.businesses USING gist (geog);
-CREATE INDEX infousa_businesses_sic4 ON infousa.businesses USING btree (sic4);
