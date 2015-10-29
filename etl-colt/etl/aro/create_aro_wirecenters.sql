65fbabb62eca8be14038af22988d5c116590291b
Michael Maloney <mmaloney@altvil.com>
2015-10-28 20:48:03 -0400

    Create empty application tables. We will not load these by default.

--- etl-colt/etl/aro/create_aro_wirecenters.sql
+++ etl-colt/etl/aro/create_aro_wirecenters.sql
@@ -17,16 +17,3 @@ SELECT AddGeometryColumn('aro', 'wirecenters', 'geom', 4326, 'MULTIPOLYGON', 2);
 
 CREATE INDEX aro_wirecenters_geom_gist ON aro.wirecenters USING gist (geom);
 CREATE INDEX aro_wirecenters_geog_gist ON aro.wirecenters USING gist (geog);
-
--- Load the data we need from geotel.wirecenters into aro.wirecenters 
--- We'll preserve all carriers' fiber plant in the aro.wirecenters table, but only one set (the '"client's") will be added to the graph.
-INSERT INTO aro.wirecenters (gid, state, wirecenter, aocn, aocn_name, geog, geom)
-	SELECT
-		gid,
-		state,
-		wirecenter,
-		aocn,
-		aocn_name,
-		Geography(ST_Force_2D(the_geom)) as geog, -- Use ST_Force_2D because the source shapefiles have geometry type MultiLineStringZ...
-		ST_Force_2D(the_geom) AS geom -- Use ST_Force_2D because the source shapefiles have geometry type MultiLineStringZ...
-	FROM geotel.wirecenters;
