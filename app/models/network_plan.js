ac7a6e207041f65e599e31e9584c88018a04d2ed
Alberto Gimeno <gimenete@gmail.com>
2015-12-15 16:56:46 +0100

    Fixes fiber not loading when creating a new analysis

--- app/models/network_plan.js
+++ app/models/network_plan.js
@@ -273,6 +273,7 @@ NetworkPlan.create_plan = function(name, area, user, callback) {
     .then(function(callback) {
       var sql = multiline(function() {;/*
         SELECT
+          $2::text AS carrier_name,
           route.id, name, area_name, ST_AsGeoJSON(area_centroid)::json as area_centroid, ST_AsGeoJSON(area_bounds)::json as area_bounds,
           users.id as owner_id, users.first_name as owner_first_name, users.last_name as owner_last_name,
           created_at, updated_at
@@ -282,7 +283,7 @@ NetworkPlan.create_plan = function(name, area, user, callback) {
         LEFT JOIN auth.users ON users.id = permissions.user_id
         WHERE route.id=$1
       */});
-      database.findOne(sql, [id], callback);
+      database.findOne(sql, [id, config.client_carrier_name], callback);
     })
     .end(callback);
   }, callback);
