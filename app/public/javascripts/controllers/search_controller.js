c542aa0207f21e9feedb834270b73b139eba9512
Alberto Gimeno <gimenete@gmail.com>
2015-12-21 16:25:01 +0100

    Markers for searched businesses

--- app/public/javascripts/controllers/search_controller.js
+++ app/public/javascripts/controllers/search_controller.js
@@ -42,9 +42,20 @@ app.controller('search-controller', ['$scope', '$rootScope', '$http', 'map_tools
   })
 
   search.on('change', function() {
-    var value = search.select2('val')
-    var business = _.findWhere($scope.search_results, { id: value })
-    map.setCenter({ lat: business.geog.coordinates[1], lng: business.geog.coordinates[0] })
+    var value = search.select2('val');
+    var business = _.findWhere($scope.search_results, { id: value });
+    var center = { lat: business.geog.coordinates[1], lng: business.geog.coordinates[0] };
+    map.setCenter(center);
+
+    var marker = new google.maps.Marker({
+      position: center,
+      map: map,
+      animation: google.maps.Animation.DROP,
+    });
+
+    google.maps.event.addListener(marker, 'click', function(event) {
+      $rootScope.$broadcast('open_location', value);
+    })
   })
 
 }]);
