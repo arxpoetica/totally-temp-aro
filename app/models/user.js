5b28d8ddf63992e0f15bcdfbd1a3c8e53aea9ce2
Alberto Gimeno <gimenete@gmail.com>
2015-12-23 17:54:18 +0100

    Authentication tests

--- app/models/user.js
+++ app/models/user.js
@@ -68,7 +68,7 @@ User.delete_user = function(user_id, callback) {
 };
 
 User.register = function(user, callback) {
-  var code = randomCode();
+  var code = user.password ? null : randomCode();
 
   validate(function(expect) {
     expect(user, 'user', 'object');
@@ -134,11 +134,14 @@ User.find_by_text = function(text, callback) {
 }
 
 function randomCode() {
-  return crypto.randomBytes(32).toString('hex');
+  var rnd = crypto.randomBytes(32).toString('hex');
+  User.latest_code = rnd;
+  return rnd;
 }
 
 User.forgot_password = function(email, callback) {
   var code = randomCode();
+  email = email && email.toLowerCase();
 
   txain(function(callback) {
     var sql = 'SELECT id FROM auth.users WHERE email=$1';
