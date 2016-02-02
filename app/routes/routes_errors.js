3888318a8ac464740b91b45218b891b5e4d12a0d
Alberto Gimeno <gimenete@gmail.com>
2016-02-02 17:52:26 +0100

    Send production errors to slack

--- app/routes/routes_errors.js
+++ app/routes/routes_errors.js
@@ -1,4 +1,5 @@
 var models = require('../models');
+var request = require('request');
 
 exports.configure = function(app, middleware) {
 
@@ -13,6 +14,22 @@ exports.configure = function(app, middleware) {
     res.status(500).json({
       error: err.message,
     });
+
+    if (process.env.NODE_ENV === 'production') {
+      var base_url = process.env.APP_BASE_URL || 'Unknown'
+      var webhook = process.env.SLACK_WEBHOOK || 'https://hooks.slack.com/services/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'
+      var text = err.stack || err.message || String(err)
+      text += '\nBase URL: '+base_url
+      var opts = {
+        url: webhook,
+        body: JSON.stringify({
+          text: text,
+        })
+      };
+      request(opts, function(err, res, body) {
+        if (err) return console.log('Error while sending slack message', err, body)
+      });
+    }
   });
 
 };
