ddfea62995e9248a2f1723493f33a7c5f5b25c20
Alberto Gimeno <gimenete@gmail.com>
2016-04-12 17:15:10 +0200

    UI for area network planning

--- app/public/javascripts/src/controllers/area_network_planning_controller.js
+++ app/public/javascripts/src/controllers/area_network_planning_controller.js
@@ -1,53 +1,28 @@
-/* global app $ _ map google */
+/* global app */
 // Search Controller
 app.controller('area-network-planning-controller', ['$scope', '$rootScope', '$http', 'map_tools', ($scope, $rootScope, $http, map_tools) => {
   // Controller instance variables
   $scope.map_tools = map_tools
-  $scope.route = null
-  $scope.search_results = null
 
-  var search = $('#search_controller input')
-  search.select2({
-    ajax: {
-      url: '/search',
-      dataType: 'json',
-      delay: 250,
-      data: (term) => ({ text: term }),
-      results: (data, params) => {
-        var items = data.map((location) => {
-          return {
-            id: String(location.location_id),
-            text: location.name,
-            geog: location.geog
-          }
-        })
-        $scope.search_results = items
+  $scope.allStatus = ['geographies', 'cover', 'budget', 'progress']
+  $scope.wizardStatus = $scope.allStatus[0]
+  $scope.advancedSettings = false
 
-        return {
-          results: items,
-          pagination: {
-            more: false
-          }
-        }
-      },
-      cache: true
+  $scope.forward = () => {
+    var index = $scope.allStatus.indexOf($scope.wizardStatus)
+    if (index + 1 < $scope.allStatus.length) {
+      $scope.wizardStatus = $scope.allStatus[index + 1]
     }
-  })
+  }
 
-  search.on('change', () => {
-    var value = search.select2('val')
-    var location = _.findWhere($scope.search_results, { id: value })
-    var center = { lat: location.geog.coordinates[1], lng: location.geog.coordinates[0] }
-    map.setCenter(center)
+  $scope.back = () => {
+    var index = $scope.allStatus.indexOf($scope.wizardStatus)
+    if (index > 0) {
+      $scope.wizardStatus = $scope.allStatus[index - 1]
+    }
+  }
 
-    var marker = new google.maps.Marker({
-      position: center,
-      map: map,
-      animation: google.maps.Animation.DROP
-    })
-
-    google.maps.event.addListener(marker, 'click', (event) => {
-      $rootScope.$broadcast('open_location', location.id)
-    })
-  })
+  $scope.toggleAdvancedSettings = () => {
+    $scope.advancedSettings = !$scope.advancedSettings
+  }
 }])
