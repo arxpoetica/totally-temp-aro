914de215bd61bbf7c794d09a0704ca5af75990e2
Scott Riggins <scott.riggins@socialmobility.com>
2016-04-22 11:07:29 -0500

    trivial cleanup of an IDE warning

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/network/impl/NetworkServiceImpl.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/network/impl/NetworkServiceImpl.java
@@ -2,7 +2,6 @@ package com.altvil.aro.service.network.impl;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
