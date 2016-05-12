729a248f4604a38d4578c4d0512a9deb8873911c
Michael Maloney <mmaloney@altvil.com>
2016-05-12 17:24:29 -0400

    Correct spelling of tooltip

--- app/public/javascripts/src/models/map_tools.js
+++ app/public/javascripts/src/models/map_tools.js
@@ -107,13 +107,13 @@ app.service('map_tools', ['$rootScope', 'tracker', ($rootScope, tracker) => {
     },
     {
       id: 'area_network_planning',
-      name: 'Area netowrk planning',
+      name: 'Area Network Planning',
       short_name: 'A',
       icon: 'icon icon-network-planning'
     },
     {
       id: 'financial_profile',
-      name: 'Financial profile',
+      name: 'Financial Profile',
       short_name: 'F',
       icon: 'fa fa-usd'
     }
