c1ef429d22881ffcd9ac15765f196fdb624b4643
Michael Maloney <mmaloney@altvil.com>
2016-06-01 14:46:34 -0400

    New info group household ETL

--- etl-aro/etl/aro/create_aro_locations.sql
+++ etl-aro/etl/aro/create_aro_locations.sql
@@ -20,11 +20,10 @@ SELECT AddGeometryColumn('aro', 'locations', 'geom', 4326, 'POINT', 2);
 
 -- SELECT setval('aro.locations_id_seq', COALESCE((SELECT MAX(id)+1 FROM locations), 1));
 
--- Load locations from infousa_businesses
+-- Make locations out of InfoUSA businesses (infousa.businesses)
 -- ONLY using UES wirecenter for Verizon
-INSERT INTO aro.locations(id, address, city, state, zipcode, lat, lon, geog, geom)
+INSERT INTO aro.locations(address, city, state, zipcode, lat, lon, geog, geom)
     SELECT DISTINCT ON (bldgid)
-        bldgid as id,
         address,
         city,
         businesses.state,
@@ -33,24 +32,36 @@ INSERT INTO aro.locations(id, address, city, state, zipcode, lat, lon, geog, geo
         long AS lon,
         businesses.geog as geog,
         businesses.geog::geometry as geom
+    FROM infousa.businesses
+    JOIN aro.wirecenters
+      ON ST_Within(businesses.geog::geometry, wirecenters.geom)
+    WHERE
+      wirecenters.wirecenter = 'NYCMNY79'
+      OR
+      wirecenters.wirecenter = 'BFLONYEL'
+      OR
+      wirecenters.wirecenter = 'ORPKNYST';
 
-    FROM infousa.businesses;-- JOIN aro.wirecenters
-      -- ON businesses.geog && wirecenters.geog
-    --   ON ST_Within(businesses.geog::geometry, wirecenters.geom)
-    -- WHERE
-    --   -- NYC Upper East Side (URBAN)
-    --   wirecenters.wirecenter = 'NYCMNY79'
-    --   OR
-    --   -- Buffalo, New York (URBAN)
-    --   wirecenters.wirecenter = 'BFLONYEL'
-    --   OR
-    --   wirecenters.wirecenter = 'BFLONYFR'
-    --   OR
-    --   -- Orchard Park, NY (SUBURBAN)
-    --   wirecenters.wirecenter = 'ORPKNYST'
-    --   OR
-    --   -- North Collins, NY (RURAL)
-    --   wirecenters.wirecenter = 'NCLNNYNO';
+-- Make locations out of InfoGroup households (temp_hh.households)
+INSERT INTO aro.locations(address, city, state, zipcode, lat, lon, geom, geog)
+    SELECT DISTINCT ST_AsText(hh.geog)
+        address,
+        city,
+        hh.state,
+        zip5 AS zipcode,
+        lat,
+        lon,
+        hh.geog::geometry as geom,
+        hh.geog
+    FROM temp_hh.households hh
+    JOIN aro.wirecenters wc
+        ON ST_Within(hh.geog::geometry, wc.geom)
+    WHERE
+        wc.wirecenter = 'NYCMNY79'
+        OR
+        wc.wirecenter = 'BFLONYEL'
+        OR
+        wc.wirecenter = 'ORPKNYST';
 
 CREATE INDEX aro_locations_geog_gist
   ON aro.locations
---
+++ etl-aro/etl/aro/create_aro_temp_households.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS aro.temp_households;
+
+CREATE TABLE aro.temp_households
+(
+	id serial,
+	location_id bigint,
+	address varchar,
+	city varchar,
+	state varchar,
+	zipcode varchar,
+	lat double precision,
+	lon double precision,
+	geog geography(POINT, 4326),
+	CONSTRAINT aro_temp_households_pkey PRIMARY KEY (id)
+);
+
+SELECT AddGeometryColumn('aro', 'temp_households', 'geom', 4326, 'POINT', 2);
+
+-- Match temp_hh.households to aro.locations using geography
+-- Assign location_id of matching location to row on insert
+INSERT INTO aro.temp_households (location_id, address, city, state, zipcode, lat, lon, geog, geom)
+	SELECT
+		loc.id,
+		hh.address,
+		hh.city,
+		hh.state,
+		hh.zip5 AS zipcode,
+		hh.lat,
+		hh.lon,
+		hh.geog,
+		hh.geog::geometry AS geom
+	FROM temp_hh.households hh
+	JOIN aro.locations loc
+		ON ST_Equals(loc.geom, hh.geog::geometry)
+	JOIN aro.wirecenters wc
+  	ON ST_Within(hh.geog::geometry, wc.geom)
+  WHERE
+  	wc.wirecenter = 'NYCMNY79'
+  OR
+  	wc.wirecenter = 'BFLONYEL'
+  OR
+  	wc.wirecenter = 'ORPKNYST';
+
+CREATE INDEX aro_temp_households_location_index ON aro.temp_households(location_id);
+CREATE INDEX aro_temp_households_geog_index ON aro.temp_households USING gist(geog);
+CREATE INDEX aro_temp_households_geom_index ON aro.temp_households USING gist(geom);
