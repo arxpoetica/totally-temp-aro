785681deaf989fd8ce10a94aa18e60742605929d
Nick Vitucci <nvitucci@altvil.com>
2016-06-08 16:32:21 -0400

    Get infousa households file from S3

--- etl-aro/etl/temp_hh/temp_hh_etl.sh
+++ etl-aro/etl/temp_hh/temp_hh_etl.sh
@@ -9,10 +9,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the scrip
 
 ${PSQL} -a -f $DIR/create_temp_hhs.sql
 
-cd $TMPDIR
+# Get infousa-households source file from S3
+cd $GISROOT;
+rm -f ${TMPDIR}/*.*
+wget https://s3.amazonaws.com/public.aro/infousa/ny_households.zip -nd -nc
+unzip ny_households.zip -d ${TMPDIR}
 
 # Placing this CSV file here because eventually this should download from S3 and get deleted after it loads into the db
-cat /$TMPDIR/ny_households.csv | ${PSQL} -a -c "COPY temp_hh.households (address, city, state, zip5, lat, lon, geog) FROM STDIN DELIMITER ',' CSV HEADER;"
+${PSQL} -a -c "COPY temp_hh.households (address, city, state, zip5, lat, lon, geog) FROM STDIN DELIMITER ',' CSV HEADER;" </$TMPDIR/ny_households.csv
 
 ${PSQL} -a -f $DIR/optimize_temp_hh.sql
 
