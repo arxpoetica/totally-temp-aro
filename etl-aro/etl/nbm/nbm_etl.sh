8e649cc7fb452760cb0d878432fabf714d93c8b0
Michael Maloney <mmaloney@altvil.com>
2016-06-14 14:21:41 -0400

    Create source table / data import for NBM

---
+++ etl-aro/etl/nbm/nbm_etl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
+GISROOT=/gisdata
+TMPDIR=/gisdata/temp/
+UNZIPTOOL=unzip
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
+
+${PSQL} -a -f $DIR/create_nbm_blocks.sql
+
+# Replace with S3 step...
+cd $TMPDIR
+cat /$TMPDIR/NY-NBM-CBLOCK-CSV-JUN-2014.CSV | ${PSQL} -a -c "COPY nbm.blocks FROM STDIN DELIMITER '|' CSV HEADER;"
