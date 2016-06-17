d89921d03f7f6ddc9c7d32e1598ba0f7bb6e61f2
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-06-17 14:01:01 -0500

    Fix Conflict Builder

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/planning/optimization/strategies/OptimizationPlanConfigurationMaxBuilder.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/planning/optimization/strategies/OptimizationPlanConfigurationMaxBuilder.java
@@ -1,13 +1,10 @@
 package com.altvil.aro.service.planning.optimization.strategies;
 
-import com.altvil.annotation.OptimizationPlanStrategy;
 import com.altvil.aro.service.planning.IrrOptimizationPlan;
 import com.altvil.aro.service.planning.OptimizationPlan;
-import com.altvil.aro.service.planning.OptimizationPlan;
 import com.altvil.aro.service.planning.optimization.OptimizationPlanConfigurationBuilder;
-import com.altvil.enumerations.OptimizationType;
 
-@OptimizationPlanStrategy(type=OptimizationPlanConfigurationBuilder.class, types={OptimizationType.BUDGET_IRR, OptimizationType.MAX_IRR, OptimizationType.TARGET_IRR})
+//@OptimizationPlanStrategy(type=OptimizationPlanConfigurationBuilder.class, types={OptimizationType.BUDGET_IRR, OptimizationType.MAX_IRR, OptimizationType.TARGET_IRR})
 public class OptimizationPlanConfigurationMaxBuilder implements OptimizationPlanConfigurationBuilder<OptimizationPlanConfigurationIrr> {
 	@Override
 	public OptimizationPlanConfigurationIrr build(OptimizationPlan fiberPlan) {
