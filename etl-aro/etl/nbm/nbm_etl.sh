7a316c48dbd133363e6919403d7cf7450452ce73
Michael Maloney <mmaloney@altvil.com>
2016-06-20 17:22:25 -0400

    Add an index to the NBM blocks for faster joins

--- etl-aro/etl/nbm/nbm_etl.sh
+++ etl-aro/etl/nbm/nbm_etl.sh
@@ -12,3 +12,5 @@ ${PSQL} -a -f $DIR/create_nbm_blocks.sql
 # Replace with S3 step...
 cd $GISROOT
 cat /$GISROOT/NY-NBM-CBLOCK-CSV-JUN-2014.CSV | ${PSQL} -a -c "COPY nbm.blocks FROM STDIN DELIMITER '|' CSV HEADER;"
+
+${PSQL} -a -f $DIR/optimize_nbm_blocks.sql
