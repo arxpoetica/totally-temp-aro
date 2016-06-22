35d348b91eaa6c0f31080dd36df34d38e70b8577
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-06-22 05:18:02 -0500

    Add Demo Columns back into ETL Script (These are deprecated and should be removed once a replacement service is created)

--- etl-aro/etl/client/create_client_plan.sql
+++ etl-aro/etl/client/create_client_plan.sql
@@ -14,6 +14,19 @@ CREATE TABLE client.plan
   created_at timestamp NOT NULL,
   updated_at timestamp NOT NULL,
 
+  "total_cost" float8,
+  "total_revenue" float8,
+  "household_revenue" float8,
+  "celltower_revenue" float8,
+  "business_revenue" float8,
+  "fiber_cost" float8,
+  "equipment_cost" float8,
+  "co_cost" float8,
+  "fdh_cost" float8,
+  "fdt_cost" float8,
+  "total_count" float8,
+  "npv" float8,
+
   CONSTRAINT client_plan_pkey PRIMARY KEY (id)
 );
 
