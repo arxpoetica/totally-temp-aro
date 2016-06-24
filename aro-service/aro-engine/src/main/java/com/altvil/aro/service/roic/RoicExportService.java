9afe533ba5f6b69901dfeab8e50063bf34e7f741
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-06-24 07:04:27 -0500

    Add support For Roic Model Export as CSV

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/roic/RoicExportService.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/roic/RoicExportService.java
@@ -1,13 +1,13 @@
 package com.altvil.aro.service.roic;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Collection;
 
-import com.altvil.aro.service.roic.analysis.model.RoicModel;
+import com.altvil.aro.service.roic.analysis.RowReference;
 
 public interface RoicExportService {
-	
-	Reader createCsvReader(RoicModel model, Collection<String> curves) ;
-	Reader createCsvReader(RoicModel model) ;
+
+	Reader createCsvReader(Collection<RowReference> rows) throws IOException;
 
 }
