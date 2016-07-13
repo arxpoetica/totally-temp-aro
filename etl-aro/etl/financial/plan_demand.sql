be47c6ee940809ae61ee43746b3d225a14841a94
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-07-13 10:41:12 -0500

    remove referential constraints to ease upgrading to new schema

--- etl-aro/etl/financial/plan_demand.sql
+++ etl-aro/etl/financial/plan_demand.sql
@@ -5,6 +5,11 @@ CREATE TABLE financial.plan_demand (
 
 	network_report_id int8 not null references financial.network_report on delete cascade ,
 	
+	--speed_type int4  not null references client.speed_type,
+	--product_type int4 not null references client.product_type,
+	--demand_type int4 not null references client.demand_type
+
+	-- Disable references until fully reloaded in all environments
 	speed_type int4  not null references client.speed_type,
 	product_type int4 not null references client.product_type,
 	demand_type int4 not null references client.demand_type
