744594cc770ecab1eace2acc56c4cf8f8ce4652f
Michael Maloney <mmaloney@altvil.com>
2016-07-18 15:58:55 -0400

    temp hh ETL fixes

--- etl-aro/etl/temp_hh/optimize_temp_hh.sql
+++ etl-aro/etl/temp_hh/optimize_temp_hh.sql
@@ -1,4 +1,13 @@
+SELECT AddGeometryColumn('temp_hh', 'households', 'geom', 4326, 'POINT', 2);
+
+UPDATE temp_hh.households SET geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);
+
 CREATE INDEX temp_hh_geog_gist
   ON temp_hh.households
   USING gist
-  (geog)
+  (geog);
+
+CREATE INDEX temp_hh_geom_gist
+  ON temp_hh.households
+  USING gist
+  (geom);
