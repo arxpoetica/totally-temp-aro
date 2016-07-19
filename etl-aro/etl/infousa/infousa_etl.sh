c4dd6d6ca633036f8d2bc155feee707b7c7c2bfc
Michael Maloney <mmaloney@altvil.com>
2016-07-18 21:43:49 -0400

    Update infoUSA ETL

--- etl-aro/etl/infousa/infousa_etl.sh
+++ etl-aro/etl/infousa/infousa_etl.sh
@@ -7,17 +7,20 @@ UNZIPTOOL=unzip
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
 
-# Get infousa source file from S3
-cd $GISROOT;
-rm -f ${TMPDIR}/*.*
-wget https://s3.amazonaws.com/public.aro/infousa/infousa_ny.zip -nd -nc
-unzip infousa_ny.zip -d ${TMPDIR}
-cd $TMPDIR;
-# Create and load infousa_businesses table
 ${PSQL} -a -f $DIR/create_infousa_businesses.sql
 
-# TOTO: remove hard-coded path and find a better way of loading this data
-cat /$TMPDIR/infousa_ny.csv | ${PSQL} -a -c "COPY infousa.businesses FROM STDIN DELIMITER ',' CSV HEADER;"
+# Use lower case state names. FIPS codes unnecessary here as well.
+declare -a STATE_ARRAY=( 'wa' )
+
+cd $GISROOT;
+
+for STATE in "${STATE_ARRAY[@]}"
+do
+	rm -f ${TMPDIR}/*.*
+	wget https://s3.amazonaws.com/public.aro/infousa/businesses_${STATE}.zip -nd -nc
+	$UNZIPTOOL businesses_${STATE}.zip -d ${TMPDIR}
+	cat /$TMPDIR/businesses_${STATE}.csv | ${PSQL} -a -c "COPY infousa.businesses FROM STDIN DELIMITER ',' CSV HEADER;"
+done
 
 # Optimize buisnesses table
 ${PSQL} -a -f $DIR/optimize_infousa_businesses.sql
