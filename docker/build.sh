f6a65215805866fe531163245d8584fcba47875c
Nick Vitucci <nvitucci@altvil.com>
2016-07-21 15:24:11 -0400

    stuff

---
+++ docker/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Container build script
+APP_SOURCE_ROOT=$(cd "$(dirname "$0")" && pwd)/..
+APP_TARGET=${APP_SOURCE_ROOT}/target
+
+# copy content into target directory
+mkdir -p ${APP_TARGET}
+for d in app conf etl-aro init ; do
+    cp -r ${APP_SOURCE_ROOT}/${d} ${APP_TARGET}
+done
+for s in runserver.sh circle.yml ; do
+    cp ${APP_SOURCE_ROOT}/${s} ${APP_TARGET}
+done
+for s in cmo.ini Dockerfile ; do
+    cp ${APP_SOURCE_ROOT}/docker/${s} ${APP_TARGET}
+done
+
+# install application npm dependencies
+(cd ${APP_TARGET}/app && npm install --no-bin-link)
+
