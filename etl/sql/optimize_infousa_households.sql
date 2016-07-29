1b5c8b570be39433e05fe67c8af929899784df41
Nick Vitucci <nvitucci@altvil.com>
2016-07-29 17:34:13 -0400

    Restructure ETL: fix error in infousa again again again FFS

--- etl/sql/optimize_infousa_households.sql
+++ etl/sql/optimize_infousa_households.sql
@@ -1,6 +1,6 @@
 SELECT AddGeometryColumn('infousa', 'households', 'geom', 4326, 'POINT', 2);
 
-UPDATE infousa_hh.households SET geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);
+UPDATE infousa.households SET geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);
 
 CREATE INDEX infousa_households_geog_gist
   ON infousa.households
