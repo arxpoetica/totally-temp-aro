1454edbcf75a86181d45f8dac2cbd705d38db123
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-08-10 15:55:37 -0500

    Force file to update so  client.fiber_route is valid

--- etl-aro/etl/client/create_client_fiber_route_segment.sql
+++ etl-aro/etl/client/create_client_fiber_route_segment.sql
@@ -4,8 +4,9 @@
 DROP TABLE IF EXISTS "client"."fiber_route_segment";
 CREATE TABLE "client"."fiber_route_segment" (
 	"id" bigserial,
-	"fiber_route_id" int8 REFERENCES client.fiber_route ON DELETE CASCADE,
-	"cable_construction_type_id" int4 REFERENCES client.cable_construction_type ,
+	"fiber_route_id" int8 not null REFERENCES client.fiber_route ON DELETE CASCADE,
+	"cable_construction_type_id" int4 not null REFERENCES client.cable_construction_type ,
+	length_meters double precision not null, 
 	"geom" "public"."geometry",
 	CONSTRAINT client_fiber_route_segment_pkey PRIMARY KEY (id)
 );
