43ee5b1dd4d7ceecacff0142984bb0d31f0b28c8
Nick Vitucci <nvitucci@altvil.com>
2016-08-11 12:50:42 -0400

    Cleanup

--- etl-colt/etl/reset_aro_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
-
-${PSQL} -c "DROP SCHEMA IF EXISTS aro CASCADE;"
-${PSQL} -c "CREATE SCHEMA aro;"
--- etl-colt/etl/reset_source_colt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
-
-${PSQL} -c "DROP SCHEMA IF EXISTS source_colt CASCADE;"
-${PSQL} -c "CREATE SCHEMA source_colt;"
-
