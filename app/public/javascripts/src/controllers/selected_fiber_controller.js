6ea1834ee2fc9a761664f5acba642d33b2b9bd58
Alberto Gimeno <gimenete@gmail.com>
2016-08-16 19:36:24 +0200

    Fix left-click on features that are not fiber

--- app/public/javascripts/src/controllers/selected_fiber_controller.js
+++ app/public/javascripts/src/controllers/selected_fiber_controller.js
@@ -2,7 +2,10 @@
 // Equipment Nodes Controller
 app.controller('selected_fiber_controller', ['$scope', '$rootScope', '$http', ($scope, $rootScope, $http) => {
   $rootScope.$on('map_layer_clicked_feature', (e, event, layer) => {
+    var fiberType = event.feature.getProperty('fiber_type')
+    if (!fiberType) return
     var centroid = event.feature.getProperty('centroid')
+    if (!centroid) return console.warn('Fiber without centroid')
     var coordinates = centroid.coordinates
     $scope.lat = coordinates[1]
     $scope.lon = coordinates[0]
