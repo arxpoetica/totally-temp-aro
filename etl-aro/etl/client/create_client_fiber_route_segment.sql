b9ad8198d02861358b7fc1adc6762e25e7a7d71a
Alberto Gimeno <gimenete@gmail.com>
2016-08-17 10:53:38 +0200

    Update aro-etl

--- etl-aro/etl/client/create_client_fiber_route_segment.sql
+++ etl-aro/etl/client/create_client_fiber_route_segment.sql
@@ -1,12 +1,9 @@
--- ------------------------------------------
---  Table structure for fiber_route_segment
--- ------------------------------------------
 DROP TABLE IF EXISTS "client"."fiber_route_segment";
 CREATE TABLE "client"."fiber_route_segment" (
 	"id" bigserial,
 	"fiber_route_id" int8 not null REFERENCES client.fiber_route ON DELETE CASCADE,
 	"cable_construction_type_id" int4 not null REFERENCES client.cable_construction_type ,
-	length_meters double precision not null, 
+	length_meters double precision not null,
 	"geom" "public"."geometry",
 	CONSTRAINT client_fiber_route_segment_pkey PRIMARY KEY (id)
 );
@@ -18,4 +15,3 @@ CREATE INDEX client_fiber_route_segment_geom_gist
   (geom);
 
  CREATE INDEX client_network_fiber_segment_route_index ON client.fiber_route_segment(fiber_route_id);
-
