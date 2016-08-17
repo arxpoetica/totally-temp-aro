0a6f104f591319ba2a17cb065f939bf9ee0393ff
Alberto Gimeno <gimenete@gmail.com>
2016-08-17 18:54:26 +0200

    Apply changes made in etl-vz to et-aro

--- etl-aro/etl/aro/calculate_locations_dn_entity_categories.sql
+++ etl-aro/etl/aro/calculate_locations_dn_entity_categories.sql
@@ -17,3 +17,12 @@ UPDATE locations SET dn_entity_categories =
 		WHERE h.location_id = locations.id
 	)
 ;
+
+ALTER TABLE aro.businesses ADD COLUMN monthly_recurring_cost numeric;
+ALTER TABLE aro.businesses ADD COLUMN source varchar;
+
+UPDATE aro.locations
+   SET dn_entity_categories = (dn_entity_categories || ARRAY['b_2kplus']::varchar[])
+  FROM aro.businesses
+ WHERE monthly_recurring_cost >= 2000
+   AND businesses.location_id = locations.id;
