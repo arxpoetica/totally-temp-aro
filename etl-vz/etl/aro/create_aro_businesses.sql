1a9b2233ec6a357798202f212053dee49b211ea5
Michael Maloney <mmaloney@altvil.com>
2016-08-23 11:34:41 -0400

    Generic industry and number of employees

--- etl-vz/etl/aro/create_aro_businesses.sql
+++ etl-vz/etl/aro/create_aro_businesses.sql
@@ -19,11 +19,13 @@ CREATE TABLE aro.businesses
 );
 
 -- Insert all VZ customers
-INSERT INTO aro.businesses(location_id, name, address, annual_recurring_cost, monthly_recurring_cost, source, geog, geom)
+INSERT INTO aro.businesses(location_id, industry_id, name, address, number_of_employees, annual_recurring_cost, monthly_recurring_cost, source, geog, geom)
 	SELECT
 		l.id,
+		(SELECT id FROM aro.industries WHERE description = 'MENS & BOYS CLOTHING STORES'), -- Generic SIC4, try to force categorization as retail
 		b.nasp_nm,
 		b.prism_formatted_address,
+		1000,
 		b.sum_mrc * 12,
 		b.sum_mrc,
 		'vz_customers',
@@ -34,9 +36,10 @@ INSERT INTO aro.businesses(location_id, name, address, annual_recurring_cost, mo
    	ON ST_Equals(l.geom, ST_SetSRID(ST_MakePoint(b.long, b.lat), 4326));
 
 -- Insert all TAMs
-INSERT INTO aro.businesses(location_id, name, address, number_of_employees, source, geog, geom)
+INSERT INTO aro.businesses(location_id, industry_id, name, address, number_of_employees, source, geog, geom)
 	SELECT
 		l.id,
+		(SELECT id FROM aro.industries WHERE description = 'MENS & BOYS CLOTHING STORES'), -- Generic SIC4, try to force categorization as retail
 		b.business_nm,
 		b.street_addr,
 		b.emp_here,
@@ -49,7 +52,6 @@ INSERT INTO aro.businesses(location_id, name, address, number_of_employees, sour
    WHERE 
    	arcgis_latitude != 0 
    	AND arcgis_longitude != 0;
-
 
 CREATE INDEX aro_businesses_location_index ON aro.businesses(location_id);
 CREATE INDEX aro_businesses_industry_index ON aro.businesses(industry_id);
