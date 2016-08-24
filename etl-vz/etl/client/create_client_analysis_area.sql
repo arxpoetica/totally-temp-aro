47768030e1b2082a2171a1aeb701c3247502836d
Michael Maloney <mmaloney@altvil.com>
2016-08-24 19:03:13 -0400

    Directional facilities: equipment and boundaries

--- etl-vz/etl/client/create_client_analysis_area.sql
+++ etl-vz/etl/client/create_client_analysis_area.sql
@@ -18,3 +18,18 @@ CREATE INDEX client_analysis_area_geom_gist ON client.analysis_area USING gist (
 CREATE INDEX client_analysis_area_geog_gist ON client.analysis_area USING gist (geog);
 CREATE INDEX client_analysis_area_edge_buffer ON client.analysis_area USING gist (edge_buffer);
 CREATE INDEX client_analysis_area_location_edge_buffer ON client.analysis_area USING gist (location_edge_buffer) ;
+
+-- Load FCC CMA boundaries (public)
+WITH cma_layer AS (
+	SELECT * FROM client.analysis_layer WHERE name = 'cma'
+)
+INSERT INTO client.analysis_area (analysis_layer_id, source_id, code, geog, geom, edge_buffer, location_edge_buffer)
+	SELECT
+		l.id,
+		c.gid::varchar,
+		c.name,
+		Geography(ST_Force_2D(the_geom)) as geog,
+		ST_Force_2D(the_geom) AS geom, -- Use ST_FORce_2D because the source shapefiles have geometry type MultiLineStringZ...
+		ST_Transform(ST_buffer(ST_Convexhull(the_geom)::Geography, 200)::Geometry, 4326),
+		ST_Transform(ST_buffer(ST_Convexhull(the_geom)::Geography, 50)::Geometry, 4326)  
+	FROM boundaries.cma c, cma_layer l;
