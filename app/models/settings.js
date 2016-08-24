c739a96b8e4c5a20765e644d4acf7675ca893fd6
Alberto Gimeno <gimenete@gmail.com>
2016-08-24 10:48:51 +0200

    Financial assumptions in application settings

--- app/models/settings.js
+++ app/models/settings.js
@@ -8,20 +8,58 @@ var pync = require('pync')
 module.exports = class Settings {
 
   static view () {
-    var sql = `
-      SELECT ca.id, cc.description AS name, uom.description AS unit, ca.cost AS cost
-        FROM financial.cost_assignment ca
-        JOIN financial.cost_code cc ON ca.cost_code_id = cc.id
-        JOIN aro.uom uom ON cc.unit_of_measure_id = uom.id
-        ORDER BY uom.description, cc.description ASC
-    `
-    return database.query(sql)
+    return Promise.all([
+      database.query(`
+        SELECT ca.id, cc.description AS name, uom.description AS unit, ca.cost AS cost
+          FROM financial.cost_assignment ca
+          JOIN financial.cost_code cc ON ca.cost_code_id = cc.id
+          JOIN aro.uom uom ON cc.unit_of_measure_id = uom.id
+          ORDER BY uom.description, cc.description ASC
+      `),
+      database.query(`
+        SELECT
+          rci.id,
+          arpu,
+          entity_growth,
+          churn_rate,
+          opex_percent,
+          maintenance_expenses,
+          connection_cost,
+          ec.description AS entity_description,
+          st.description AS product
+        FROM financial.roic_component_input rci
+        JOIN client.speed_type st ON rci.speed_type_id = st.id
+        JOIN client.entity_category ec ON rci.entity_category_id = ec.id
+      `)
+    ])
+    .then((results) => ({
+      networkCosts: results[0],
+      financialAssumptions: results[1]
+    }))
   }
 
   static update (options) {
-    return pync.series(Object.keys(options), (key) => (
-      database.execute('UPDATE financial.cost_assignment SET cost=$1 WHERE id=$2', [options[key], key])
-    ))
+    var networkCosts = options.networkCosts || {}
+    var financialAssumptions = options.financialAssumptions || {}
+    var financialFields = [
+      'arpu',
+      'entity_growth',
+      'churn_rate',
+      'opex_percent',
+      'maintenance_expenses',
+      'connection_cost'
+    ]
+    return Promise.all([
+      pync.series(Object.keys(networkCosts), (key) => (
+        database.execute('UPDATE financial.cost_assignment SET cost=$1 WHERE id=$2', [networkCosts[key].cost, key])
+      )),
+      pync.series(Object.keys(financialAssumptions), (key) => (
+        pync.series(financialFields, (field) => (
+          financialAssumptions[key][field] != null &&
+          database.execute(`UPDATE financial.roic_component_input SET ${field}=$1 WHERE id=$2`, [financialAssumptions[key][field], key])
+        ))
+      ))
+    ])
   }
 
 }
---
+++ app/public/javascripts/src/controllers/settings_controller.js
@@ -0,0 +1,46 @@
+/* global app $ */
+// Navigation Menu Controller
+app.controller('settings_controller', ['$scope', '$rootScope', '$http', '$filter', '$timeout', ($scope, $rootScope, $http, $filter, $timeout) => {
+  function fetchApplicationSettings () {
+    $http.get('/admin/settings')
+      .success((response) => {
+        $scope.applicationSettings = response
+        $scope.changes = {}
+
+        $timeout(() => {
+          const parseCost = (input) => +(input.val() || '0').match(/[\d\.]/g).join('') || 0
+
+          $('#application-settings .format-currency')
+            .on('focus', function () {
+              var input = $(this)
+              input.val(parseCost(input).toFixed(2))
+            })
+            .on('change', function () {
+              var input = $(this)
+              var tab = input.data('tab')
+              var id = input.data('id')
+              var field = input.data('field')
+              var obj = $scope.changes[tab] || {}
+              $scope.changes[tab] = obj
+              var values = obj[id] || {}
+              obj[id] = values
+              values[field] = parseCost(input)
+              console.log('changes', $scope.changes)
+            })
+            .on('blur', function () {
+              var input = $(this)
+              input.val($filter('number')(parseCost(input), 2))
+            })
+        }, 0)
+      })
+  }
+
+  $('#application-settings').on('shown.bs.modal', fetchApplicationSettings)
+
+  $scope.updateSettings = () => {
+    $http.post('/admin/settings', $scope.changes).success((response) => {
+      $scope.changes = {}
+      $('#application-settings').modal('hide')
+    })
+  }
+}])
