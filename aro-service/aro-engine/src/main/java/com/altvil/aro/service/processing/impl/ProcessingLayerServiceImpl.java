cd5ba5bed20aef61c4334283588a679306d447fc
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-08-24 10:00:49 -0500

    Enhanced Rule Support

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/processing/impl/ProcessingLayerServiceImpl.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/processing/impl/ProcessingLayerServiceImpl.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -23,17 +24,16 @@ import com.altvil.aro.service.entity.LocationEntityType;
 import com.altvil.aro.service.entity.mapping.LocationEntityTypeMapping;
 import com.altvil.aro.service.processing.ProcessingLayerService;
 import com.altvil.utils.StreamUtil;
+import com.altvil.utils.reference.VolatileReference;
 
 @Service
 public class ProcessingLayerServiceImpl implements ProcessingLayerService {
 
-	//private static final String RULE = "system_defaults";
+	// private static final String RULE = "system_defaults";
 
 	private ServiceLayerRepository serviceLayerRepository;
 
-	private Map<Integer, ServiceLayer> serviceLayerMap;
-	private Map<LocationEntityType, List<ServiceLayer>> entityMap;
-	private Comparator<ServiceLayer> serviceLayerComparator;
+	VolatileReference<SystemRule> systemRuleRef;
 
 	@Autowired
 	public ProcessingLayerServiceImpl(
@@ -42,84 +42,158 @@ public class ProcessingLayerServiceImpl implements ProcessingLayerService {
 		this.serviceLayerRepository = serviceLayerRepository;
 	}
 
+	private SystemRule loadSystemRule() {
+		return new SystemRule().construct();
+	}
+
 	@PostConstruct
 	void postConstruct() {
-		serviceLayerMap = StreamUtil.hash(serviceLayerRepository.findAll(),
-				ServiceLayer::getId);
-		entityMap = loadCategoryRules(serviceLayerMap);
-		serviceLayerComparator = createComparator(loadPrioityMap(serviceLayerMap));
-	}
-
-	private Comparator<ServiceLayer> createComparator(
-			Map<ServiceLayer, Integer> priorityMap) {
-		return (s1, s2) -> priorityMap.get(s2).compareTo(priorityMap.get(s1));
-	}
-
-	private Map<LocationEntityType, List<ServiceLayer>> loadCategoryRules(
-			Map<Integer, ServiceLayer> serviceLayerMap) {
-		Map<Object, List<Object[]>> objectMap = serviceLayerRepository
-				.queryMappedCategories().stream()
-				.collect(Collectors.groupingBy(a -> a[1]));
-
-		Map<LocationEntityType, List<ServiceLayer>> result = new EnumMap<>(
-				LocationEntityType.class);
-
-		objectMap.entrySet().forEach(
-				e -> {
-
-					LocationEntityType type = LocationEntityTypeMapping.MAPPING
-							.toLocationEntityType(((Number) e.getKey())
-									.intValue());
-
-					List<ServiceLayer> serviceLayers = e.getValue().stream()
-							.map(a -> ((Number) a[0]).intValue())
-							.map(serviceLayerMap::get)
-							.collect(Collectors.toList());
-
-					result.put(type, serviceLayers);
-
-				});
-
-		return result;
-
-	}
-
-	private Map<ServiceLayer, Integer> loadPrioityMap(
-			Map<Integer, ServiceLayer> serviceLayerMap) {
-
-		Map<ServiceLayer, Integer> result = new HashMap<>();
-
-		serviceLayerRepository.queryMappedPriorities().forEach(
-				a -> {
-					result.put(serviceLayerMap.get(((Number) a[0]).intValue()),
-							((Number) a[1]).intValue());
-				});
-
-		return result;
-	}
-
-	@Override
-	public Collection<ServiceLayer> getServiceLayers(
-			Collection<Integer> serviceLayersIds) {
-		return StreamUtil.map(serviceLayersIds, serviceLayerMap::get);
+		systemRuleRef = new VolatileReference<>(this::loadSystemRule,
+				1000L * 60L * 5L);
 	}
 
 	@Override
 	public Collection<ServiceLayer> inferServiceLayers(
 			Collection<LocationEntityType> locationEntityTypes) {
+		return systemRuleRef.get().inferServiceLayers(locationEntityTypes);
+	}
 
-		Set<ServiceLayer> results = new HashSet<>();
-		locationEntityTypes.stream().forEach(e -> {
-			List<ServiceLayer> serviceLayers = entityMap.get(e);
-			if (e != null) {
-				results.addAll(serviceLayers);
-			}
-		});
+	@Override
+	public Collection<ServiceLayer> getServiceLayers(
+			Collection<Integer> serviceLayersIds) {
+		return systemRuleRef.get().getServiceLayers(serviceLayersIds);
+	}
 
-		List<ServiceLayer> layers = new ArrayList<>(results);
-		Collections.sort(layers, serviceLayerComparator);
+	private class SystemRule implements ProcessingLayerService {
 
-		return layers;
+		private Map<Integer, ServiceLayer> serviceLayerMap;
+		private LinkedHashMap<ServiceLayer, Set<LocationEntityType>> layerAssignmentMap;
+
+		public SystemRule construct() {
+			serviceLayerMap = StreamUtil.hash(serviceLayerRepository.findAll(),
+					ServiceLayer::getId);
+
+			layerAssignmentMap = createLayerAssignmentMap(
+					createOrderedLayers(serviceLayerMap.values(),
+							createComparator(loadPrioityMap(serviceLayerMap))),
+					loadCategoryRules(serviceLayerMap));
+
+			return this;
+		}
+
+		private LinkedHashMap<ServiceLayer, Set<LocationEntityType>> createLayerAssignmentMap(
+				Collection<ServiceLayer> orderedLayers,
+				Map<ServiceLayer, Set<LocationEntityType>> map) {
+			LinkedHashMap<ServiceLayer, Set<LocationEntityType>> result = new LinkedHashMap<>();
+
+			orderedLayers.forEach(l -> {
+				result.put(l, map.get(l));
+			});
+
+			return result;
+		}
+
+		private Collection<ServiceLayer> createOrderedLayers(
+				Collection<ServiceLayer> layers,
+				Comparator<ServiceLayer> serviceLayerComparator) {
+			List<ServiceLayer> result = new ArrayList<>();
+			result.addAll(layers);
+			Collections.sort(result, serviceLayerComparator);
+			return result;
+		}
+
+		private Comparator<ServiceLayer> createComparator(
+				Map<ServiceLayer, Integer> priorityMap) {
+			return (s1, s2) -> priorityMap.get(s2).compareTo(
+					priorityMap.get(s1));
+		}
+
+		private Map<ServiceLayer, Set<LocationEntityType>> loadCategoryRules(
+				Map<Integer, ServiceLayer> serviceLayerMap) {
+
+			Map<Object, List<Object[]>> objectMap = serviceLayerRepository
+					.queryMappedCategories().stream()
+					.collect(Collectors.groupingBy(a -> a[0]));
+
+			Map<ServiceLayer, Set<LocationEntityType>> result = new HashMap<>();
+
+			objectMap
+					.entrySet()
+					.forEach(
+							e -> {
+
+								ServiceLayer sl = serviceLayerMap
+										.get(((Number) e.getKey()).intValue());
+
+								Set<LocationEntityType> entityTypes = e
+										.getValue()
+										.stream()
+										.map(a -> ((Number) a[1]).intValue())
+										.map(LocationEntityTypeMapping.MAPPING::toLocationEntityType)
+										.collect(Collectors.toSet());
+
+								// Speed and size
+								Set<LocationEntityType> enumEntityTypes = EnumSet
+										.noneOf(LocationEntityType.class);
+								enumEntityTypes.addAll(entityTypes);
+
+								result.put(sl, enumEntityTypes);
+
+							});
+
+			return result;
+
+		}
+
+		private Map<ServiceLayer, Integer> loadPrioityMap(
+				Map<Integer, ServiceLayer> serviceLayerMap) {
+
+			Map<ServiceLayer, Integer> result = new HashMap<>();
+
+			serviceLayerRepository.queryMappedPriorities()
+					.forEach(
+							a -> {
+								result.put(serviceLayerMap.get(((Number) a[0])
+										.intValue()), ((Number) a[1])
+										.intValue());
+							});
+
+			return result;
+		}
+
+		@Override
+		public Collection<ServiceLayer> getServiceLayers(
+				Collection<Integer> serviceLayersIds) {
+			return StreamUtil.map(serviceLayersIds, serviceLayerMap::get);
+		}
+
+		@Override
+		public Collection<ServiceLayer> inferServiceLayers(
+				Collection<LocationEntityType> locationEntityTypes) {
+
+			Set<LocationEntityType> set = EnumSet
+					.noneOf(LocationEntityType.class);
+			set.addAll(locationEntityTypes);
+
+			final LinkedHashSet<ServiceLayer> selectedLayers = new LinkedHashSet<>();
+			layerAssignmentMap.entrySet().forEach(e -> {
+				
+				Set<LocationEntityType> matchSet = e.getValue() ;
+				
+				if( matchSet != null ) {
+					matchSet.forEach(entity -> {
+						if (set.contains(entity)) {
+							set.remove(entity);
+							selectedLayers.add(e.getKey());
+						}
+					});
+				}
+
+			});
+
+			return selectedLayers;
+		}
+
 	}
 
 }
