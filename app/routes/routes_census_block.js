1f992d937dd9dfb21824da499528e1e17611ffc4
Alberto Gimeno <gimenete@gmail.com>
2016-08-30 19:27:38 +0200

    Fix clicking on a census_block and changes in customers

--- app/routes/routes_census_block.js
+++ app/routes/routes_census_block.js
@@ -3,6 +3,13 @@ var models = require('../models')
 exports.configure = (api, middleware) => {
   var jsonSuccess = middleware.jsonSuccess
 
+  api.get('/census_blocks/:id/info', (request, response, next) => {
+    var id = request.params.id
+    models.CensusBlock.findCarriers(id)
+      .then(jsonSuccess(response, next))
+      .catch(next)
+  })
+
   api.get('/census_blocks/:statefp/:countyfp', middleware.viewport, (request, response, next) => {
     var statefp = request.params.statefp
     var countyfp = request.params.countyfp
@@ -15,13 +22,6 @@ exports.configure = (api, middleware) => {
   api.get('/census_blocks/all', middleware.viewport, (request, response, next) => {
     var viewport = request.viewport
     models.CensusBlock.findAllNbmCarriers(viewport)
-      .then(jsonSuccess(response, next))
-      .catch(next)
-  })
-
-  api.get('/census_blocks/:id/info', (request, response, next) => {
-    var id = request.params.id
-    models.CensusBlock.findCarriers(id)
       .then(jsonSuccess(response, next))
       .catch(next)
   })
