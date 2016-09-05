7dd2bf9a5c54f9d59b217b28d1a31db166be3756
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-05 17:55:46 -0500

    debug crazy bug

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/optimization/impl/PlanCommandExecutorServiceImpl.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/optimization/impl/PlanCommandExecutorServiceImpl.java
@@ -183,8 +183,12 @@ public class PlanCommandExecutorServiceImpl implements PlanCommandService {
 	public OptimizedPlan reifyPlanSummarizeAndSave(
 			OptimizationConstraints constraints, PlannedNetwork plan) {
 
+		log.info("Summarize Plan " + plan.getPlanId());
 		OptimizedPlan optimizedPlan = summarize(reifyPlan(constraints, plan));
+		log.info("Summarized Plan " + plan.getPlanId());
 		save(optimizedPlan);
+		log.info("Saved Plan " + plan.getPlanId());
+		
 		return optimizedPlan;
 
 	}
--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/optimization/wirecenter/impl/WirecenterPlanningServiceImpl.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/optimization/wirecenter/impl/WirecenterPlanningServiceImpl.java
@@ -1,5 +1,8 @@
 package com.altvil.aro.service.optimization.wirecenter.impl;
 
+import org.eclipse.jetty.util.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +10,17 @@ import com.altvil.aro.persistence.repository.FiberRouteRepository;
 import com.altvil.aro.persistence.repository.NetworkNodeRepository;
 import com.altvil.aro.service.optimization.OptimizedPlan;
 import com.altvil.aro.service.optimization.wirecenter.WirecenterPlanningService;
+import com.altvil.aro.service.plan.impl.PlanServiceImpl;
 import com.altvil.aro.service.report.GeneratedPlan;
 import com.altvil.aro.service.report.NetworkReportService;
 import com.altvil.aro.service.report.PlanAnalysisReportService;
 
 @Service
 public class WirecenterPlanningServiceImpl implements WirecenterPlanningService {
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(WirecenterPlanningServiceImpl.class.getName());
+
 
 	private NetworkNodeRepository networkNodeRepository;
 	private FiberRouteRepository fiberRouteRepository;
@@ -42,10 +50,20 @@ public class WirecenterPlanningServiceImpl implements WirecenterPlanningService 
 
 	@Override
 	public void save(GeneratedPlan plan) {
+		
+		log.info("Equipment save .... " + plan.getWirecenterNetworkPlan().getPlanId()) ;
+		
 		networkNodeRepository.save(plan.getWirecenterNetworkPlan()
 				.getNetworkNodes());
+		
+		log.info("Equipment  saved .... " + plan.getWirecenterNetworkPlan().getPlanId()) ;
+		
 		fiberRouteRepository.save(plan.getWirecenterNetworkPlan()
 				.getFiberRoutes());
+		
+		log.info("Fiber saved .... " + plan.getWirecenterNetworkPlan().getPlanId()) ;
+		
+		
 
 	}
 
