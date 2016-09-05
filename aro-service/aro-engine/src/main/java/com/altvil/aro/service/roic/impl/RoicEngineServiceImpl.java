6321dd7bb2c457e77ad121fa7ad027656733e5e2
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-05 18:35:32 -0500

    remove import

--- aro-service/aro-engine/src/main/java/com/altvil/aro/service/roic/impl/RoicEngineServiceImpl.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/service/roic/impl/RoicEngineServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.jetty.util.log.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
