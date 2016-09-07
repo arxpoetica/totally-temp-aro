dee302992f05a95caa16a230bcbd54c7aedfa693
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-07 12:53:43 -0500

    revert edge wight util

--- aro-service/aro-core/src/main/java/com/altvil/aro/service/graph/EdgeUtils.java
+++ aro-service/aro-core/src/main/java/com/altvil/aro/service/graph/EdgeUtils.java
@@ -5,7 +5,7 @@ import com.altvil.aro.service.graph.segment.GeoSegment;
 public class EdgeUtils {
 	
 	public static double getLength(AroEdge<GeoSegment> edge) {
-		return edge.getValue().getLength() ;
+		return edge.getWeight() ;
 	}
 	
 }
