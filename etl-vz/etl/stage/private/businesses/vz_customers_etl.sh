15c21aa94b8694298f1be806038d5d7d0d90d601
Michael Maloney <mmaloney@altvil.com>
2016-09-10 22:59:18 -0400

    ETL for all customers in one source file

--- etl-vz/etl/stage/private/businesses/vz_customers_etl.sh
+++ etl-vz/etl/stage/private/businesses/vz_customers_etl.sh
@@ -10,15 +10,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the scrip
 
 ${PSQL} -a -f $DIR/create_vz_customers.sql
 
-# Use lower case state names. FIPS codes unnecessary here as well.
-declare -a STATE_ARRAY=( 'mo' 'wa' )
-
 cd $GISROOT;
 
-for STATE in "${STATE_ARRAY[@]}"
-do
-	rm -f ${TMPDIR}/*.*
-	aws s3 cp s3://public.aro/proto/businesses/customers_${STATE}.zip $GISROOT/customers_${STATE}.zip
-	$UNZIPTOOL customers_${STATE}.zip -d ${TMPDIR}
-	cat /$TMPDIR/customers_${STATE}.csv | ${PSQL} -a -c "COPY businesses.vz_customers FROM STDIN DELIMITER ',' CSV HEADER;"
-done
+rm -f ${TMPDIR}/*.*
+aws s3 cp s3://public.aro/proto/businesses/vz_customers.zip $GISROOT/vz_customers.zip
+$UNZIPTOOL vz_customers.zip -d ${TMPDIR}
+cat /$TMPDIR/vz_customers.csv | ${PSQL} -a -c "COPY businesses.vz_customers FROM STDIN DELIMITER ',' CSV HEADER;"
