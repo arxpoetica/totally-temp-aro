32ab21fe8b04e2f5eb5111575ff68cac3f9a1936
Michael Maloney <mmaloney@altvil.com>
2016-09-12 23:16:35 -0400

    Only load WI CRANs to limit business location loads

--- etl-vz/etl/stage/private/boundaries/create_cran_boundaries.sql
+++ etl-vz/etl/stage/private/boundaries/create_cran_boundaries.sql
@@ -9,22 +9,6 @@ CREATE TABLE boundaries.cran
 
 SELECT AddGeometryColumn('boundaries', 'cran', 'the_geom', 4326, 'MULTIPOLYGON', 2);
 
-
--- Load MO CRANs
-INSERT INTO boundaries.cran(gid, name, the_geom)
-	SELECT
-		gid,
-		name,
-		the_geom
-	FROM boundaries.cran_mo;
-
-INSERT INTO boundaries.cran(gid, name, the_geom)
-	SELECT
-		gid,
-		gid::varchar,
-		the_geom
-	FROM boundaries.cran_wa_v2;
-
 INSERT INTO boundaries.cran(gid, name, the_geom)
 	SELECT
 		gid,
