afbd40a8fadd8f3586f2d58d406303d4da8056d9
Michael Maloney <mmaloney@altvil.com>
2016-09-12 22:54:19 -0400

    Failsafe...

--- etl-vz/etl/stage/private/businesses/reset.sh
+++ etl-vz/etl/stage/private/businesses/reset.sh
@@ -3,5 +3,6 @@
 export PGBIN=/usr/bin
 PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
 
-${PSQL} -c "DROP SCHEMA IF EXISTS businesses CASCADE;"
-${PSQL} -c "CREATE SCHEMA businesses;"
+# TEMPORARY SAFETY MEASURE
+# ${PSQL} -c "DROP SCHEMA IF EXISTS businesses CASCADE;"
+# ${PSQL} -c "CREATE SCHEMA businesses;"
