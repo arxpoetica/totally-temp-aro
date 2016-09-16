9b44a4f5355948528b063bfd52b7f8b6e5c74687
soccki <soccki@gmail.com>
2016-09-16 18:19:26 +0200

    added cache_etl

---
+++ etl-vz/etl/schema/cache/cache_etl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
+
+# Create auth.users table
+${PSQL} -a -f $DIR/create_cache_entries.sql  
+
+# Create auth.permissions table
+${PSQL} -a -f $DIR/create_service_area_versions.sql
