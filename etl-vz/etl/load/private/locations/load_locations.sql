88fc8d6cd07fdb6c24b485cff4aa3ae930840766
Michael Maloney <mmaloney@altvil.com>
2016-09-20 00:34:32 -0400

    Let's try not dropping the tables after we load them with hundreds of thousands of rows...

--- etl-vz/etl/load/private/locations/load_locations.sql
+++ etl-vz/etl/load/private/locations/load_locations.sql
@@ -20,8 +20,8 @@ BEGIN
   scoped_name := parent_schema || '.' || parent_table_name;
   
   
-  EXECUTE 'DROP TABLE IF EXISTS ' || table_name;
-  EXECUTE 'CREATE TABLE ' || table_name || ' (CHECK (state = ''' || state_name || ''' OR state = ''' || state_abbrev || ''')) INHERITS (' || scoped_name || ');';
+  --EXECUTE 'DROP TABLE IF EXISTS ' || table_name;
+  EXECUTE 'CREATE TABLE IF NOT EXISTS' || table_name || ' (CHECK (state = ''' || state_name || ''' OR state = ''' || state_abbrev || ''')) INHERITS (' || scoped_name || ');';
   EXECUTE 'CREATE INDEX ' || index_prefix_name ||  '_geog_gist ON ' || table_name || ' USING gist (geog);';
   EXECUTE 'CREATE INDEX ' || index_prefix_name ||  '_geom_gist ON ' || table_name || ' USING gist (geom);';
   EXECUTE 'CREATE INDEX ' || index_prefix_name ||  '_total_businesses_index ON ' || table_name || '(total_businesses);';
