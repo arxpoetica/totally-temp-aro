18a9d73e93e61bb56f95c38faa47ba4bf89f3c2f
Michael Maloney <mmaloney@altvil.com>
2016-09-19 22:04:05 -0400

    DIR me, I've forgotten

--- etl-vz/etl/stage/reference/businesses/reset.sh
+++ etl-vz/etl/stage/reference/businesses/reset.sh
@@ -2,6 +2,7 @@
 
 export PGBIN=/usr/bin
 PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ${PSQL} -c "DROP SCHEMA IF EXISTS ref_businesses CASCADE;"
 ${PSQL} -c "CREATE SCHEMA ref_businesses;"
