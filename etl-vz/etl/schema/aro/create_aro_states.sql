7ca49a8dcb314fbf896689238e00d1e18819100a
Alberto Gimeno <gimenete@gmail.com>
2016-09-21 18:32:02 +0200

    Drop index if exists after creating it

--- etl-vz/etl/schema/aro/create_aro_states.sql
+++ etl-vz/etl/schema/aro/create_aro_states.sql
@@ -10,4 +10,5 @@ CREATE TABLE aro.states (
 
 SELECT AddGeometryColumn('aro', 'states', 'geom', 4326, 'MULTIPOLYGON', 2);
 
+DROP INDEX IF EXISTS idx_aro_edges_geom_gist;
 CREATE INDEX idx_aro_edges_geom_gist ON aro.edges USING gist(geom);
