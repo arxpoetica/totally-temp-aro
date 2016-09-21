95634c1b3b0802e8cedcad87b17cf3c0d951cfd0
Alberto Gimeno <gimenete@gmail.com>
2016-09-21 12:12:50 +0200

    Testing new locations query

--- app/models/location.js
+++ app/models/location.js
@@ -27,7 +27,23 @@ module.exports = class Location {
           END
         ) AS largest_type
       FROM locations
-      WHERE ARRAY[$1]::varchar[] && dn_entity_categories
+      WHERE ARRAY[$1]::text[] && (
+        array(
+          SELECT DISTINCT 'b_' || bc.name
+          FROM businesses b
+          JOIN client.business_category_mappings bcm ON b.id = bcm.business_id
+          JOIN client.business_categories bc ON bc.id = bcm.business_category_id
+          WHERE b.location_id = locations.id
+        )
+        ||
+        array(
+          SELECT DISTINCT 'h_' || hc.name
+          FROM households h
+          JOIN client.household_category_mappings hcm ON h.id = hcm.household_id
+          JOIN client.household_categories hc ON hc.id = hcm.household_category_id
+          WHERE h.location_id = locations.id
+        )
+      )
       ${database.intersects(viewport, 'geom', 'AND')}
 
       EXCEPT
