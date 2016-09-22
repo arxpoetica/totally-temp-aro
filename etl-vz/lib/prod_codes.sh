46c2dbacb7099e4e4c7725b8dfb2f07af51899dd
Michael Maloney <mmaloney@altvil.com>
2016-09-22 11:21:15 -0400

    add IL towers to prodcodes

--- etl-vz/lib/prod_codes.sh
+++ etl-vz/lib/prod_codes.sh
@@ -4,5 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ${DIR}/lookup_codes.sh
 
 export STATE_CODES='fl,il,mo,wa,wi' ;
-export TOWER_CODES='towers_seattle_wa,towers_columbus_oh,towers_mo,towers_wi' ;
+export TOWER_CODES='towers_seattle_wa,towers_columbus_oh,towers_mo,towers_wi,towers_il' ;
 export CRAN_CODES='mo,wa,wi';
