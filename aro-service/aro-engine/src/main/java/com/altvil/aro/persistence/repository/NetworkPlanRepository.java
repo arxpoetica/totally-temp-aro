9d786f765aedd0e8d0bbc71c86b75f150d412f92
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-22 17:02:17 -0500

    remove dependence on city_code

--- aro-service/aro-engine/src/main/java/com/altvil/aro/persistence/repository/NetworkPlanRepository.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/persistence/repository/NetworkPlanRepository.java
@@ -96,7 +96,7 @@ public interface NetworkPlanRepository extends
 			"  select l.id, l.block_id, e.entity_type, e.count, e.monthly_spend, l.competitor_strength\n" + 
 			"  from selected_locations l\n" + 
 			"  join client.business_summary e on e.location_id = l.id and  ((e.entity_type = 3 and monthly_recurring_cost>=:mrc) or e.entity_type !=3)\n" + 
-			"   where year = :year and city_id = 1\n" + 
+			"   where year = :year\n" + 
 			"),\n" + 
 			"hs as (\n" + 
 			"  select l.id, l.block_id, 4 as entity_type, e.count, e.count*60 as monthly_spend, l.competitor_strength\n" + 
