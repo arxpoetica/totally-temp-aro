50a66fbe6b664e4bef9de3fb151f13d797ff3745
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-22 12:13:10 -0500

    Fix aro-edges

--- etl-vz/etl/load/public/aro/reset.sh
+++ etl-vz/etl/load/public/aro/reset.sh
@@ -8,8 +8,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the scrip
 ${PSQL} -c "DROP SCHEMA IF EXISTS aro_edges_data CASCADE;"
 ${PSQL} -c "CREATE SCHEMA aro_edges_data;"
 
-${PSQL} -c "DROP SCHEMA IF EXISTS aro_edges_data CASCADE;"
-${PSQL} -c "CREATE SCHEMA aro_edges_data;"
-
 ${PSQL} -a -f $DIR/load_aro_edges.sql
 
