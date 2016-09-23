acb82c2fec64a9bd945f9a98e7f9096c9bdb348f
Michael Maloney <mmaloney@altvil.com>
2016-09-23 14:39:38 -0400

    Nope

--- etl-vz/etl/load/private/boundaries/reset.sh
+++ etl-vz/etl/load/private/boundaries/reset.sh
@@ -4,4 +4,5 @@ PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
 
+${PSQL} -a -f $DIR/reset_boundaries.sql
 
---
+++ etl-vz/etl/load/private/boundaries/reset_boundaries.sql
@@ -0,0 +1,2 @@
+DELETE FROM client.analysis_area CASCADE ;
+DELETE FROM client.service_area CASCADE ;
