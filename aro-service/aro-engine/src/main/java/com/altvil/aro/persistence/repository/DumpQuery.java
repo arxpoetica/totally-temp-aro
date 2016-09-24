58a4cc88322b3fcb89558f7e5509af8b1d307100
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-24 05:03:47 +0100

    another query

--- aro-service/aro-engine/src/main/java/com/altvil/aro/persistence/repository/DumpQuery.java
+++ aro-service/aro-engine/src/main/java/com/altvil/aro/persistence/repository/DumpQuery.java
@@ -17,35 +17,36 @@ public class DumpQuery {
 		}
 	}
 	
-	private static String query = "WITH selected_master AS (\n" + 
-			"	SELECT p.*\n" + 
-			"	FROM client.plan p\n" + 
-			"	WHERE p.id = :inputMasterPlan\n" + 
+	private static String query = "with selected_locations as (\n" + 
+			"select l.id, b.gid as block_id, case when c.strength is null then 0 else c.strength end as competitor_strength\n" + 
+			"   from client.service_area w \n" +
+			"	join aro.locations l on w.id = :serviceAreaId and st_contains(w.geom, l.geom) and l.state in :stateUSPS \n" +
+			"	join aro.census_blocks b on st_contains(b.geom, l.geom)\n" + 
+			"	left join client.summarized_competitors_strength c on c.location_id = l.id and c.entity_type = 3 and c.state in :stateUSPS \n" +
+			"),\n" + 
+			"bs as (\n" + 
+			"  select l.id, l.block_id, e.entity_type, e.count, e.monthly_spend, l.competitor_strength\n" + 
+			"  from selected_locations l\n" + 
+			"  join client.business_summary e on e.location_id = l.id and  ((e.entity_type = 3 and monthly_recurring_cost>=:mrc) or e.entity_type !=3) " +
+			"  and e.state in :stateUSPS" +
+			"   where year = :year\n" + 
+			"),\n" + 
+			"hs as (\n" + 
+			"  select l.id, l.block_id, 4 as entity_type, e.count, e.count*60 as monthly_spend, l.competitor_strength\n" + 
+			"  from selected_locations l\n" + 
+			"  join client.households_summary e on e.location_id = l.id \n" +
+			"),\n" + 
+			"ct as (\n" + 
+			"  select l.id, l.block_id, 5 as entity_type, e.count, e.count*500 as monthly_spend, l.competitor_strength\n" + 
+			"  from selected_locations l\n" + 
+			"  join client.celltower_summary cts on cts.location_id = l.id and cts.state in :stateUSPS \n" +
 			")\n" + 
-			",\n" + 
-			"all_fiber AS (\n" + 
-			"	SELECT\n" + 
-			"		id,\n" + 
-			"		ST_Union(f.geom) AS geom\n" + 
-			"	FROM (\n" + 
-			"	(SELECT mp.id, pc.geom\n" + 
-			"	FROM selected_master mp\n" + 
-			"	JOIN client.plan_fiber_conduit pc\n" + 
-			"		ON pc.plan_id = mp.id)\n" + 
-			"\n" + 
-			"		UNION\n" + 
-			"\n" + 
-			"	(SELECT mp.id, pc.geom\n" + 
-			"	FROM selected_master mp\n" + 
-			"	JOIN client.plan_fiber_conduit pc\n" + 
-			"		ON pc.plan_id = mp.id)\n" + 
-			"	) AS f\n" + 
-			"	GROUP BY id\n" + 
-			")\n" + 
-			"INSERT INTO client.plan_fiber_conduit\n" + 
-			"	(:planId, geom)\n" + 
-			"SELECT id, geom \n" + 
-			"FROM all_fiber";
+			"select * from  bs\n" + 
+			"UNION\n" + 
+			"select * from  hs\n" + 
+			"UNION\n" +
+			"select * from ct\n" +
+			"limit 200000";
 	
 	public static void value() {
 		System.out.println(query) ;
