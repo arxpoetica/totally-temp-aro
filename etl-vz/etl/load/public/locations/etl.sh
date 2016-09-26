2120813b8de62b1886a8759dbaf566b969a7e7dd
Michael Maloney <mmaloney@altvil.com>
2016-09-26 12:40:47 -0400

    Apply costs fix to public locations as well

--- etl-vz/etl/load/public/locations/etl.sh
+++ etl-vz/etl/load/public/locations/etl.sh
@@ -42,6 +42,10 @@ do
 	${PSQL} -a -c "SELECT aro.load_shard_infousa_businesses('${INFOUSA_SCOPED_SOURCE_TABLE}_${STATE}', '${TARGET_SCHEMA_NAME}', '${STATE}');"
 	#${PSQL} -a -c "SELECT aro.update_shard_industries('${INFOUSA_SCOPED_SOURCE_TABLE}_${STATE}', '${STATE}');"
 
+	# Fix problem where having NULL instead of 0 in costs columns causes issues.
+	${PSQL} -a -c "UPDATE aro.businesses SET monthly_recurring_cost = 0 WHERE monthly_recurring_cost IS NULL;"
+	${PSQL} -a -c "UPDATE aro.businesses SET annual_recurring_cost = 0 WHERE annual_recurring_cost IS NULL;"
+
 	${PSQL} -a -c "SELECT aro.create_towers_shard_table('${STATE}', '${TARGET_SCHEMA_NAME}');"
 	${PSQL} -a -c "SELECT aro.load_shard_towers('${TOWERS_SCOPED_SOURCE_TABLE}', '${TARGET_SCHEMA_NAME}', '${STATE}');"
 	
