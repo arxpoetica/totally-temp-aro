7cc053e046b28b5f29c6997d4bb73395138ab396
Michael Maloney <mmaloney@altvil.com>
2016-09-26 18:36:26 -0400

    Adding Indiana back to the state codes. Who could forget about the Buckeye state? Er... the Show Me State? Maybe we just leave it out...

--- etl-vz/lib/lookup_codes.sh
+++ etl-vz/lib/lookup_codes.sh
@@ -4,7 +4,7 @@ state_code_lookup() {
 	declare -n __result=$1
 	declare  state_code=$2
 
-	declare -A STATE_FIPS_ARRAY=( [wv]=54 [fl]=12 [il]=17 [mn]=27 [md]=24 [ri]=44 [id]=16 [nh]=33 [nc]=37 [vt]=50 [ct]=09 [de]=10 [nm]=35 [ca]=06 [nj]=34 [wi]=55 [or]=41 [ne]=31 [pa]=42 [wa]=53 [la]=22 [ga]=13 [al]=01 [ut]=49 [oh]=39 [tx]=48 [co]=08 [sc]=45 [ok]=40 [tn]=47 [wy]=56 [hi]=15 [nd]=38 [ky]=21 [mp]=69 [gu]=66 [me]=23 [ny]=36 [nv]=32 [ak]=02 [as]=60 [mi]=26 [ar]=05 [ms]=28 [mo]=29 [mt]=30 [ks]=20 [pr]=72 [sd]=46 [ma]=25 [va]=51 [dc]=11 [ia]=19 [az]=04 [vi]=78 ) ;
+	declare -A STATE_FIPS_ARRAY=( [in]=18 [wv]=54 [fl]=12 [il]=17 [mn]=27 [md]=24 [ri]=44 [id]=16 [nh]=33 [nc]=37 [vt]=50 [ct]=09 [de]=10 [nm]=35 [ca]=06 [nj]=34 [wi]=55 [or]=41 [ne]=31 [pa]=42 [wa]=53 [la]=22 [ga]=13 [al]=01 [ut]=49 [oh]=39 [tx]=48 [co]=08 [sc]=45 [ok]=40 [tn]=47 [wy]=56 [hi]=15 [nd]=38 [ky]=21 [mp]=69 [gu]=66 [me]=23 [ny]=36 [nv]=32 [ak]=02 [as]=60 [mi]=26 [ar]=05 [ms]=28 [mo]=29 [mt]=30 [ks]=20 [pr]=72 [sd]=46 [ma]=25 [va]=51 [dc]=11 [ia]=19 [az]=04 [vi]=78 ) ;
 
 	__result=${STATE_FIPS_ARRAY[$state_code]} ;
 	
