bf53393530b761d7eee5d27d3fb8785017825366
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-29 17:28:19 +0100

    add user_data schema + link to user_data from service_area

--- etl-vz/etl/schema/client/create_client_service_area.sql
+++ etl-vz/etl/schema/client/create_client_service_area.sql
@@ -6,11 +6,13 @@ CREATE TABLE client.service_area
 	service_type varchar(1),
 	service_layer_id int4 not null references client.service_layer, 
 	source_id varchar(64),
+	data_source_id bigint references user_data.data_source(id) on delete cascade,
 	state varchar(6),
 	code varchar(64),
 	geog geography('MULTIPOLYGON', 4326),
 	CONSTRAINT client_service_area_pkey PRIMARY KEY (id)
 );
+
 
 SELECT AddGeometryColumn('client', 'service_area', 'geom', 4326, 'MULTIPOLYGON', 2);
 SELECT AddGeometryColumn('client', 'service_area', 'edge_buffer', 4326, 'GEOMETRY', 2);
@@ -21,3 +23,4 @@ CREATE INDEX client_service_area_geom_gist ON client.service_area USING gist (ge
 CREATE INDEX client_service_area_geog_gist ON client.service_area USING gist (geog);
 CREATE INDEX client_service_area_edge_buffer ON client.service_area USING gist (edge_buffer);
 CREATE INDEX client_service_area_location_edge_buffer ON client.service_area USING gist (location_edge_buffer);
+CREATE INDEX client_service_area_data_source_id ON client.service_area (data_source_id);
