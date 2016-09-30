eec56c36396ef91fb91d318a51061804c4f387f5
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-09-30 15:59:47 +0100

    add multi part resolver

--- etl-vz/etl/schema/user_data/create_data_source.sql
+++ etl-vz/etl/schema/user_data/create_data_source.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_data.data_source(
 	id bigserial PRIMARY KEY,
 	user_id integer references auth.users(id),
-	Name varchar, 
-	Description varchar
+	name varchar, 
+	description varchar
 );
