97609d7289d43ca26cb8a13af209a6138ef92fab
Michael Maloney <mmaloney@altvil.com>
2016-10-03 22:54:56 -0400

    Rework NBM ETL for all 50 states, including partitions for blocks table and competitor speed category tables

--- etl-vz/etl/stage/reference/nbm/nbm_etl.sh
+++ etl-vz/etl/stage/reference/nbm/nbm_etl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e;
 
-
 PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
 GISROOT=/gisdata
 TMPDIR=/gisdata/temp/
@@ -10,24 +9,28 @@ export AWS_DEFAULT_REGION=us-east-1
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
 
-${PSQL} -a -f $DIR/create_nbm_blocks.sql
-
 # Use upper case state names. FIPS codes unnecessary here as well.
 IFS=',' read -a STATE_ARRAY <<< "${STATE_CODES}"
+TARGET_SCHEMA_NAME='nbm'
 
 cd $GISROOT;
 for STATE_CODE in "${STATE_ARRAY[@]}"
 do
-	STATE="${STATE_CODE,,}"
+	STATE="${STATE_CODE^^}"
 	rm -f ${TMPDIR}/*.*
+
 	if [ ! -f $GISROOT/${STATE}-NBM-CBLOCK-CSV-JUN-2014.zip ]; then
-		aws s3 cp s3://public.aro/nbm/${STATE}-NBM-CBLOCK-CSV-JUN-2014.zip $GISROOT/${STATE}-NBM-CBLOCK-CSV-JUN-2014.zip
+		wget http://www.broadbandmap.gov/download/${STATE}-NBM-CSV-June-2014.zip --accept=zip --reject=html -nd -nc
+		$UNZIPTOOL ${STATE}-NBM-CSV-June-2014.zip -d ${TMPDIR}
+		${PSQL} -a -c "SELECT create_nbm_blocks_table('${STATE}', '${TARGET_SCHEMA_NAME}');"
+		$UNZIPTOOL -p ${TMPDIR}/${STATE}-NBM-CSV-June-2014/${STATE}-NBM-CBLOCK-CSV-JUN-2014.zip | ${PSQL} -a -c "COPY ${TARGET_SCHEMA_NAME}.blocks_${STATE} FROM STDIN DELIMITER '|' CSV HEADER;"
+		${PSQL} -a -c "SELECT create_nbm_blocks_indexes('${STATE}', '${TARGET_SCHEMA_NAME}');"
+		${PSQL} -a -c "SELECT create_competitor_speed_category_partition('${STATE}', '${TARGET_SCHEMA_NAME}');"
+		${PSQL} -a -c "SELECT load_competitor_speed_category_partition('${STATE}', '${TARGET_SCHEMA_NAME}');"
+		rm -rf ${TMPDIR}/${STATE}-NBM-CSV-June-2014
 	fi
-	$UNZIPTOOL -p ${STATE}-NBM-CBLOCK-CSV-JUN-2014.zip | ${PSQL} -a -c "COPY nbm.blocks FROM STDIN DELIMITER '|' CSV HEADER;" 
 done
 
-${PSQL} -a -f $DIR/optimize_nbm_blocks.sql
-${PSQL} -a -f $DIR/competitor_speed_category.sql
 ${PSQL} -a -f $DIR/brand_strength.sql
 
 
--- etl-vz/etl/stage/reference/nbm/reset.sh
+++ etl-vz/etl/stage/reference/nbm/reset.sh
@@ -5,5 +5,13 @@ set -e;
 export PGBIN=/usr/bin
 PSQL="${PGBIN}/psql -v ON_ERROR_STOP=1"
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # gets directory the script is running from
+
 ${PSQL} -c "DROP SCHEMA IF EXISTS nbm CASCADE;"
 ${PSQL} -c "CREATE SCHEMA nbm;"
+
+# Load functions for NBM table operations
+${PSQL} -a -f $DIR/create_nbm_blocks.sql
+
+# Create this master table (this is schema...) using a new function created above
+${PSQL} -a -c "SELECT create_competitor_speed_category_master_table('nbm');"
