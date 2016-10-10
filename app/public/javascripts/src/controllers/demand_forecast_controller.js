9edc5ce0c9edb7ad5259366e2cd08149e38923e2
Alberto Gimeno <gimenete@gmail.com>
2016-10-10 16:04:48 +0200

    Hide Demand Forecast tab

--- app/public/javascripts/src/controllers/demand_forecast_controller.js
+++
@@ -1,6 +0,0 @@
-/* global app */
-app.controller('demand-forecast-controller', ['$scope', '$rootScope', '$http', 'map_tools', ($scope, $rootScope, $http, map_tools) => {
-  $scope.map_tools = map_tools
-  $scope.growth = 0
-  $scope.duration = 5
-}])
