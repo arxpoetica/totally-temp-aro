c12cb07e3245ccf77bc7b9457c5cf799f5b11035
Alberto Gimeno <gimenete@gmail.com>
2016-10-18 13:38:03 +0200

    User can see optimization statistics for the optimization they are running

--- app/public/javascripts/src/interceptors/saving_plan_handler.js
+++ app/public/javascripts/src/interceptors/saving_plan_handler.js
@@ -1,18 +1,35 @@
 /* global app $ */
 app.config(($httpProvider) => {
   var operations = 0
+  var time
+  var interval
 
   function updateOperations (n) {
     operations += n
     if (operations > 0) {
+      time = time || Date.now()
       $('#plan-saved').stop().hide()
       $('#plan-saving').stop().show()
+      $('#plan-saving .fa').show()
+      if (!interval) {
+        $('#plan-saving .text').text('00:00')
+        interval = setInterval(() => {
+          var diff = (Date.now() - time) / 1000
+          var min = Math.floor(diff / 60)
+          var sec = Math.ceil(diff % 60)
+          $('#plan-saving .text').text(`${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`)
+        }, 1000)
+      }
     } else {
-      $('#plan-saving').stop().hide()
-      $('#plan-saved').stop().show()
-      setTimeout(() => {
-        $('#plan-saved').fadeOut()
-      }, 500)
+      $('#plan-saving').stop()
+      $('#plan-saving .fa').hide()
+      time = null
+      clearInterval(interval)
+      // $('#plan-saving').stop().hide()
+      // $('#plan-saved').stop().show()
+      // setTimeout(() => {
+      //   $('#plan-saved').fadeOut()
+      // }, 500)
     }
   }
 
