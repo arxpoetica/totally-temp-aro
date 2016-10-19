2e61332cd8e6d57ee61efaf2ba3ff50a4fac3fd2
soccki <soccki@gmail.com>
2016-10-19 16:40:32 +0200

    fixed reference.data_source table definition

--- etl-vz/etl/schema/reference/create_data_source.sql
+++ etl-vz/etl/schema/reference/create_data_source.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS client.data_source
+
+CREATE TABLE IF NOT EXISTS reference.data_source
 (
-  id serial,
+  id serial primary key,
   name character varying,
   description character varying,
   is_user_defined boolean default true,
   created_at timestamp default now()
 );
 
-INSERT INTO client.data_source(name, description, is_user_defined)
+INSERT INTO reference.data_source(name, description, is_user_defined)
 	 VALUES('system', 'System', false);
 
