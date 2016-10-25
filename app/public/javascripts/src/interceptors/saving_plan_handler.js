ae0744b37fc94cd87c7011d5ec0a7b623fb7bacc
Alberto Gimeno <gimenete@gmail.com>
2016-10-25 15:33:58 +0200

    Improved progress bar

--- app/public/javascripts/src/interceptors/saving_plan_handler.js
+++ app/public/javascripts/src/interceptors/saving_plan_handler.js
@@ -3,6 +3,12 @@ app.config(($httpProvider) => {
   var operations = 0
   var time
   var interval
+  var wait = 0
+
+  var n = document.location.search.indexOf('t=')
+  if (n > 0) {
+    wait = +document.location.search.substring(n + 2) || wait
+  }
 
   function updateOperations (n) {
     operations += n
@@ -13,28 +19,29 @@ app.config(($httpProvider) => {
       $('#plan-saving-progress').show()
       $('#plan-saving .fa').show()
       if (!interval) {
-        $('#plan-saving-progress .progress-bar').css('width', '0%').stop()
-        $('#plan-saving .text').text('00:00 Runtime')
+        $('#plan-saving-progress .progress-bar').css('width', '0%').stop().text('00:00 Runtime')
+          .addClass('progress-bar-striped')
+          .removeClass('progress-bar-success')
         interval = setInterval(() => {
           var diff = (Date.now() - time) / 1000
           var min = Math.floor(diff / 60)
           var sec = Math.ceil(diff % 60)
-          $('#plan-saving .text').text(`${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec} Runtime`)
           var per = Math.min(99, Math.ceil(Math.log(diff * 5) * 10))
-          $('#plan-saving-progress .progress-bar').css('width', per + '%')
+          $('#plan-saving-progress .progress-bar').css('width', per + '%').text(`${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec} Runtime`)
         }, 200)
       }
     } else {
-      $('#plan-saving').stop()
-      $('#plan-saving .fa').hide()
-      $('#plan-saving-progress .progress-bar').css('width', '100%')
       setTimeout(() => {
-        $('#plan-saving-progress').hide()
-        $('#plan-saving-progress .progress-bar').css('width', '0%').stop()
-      }, 600)
-      time = null
-      clearInterval(interval)
-      interval = null
+        $('#plan-saving').stop()
+        $('#plan-saving .fa').hide()
+        $('#plan-saving-progress .progress-bar')
+          .css('width', '100%')
+          .removeClass('progress-bar-striped')
+          .addClass('progress-bar-success')
+        time = null
+        clearInterval(interval)
+        interval = null
+      }, wait)
     }
   }
 
