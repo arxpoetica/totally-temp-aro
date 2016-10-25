a6e10aa8fed18832243cd34dcf8cced8fa884069
Alberto Gimeno <gimenete@gmail.com>
2016-10-25 13:06:54 +0200

    Progress bar

--- app/public/javascripts/src/interceptors/saving_plan_handler.js
+++ app/public/javascripts/src/interceptors/saving_plan_handler.js
@@ -10,19 +10,28 @@ app.config(($httpProvider) => {
       time = time || Date.now()
       $('#plan-saved').stop().hide()
       $('#plan-saving').stop().show()
+      $('#plan-saving-progress').show()
       $('#plan-saving .fa').show()
       if (!interval) {
+        $('#plan-saving-progress .progress-bar').css('width', '0%').stop()
         $('#plan-saving .text').text('00:00 Runtime')
         interval = setInterval(() => {
           var diff = (Date.now() - time) / 1000
           var min = Math.floor(diff / 60)
           var sec = Math.ceil(diff % 60)
           $('#plan-saving .text').text(`${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec} Runtime`)
+          var per = Math.min(99, Math.ceil(Math.log(diff * 5) * 10))
+          $('#plan-saving-progress .progress-bar').css('width', per + '%')
         }, 200)
       }
     } else {
       $('#plan-saving').stop()
       $('#plan-saving .fa').hide()
+      $('#plan-saving-progress').hide()
+      $('#plan-saving-progress .progress-bar').css('width', '100%')
+      setTimeout(() => {
+        $('#plan-saving-progress .progress-bar').css('width', '0%').stop().hide()
+      }, 400)
       time = null
       clearInterval(interval)
       interval = null
