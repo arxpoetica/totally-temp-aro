b21c3ff1bbab8e7c0d23e868d5d4ec3f71fa36ad
Michael Maloney <mmaloney@altvil.com>
2016-10-25 21:18:49 -0400

    Add conduit to fiber table

--- etl-vz/etl/stage/private/network_equipment/create_conduit.sql
+++ etl-vz/etl/stage/private/network_equipment/create_conduit.sql
@@ -1,24 +1,20 @@
-DROP TABLE IF EXISTS network_equipment.conduit;
-
-CREATE TABLE network_equipment.conduit
-(
-	id serial,
-	CONSTRAINT network_equipment_conduit_pkey PRIMARY KEY (id)
-);
-
-SELECT AddGeometryColumn('network_equipment', 'conduit', 'geom', 4326, 'MULTILINESTRING', 2);
-
-INSERT INTO network_equipment.conduit(geom)
+INSERT INTO network_equipment.fiber(source, is_partner_carrier, geom)
 	SELECT
+		'vzb_conduit',
+		false,
 		the_geom
 	FROM network_equipment.vzb_conduit_1;
 
-INSERT INTO network_equipment.conduit(geom)
+INSERT INTO network_equipment.fiber(source, is_partner_carrier, geom)
 	SELECT
+		'vzb_conduit',
+		false,
 		the_geom
 	FROM network_equipment.vzb_conduit_2;
 
-INSERT INTO network_equipment.conduit(geom)
+INSERT INTO network_equipment.fiber(source, is_partner_carrier, geom)
 	SELECT
+		'vzb_conduit',
+		false,
 		the_geom
 	FROM network_equipment.vzb_conduit_3;
--- etl-vz/etl/stage/private/network_equipment/create_fiber.sql
+++ etl-vz/etl/stage/private/network_equipment/create_fiber.sql
@@ -4,20 +4,20 @@ CREATE TABLE network_equipment.fiber
 (
 	id serial,
 	source varchar,
-	is_partner_provider boolean,
+	is_partner_carrier boolean,
 	CONSTRAINT network_equipment_fiber_pkey PRIMARY KEY (id)
 );
 
 SELECT AddGeometryColumn('network_equipment', 'fiber', 'geom', 4326, 'MULTILINESTRING', 2);
 
-INSERT INTO network_equipment.fiber(source, is_partner_provider, geom)
+INSERT INTO network_equipment.fiber(source, is_partner_carrier, geom)
 	SELECT
 		'vzb_fiber',
 		false,
 		the_geom
 	FROM network_equipment.vzb_fiber_part1;
 
-INSERT INTO network_equipment.fiber(source, is_partner_provider, geom)
+INSERT INTO network_equipment.fiber(source, is_partner_carrier, geom)
 	SELECT
 		'vzb_fiber',
 		false,
