798d5ac198f4f5780793aad4561390a847075d63
soccki <soccki@gmail.com>
2016-10-28 16:13:36 +0200

    fixed geom issue

--- aro-service/aro-utils/src/main/java/com/altvil/utils/BufferedGeographyMatcher.java
+++ aro-service/aro-utils/src/main/java/com/altvil/utils/BufferedGeographyMatcher.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.prep.PreparedGeometry;
 import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
 
@@ -17,9 +19,23 @@ public class BufferedGeographyMatcher {
         Collection<T> geometriesFiltered = geometries.stream()
                 .filter(geometry -> ! geometry.isEmpty())
                 .collect(Collectors.toList());
+
         Point centroid = GeometryUtil.factory().buildGeometry(geometriesFiltered).getCentroid();
         Collection<T> geographies = GeometryUtil.transformGeometriesToGeographies(geometriesFiltered, centroid);
-        List<Geometry> bufferedGeographies = StreamUtil.map(geographies, geometry -> geometry.buffer(bufferDistance));
+        List<Geometry> bufferedGeographies = geographies.stream()
+                .map(geometry -> geometry.buffer(bufferDistance))
+                .map(geometry -> {
+                            if (geometry instanceof MultiPolygon) {
+                                return geometry;
+                            } if (geometry instanceof Polygon) {
+                                return GeometryUtil.factory().createMultiPolygon(new Polygon[]{(Polygon) geometry});
+                            }
+                            return null;
+                        }
+                )
+                .filter(geometry -> geometry != null && ! geometry.isEmpty())
+                .collect(Collectors.toList());
+
         Collection<Geometry> bufferedGeometries = GeometryUtil.transformGeographiesToGeometries(bufferedGeographies, centroid);
         preparedGeometry = PreparedGeometryFactory.prepare(GeometryUtil.factory().buildGeometry(bufferedGeometries));
 
