3f0b6bcd050e99ae141a88ad4707070b26b4bd6d
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-10-28 12:46:10 +0100

    Fixes to network_plan_data

--- etl-vz/etl/schema/client/create_client_network_plan_data.sql
+++ etl-vz/etl/schema/client/create_client_network_plan_data.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS client.network_plan_data;
 CREATE TABLE client.network_plan_data
 (
-  id bigint  NOT NULL,
-  key character varying NOT NULL,
-  network_plan_id bigint references client.plan(id) on delete cascade, 
-  geom geometry,
-  data_field character varying,
-  PRIMARY KEY(id, key)
+ network_plan_id bigint references client.plan(id) on delete cascade, 
+ data_key character varying NOT NULL,
+ geom geometry,
+ data_field character varying,
+ PRIMARY KEY(network_plan_id, data_key)
 );
