ea9fff37e28cc6c79a7078a15984063279f7622a
HarryTeunissen <HarryTeunissen@yahoo.com>
2016-10-28 13:34:00 +0100

    updates to plan_location_link to speed up data saving

--- etl-vz/etl/schema/client/create_client_plan_location_link.sql
+++ etl-vz/etl/schema/client/create_client_plan_location_link.sql
@@ -1,7 +1,13 @@
 DROP TABLE IF EXISTS client.plan_location_link;
+CREATE SEQUENCE client.plan_location_link_id_seq
+ INCREMENT 1000
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 1
+ CACHE 1;
 CREATE TABLE client.plan_location_link
 (
-    id bigserial,
+    id bigint,
     plan_id bigint references client.plan(id) on delete cascade, 
     location_id bigint,
     state character varying,
@@ -13,5 +19,6 @@ CREATE TABLE client.plan_location_link
     total_revenue double precision,
     monthly_revenue double precision,
     penetration double precision,
-    fairshare_demand double precision
+    fairshare_demand double precision,
+    PRIMARY KEY(id)
 );
