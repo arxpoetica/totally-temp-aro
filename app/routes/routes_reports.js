141a5ecc68511c5b4bf20f61e119611d38775fa8
Alberto Gimeno <gimenete@gmail.com>
2016-10-31 20:09:35 +0100

    Yet another fix to TABC KML queries

--- app/routes/routes_reports.js
+++ app/routes/routes_reports.js
@@ -38,7 +38,7 @@ exports.configure = (api, middleware) => {
 
     var planQuery = `
       IN (
-        (SELECT p.id FROM client.plan p WHERE plan_type ${type} AND name='T' AND p.parent_plan_id IN (
+        (SELECT p.id FROM client.plan p WHERE name ${type} AND plan_type='G' AND p.parent_plan_id IN (
           (SELECT r.id FROM client.plan r WHERE r.parent_plan_id IN (
             SELECT id FROM client.plan WHERE parent_plan_id=$1
           ))
