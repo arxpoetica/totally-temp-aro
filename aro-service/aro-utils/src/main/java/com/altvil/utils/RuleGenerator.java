1520818f0272cb1990456dea2492dd886d5df09b
soccki <soccki@gmail.com>
2016-11-01 14:56:12 +0100

    fixed rule generator

--- aro-service/aro-utils/src/main/java/com/altvil/utils/RuleGenerator.java
+++ aro-service/aro-utils/src/main/java/com/altvil/utils/RuleGenerator.java
@@ -7,24 +7,24 @@ import com.altvil.utils.template.SimpleTemplate;
 
 public class RuleGenerator {
 
-	private List<String> states = Arrays.asList(new String[] { "WA" });
+	private List<String> states = Arrays.asList(new String[] { "WA", "FL","IL","MO","WI",});
 
-	private static String template = "CREATE RULE ${SRC_SCHEMA}_${PARENT_TBL}_${STATE_LOWER} AS ON INSERT TO ${SRC_SCHEMA}.${PARENT_TBL} WHERE (state = '${STATE}')\n"
+	private static String template = "CREATE RULE ${SRC_SCHEMA}_${PARENT_TBL}_${STATE_LOWER} AS ON INSERT TO ${SRC_SCHEMA}.${PARENT_TBL} WHERE (${STATE_COL} = '${STATE}')\n"
 			+ "    DO INSTEAD INSERT INTO ${PARTITION_TBL}_${STATE_LOWER} VALUES (NEW.*);";
 
 	
 	
 	public String locationRule() {
 		return process("aro", "locations",
-				"aro_location_data.locations") ;
+				"aro_location_data.locations", "state") ;
 	}
 	
 	public String towerRule() {
 		return process("aro", "towers",
-				"aro_location_data.towers") ;
+				"aro_location_data.towers", "parcel_state") ;
 	}
 
-	public String process(String srcSchema, String srcName, String targetName) {
+	public String process(String srcSchema, String srcName, String targetName, String stateCol) {
 		StringBuilder sb = new StringBuilder();
 		for (String state : states) {
 			String lowerCaseState = state.toLowerCase();
@@ -32,6 +32,7 @@ public class RuleGenerator {
 					.replace("SRC_SCHEMA", srcSchema)
 					.replace("PARENT_TBL", srcName)
 					.replace("PARTITION_TBL", targetName)
+					.replace("STATE_COL", stateCol)
 					.replace("STATE", state).replace("STATE_LOWER", lowerCaseState)
 					.getResult());
 			sb.append("\n");
@@ -42,6 +43,7 @@ public class RuleGenerator {
 	public static void main(String[] args) {
 
 		System.out.println(new RuleGenerator().towerRule());
+		System.out.println(new RuleGenerator().locationRule());
 
 	}
 
