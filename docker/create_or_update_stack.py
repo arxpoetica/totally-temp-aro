3df39d4d6fd6629ff8e6379f705dadfa47508ae3
Nick Vitucci <nvitucci@altvil.com>
2016-11-02 12:53:02 -0400

    Add variables and settings for enhanced deployment
     - Db initial/population only happens for QA env - not staging

--- docker/create_or_update_stack.py
+++ docker/create_or_update_stack.py
@@ -48,9 +48,11 @@ with open(TEMPLATE_FILE, 'r') as template_file:
 branch_name = os.environ['CIRCLE_BRANCH'].translate(string.maketrans('_', '-'))
 build_num = os.environ['CIRCLE_BUILD_NUM']
 etl_image_version = os.environ.get('ARO_ETL_IMAGE_VERSION')
+aro_etl_image_name = os.environ.get('ARO_ETL_IMAGE_NAME') or 'aro-etl'
 nginx_image_version = os.environ.get('ARO_NGINX_IMAGE_VERSION') 
 aro_service_image_version = os.environ.get('ARO_SERVICE_IMAGE_VERSION')
 aro_data_image_version = os.environ.get('ARO_DATA_IMAGE_VERSION')
+aro_data_image_name = os.environ.get('ARO_DATA_IMAGE_NAME') or 'aro-data'
 domain_name = os.environ.get('ARO_APP_CLIENT_DOMAIN')
 aro_client = os.environ.get('ARO_CLIENT') or 'aro'
 env_slug = branch_name
@@ -148,13 +150,13 @@ def provision_stack(cloudformation_stack):
         # internal_layer_id=stack.get_cfn_stack_output(cloudformation_stack, 'ExtraInternalLayer'),
         rds_instance_identifier=stack.get_cfn_stack_output(cloudformation_stack, 'RDSInstance'),
         environment=environment,
-        name='ARO-APP',
+        name='ARO',
         name_component=real_name_component,
         db={'user': db_user, 'pass': db_pass},
         docker_pass=docker_pass,
         environment_vars=_set_environment(),
         start_stack=True,
-        initialize_database=True,
+        initialize_database = True if environment == 'qa' else False,
         opsworks_client=opsworks_client,
         logs_client=logs_client,
         iam_client= iam_client,
@@ -187,6 +189,8 @@ def _set_environment():
             { 'Key': 'nginx_container_tag', 'Value': str(nginx_image_version), 'Secure': False },
             { 'Key': 'aro_service_container_tag', 'Value': str(aro_service_image_version), 'Secure': False },
             { 'Key': 'aro_data_container_tag', 'Value': str(aro_data_image_version), 'Secure': False },
+            { 'Key': 'aro_data_image_name', 'Value': str(aro_data_image_name), 'Secure': False },
+            { 'Key': 'aro_etl_image_name', 'Value': str(aro_etl_image_name), 'Secure': False },
             # { 'Key': 'database_url', 'Value': str(database_url), 'Secure': True },
             { 'Key': 'aro_client', 'Value': str(aro_client), 'Secure': False },
             { 'Key': 'client_slug', 'Value': str(name_component), 'Secure': False },
