4bf0832cd9367085041ff2a5467f264549a1142e
Alberto Gimeno <gimenete@gmail.com>
2016-11-04 10:06:28 +0100

    Initial code for backhaul functionality

--- app/public/javascripts/src/controllers/backhaul_controller.js
+++ app/public/javascripts/src/controllers/backhaul_controller.js
@@ -1,3 +1,23 @@
 /* global app */
 app.controller('backhaul-controller', ['$scope', '$rootScope', '$http', ($scope, $rootScope, $http) => {
+  $scope.selectedEquipment = []
+  $scope.addEquipment = () => {
+    $rootScope.$broadcast('edit-backhaul')
+  }
+  $scope.removeEquipment = (equipment) => {
+    var i = $scope.selectedEquipment.indexOf(equipment)
+    if (i >= 0) {
+      $scope.selectedEquipment.splice(i, 1)
+    }
+  }
+  $rootScope.$on('map_layer_clicked_feature', (e, event, layer) => {
+    if (layer.type !== 'network_nodes') return
+    var id = event.feature.getProperty('id')
+    var equipment = $scope.selectedEquipment.find((item) => item.id === id)
+    if (equipment) return
+    $scope.selectedEquipment.push({
+      name: 'node',
+      id: id
+    })
+  })
 }])
