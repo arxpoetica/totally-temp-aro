5c1e2bc4d03b58032e498ef3ee75af27d7300bb4
Alberto Gimeno <gimenete@gmail.com>
2016-12-28 19:28:14 +0100

    Avoid flickering. Show progress bar complete if child plans

--- app/models/network_plan.js
+++ app/models/network_plan.js
@@ -379,7 +379,8 @@ module.exports = class NetworkPlan {
             $1::text AS carrier_name,
             plan.id, name, area_name, ST_AsGeoJSON(area_centroid)::json as area_centroid, ST_AsGeoJSON(area_bounds)::json as area_bounds,
             users.id as owner_id, users.first_name as owner_first_name, users.last_name as owner_last_name,
-            created_at, updated_at
+            created_at, updated_at,
+            (SELECT EXISTS(SELECT id FROM client.plan p WHERE parent_plan_id=plan.id LIMIT 1)) AS "ranOptimization"
           FROM client.plan
           LEFT JOIN auth.permissions ON permissions.plan_id = plan.id AND permissions.rol = 'owner'
           LEFT JOIN auth.users ON users.id = permissions.user_id
