9256028940236f257c6bd408e254f965bfdcf74a
Michael Maloney <mmaloney@altvil.com>
2017-02-14 15:19:29 -0500

    Adding data from plan output and FIM for street view demo

--- app/public/streetview/app/controller/map_controller.js
+++ app/public/streetview/app/controller/map_controller.js
@@ -17,7 +17,7 @@ function MapsController($scope,$rootScope , $timeout , $compile ,MapLayer,$templ
         },
         initMap : function () {
            $timeout(function () {
-               var astorPlace = this.mapCenter = {lat: 42.356592, lng: -71.053956};
+               var astorPlace = this.mapCenter = {lat: 42.351573, lng: -71.080591};
                this.map = new google.maps.Map(document.getElementById('mapView'), {
                    center: astorPlace,
                    zoom: 18,
