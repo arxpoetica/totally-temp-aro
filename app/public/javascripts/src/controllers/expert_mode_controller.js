7912e1c787399b63a21345045a5b116859fce5db
dileep <dileep@metronconsulting.com>
2017-03-21 12:42:59 +0530

    User can use Expert mode in Area Network Planning (141457177)

---
+++ app/public/javascripts/src/controllers/expert_mode_controller.js
@@ -0,0 +1,14 @@
+/* global app config $ encodeURIComponent */
+// Selected expert mode controller
+app.controller('expert_mode_controller', ($rootScope, $scope, $http, map_layers, tracker, map_tools) => {
+	
+	$scope.runexpertmode = () => {
+		
+		$rootScope.$broadcast('expert-mode-plan-edited',$('#expert_mode_body').val());  
+	}
+	
+	$scope.refreshexpertmode = () => {
+		
+		$('#expert_mode_body').val("");  
+	}
+})
