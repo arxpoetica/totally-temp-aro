8f3b1418924103606dc8d31a20210ed42c5e2c89
Parag Patwardhan <parag@metronconsulting.com>
2017-09-05 16:37:36 +0530

    Plan name UI is now the same as toolbar and ARO logo

--- app/public/javascripts/src/components/network-plan-component.js
+++ app/public/javascripts/src/components/network-plan-component.js
@@ -7,31 +7,25 @@ app.component('networkPlan', {
       white-space: nowrap;
       right: 0px;
       position: absolute;
-      background: #1a79db;
-      padding: 7px;
-      border-bottom-left-radius: 5px;
-      border-bottom-right-radius: 5px;
-    }
-
-    .network-plan-load {
-      font-size: 25px;
+      background: rgba(50, 50, 50, 0.7);
+      font-size: 20px;
       padding-left: 10px;
       padding-right: 10px;
-      line-height: 46px;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
       cursor: pointer;
+      color: white;
     }
 
-    .network-plan:hover, .network-plan-load:hover, .network-plan span:hover {
+    .network-plan:hover, .network-plan:hover, .network-plan span:hover {
       color: #ccc
     }
     
   </style>
 
-  <div class="network-plan">
-    <div class="network-plan-load" ng-click="showNetworkPlanModal()">
-      <span ng-if="state.planName">{{state.planName}}</span>
-      <span ng-if="!state.planName"><i class="fa fa-plus f-select-plan" style="font-size: 12px"></i> Select a plan</span>
-    </div>
+  <div class="network-plan" ng-click="showNetworkPlanModal()">
+    <span ng-if="state.planName">{{state.planName}}</span>
+    <span ng-if="!state.planName"><i class="fa fa-plus f-select-plan" style="font-size: 12px"></i> Select a plan</span>
   </div>
   `,
   bindings: {},
