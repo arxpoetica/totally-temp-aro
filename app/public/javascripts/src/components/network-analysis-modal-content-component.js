bf725dbcd9a9f78a593428d5199edc9a402e28d8
dileep <dileep@metronconsulting.com>
2017-09-12 12:08:22 +0530

    Displaying y-axis values as xxM/xxK, irr as % and sorting x-axis values

--- app/public/javascripts/src/components/network-analysis-modal-content-component.js
+++ app/public/javascripts/src/components/network-analysis-modal-content-component.js
@@ -52,12 +52,29 @@ class NetworkAnalysisModalContentController {
         var options = {
           datasetFill: false,
           bezierCurve: false,
-          scaleLabel: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
-          tooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
-          multiTooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
+          //scaleLabel: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
+          //tooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
+          //multiTooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
+        }
+        if ($scope.selectedOption.key === 'irr') {
+          options.scaleLabel = function (label) { return String($filter('number')(label.value,0) + '%') }
+          options.tooltipTemplate = function (label) { return String($filter('number')(label.value,2) + '%') }
+          options.multiTooltipTemplate = function (label) { return String($filter('number')(label.value,2) + '%') }
+        } else if ($scope.selectedOption.key === 'npv') {
+          options.scaleLabel = function (label) { return buildLabel (label,0) }
+          options.tooltipTemplate = function (label) { return buildLabel (label,2) }
+          options.multiTooltipTemplate = function (label) { return buildLabel (label,2) }
+        } else {
+          options.scaleLabel = function (label) { return buildLabel (label,1) }
+          options.tooltipTemplate = function (label) { return buildLabel (label,2) }
+          options.multiTooltipTemplate = function (label) { return buildLabel (label,2) }
         }
         showChart('network-analysis-chart-cash-flow', 'Line', data, options)
       })
+    }
+
+    function buildLabel(label, fractionSize) {
+      return $filter('currency')(+label.value >= 10000000 ? +label.value / 1000000 : +label.value / 1000, '$', fractionSize) + (+label.value >= 10000000 ? 'M' : 'K')
     }
 
     function request (key, params, callback) {
@@ -72,7 +89,8 @@ class NetworkAnalysisModalContentController {
     
     function buildChartData (result, datasets) {
       var labels = result.splice(0, 1)
-      result = result.map((row) => _.object(labels[0],row))
+      result = result.map((row) => _.object(labels[0],row.map((value) => +value)))
+      result = _.sortBy(result,'capex')
       
       return {
         labels: result.map((row) => String($filter('number')(+row.capex/1000,0)+'K')),
