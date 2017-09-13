0cd62a7f4ecfad78cd35dc91810c0f40fb8c67a8
dileep <dileep@metronconsulting.com>
2017-09-13 15:29:41 +0530

    151005288: updates to network analysis output

--- app/public/javascripts/src/components/network-analysis-modal-content-component.js
+++ app/public/javascripts/src/components/network-analysis-modal-content-component.js
@@ -54,32 +54,60 @@ class NetworkAnalysisModalContentController {
     $scope.showCashFlowChart = () => {
       request('optimization_analysis', {}, (cashFlow) => {
         var data = buildChartData(cashFlow, $scope.selectedOption)
+        var categories = Object.freeze({
+          Normal: 1,
+          Thousand: 1000,
+          Million: 1000000
+        })
+        var yAxisCategory = categories.Normal 
+        if(_.max(data.datasets[0].data) >= 10000000) 
+          yAxisCategory = categories.Million
+        else if(_.max(data.datasets[0].data) >= 10000 && _.max(data.datasets[0].data) < 10000000)
+          yAxisCategory = categories.Thousand
+        else
+          yAxisCategory = categories.Normal
         var options = {
           datasetFill: false,
           bezierCurve: false,
+          //scaleBeginAtZero : true,
           //scaleLabel: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
           //tooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
           //multiTooltipTemplate: `<%= angular.injector(['ng']).get('$filter')('currency')(value / 1000, '$', 0) + ' K' %>`, // eslint-disable-line
         }
+        //Starting Yaxis at zero if minimum value is greater than zero
+        if ( _.min(data.datasets[0].data) > 0 ) options.scaleBeginAtZero = true
+        
         if ($scope.selectedOption.key === 'irr') {
-          options.scaleLabel = function (label) { return String($filter('number')(label.value,0) + '%') }
-          options.tooltipTemplate = function (label) { return String($filter('number')(label.value,2) + '%') }
-          options.multiTooltipTemplate = function (label) { return String($filter('number')(label.value,2) + '%') }
+          options.scaleLabel = function (label) { return buildLabel (label,0,yAxisCategory,false,'%') }
+          options.tooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,false,'%') }
+          options.multiTooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,false,'%') }
         } else if ($scope.selectedOption.key === 'npv') {
-          options.scaleLabel = function (label) { return buildLabel (label,0) }
-          options.tooltipTemplate = function (label) { return buildLabel (label,2) }
-          options.multiTooltipTemplate = function (label) { return buildLabel (label,2) }
+          options.scaleLabel = function (label) { return buildLabel (label,0,yAxisCategory,true,'$') }
+          options.tooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,true,'$') }
+          options.multiTooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,true,'$') }
         } else {
-          options.scaleLabel = function (label) { return buildLabel (label,1) }
-          options.tooltipTemplate = function (label) { return buildLabel (label,2) }
-          options.multiTooltipTemplate = function (label) { return buildLabel (label,2) }
+          options.scaleLabel = function (label) { return buildLabel (label,0,yAxisCategory,false) }
+          options.tooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,false) }
+          options.multiTooltipTemplate = function (label) { return buildLabel (label,2,yAxisCategory,false) }
         }
         showChart('network-analysis-chart-cash-flow', 'Line', data, options)
       })
     }
 
-    function buildLabel(label, fractionSize) {
-      return $filter('currency')(+label.value >= 10000000 ? +label.value / 1000000 : +label.value / 1000, '$', fractionSize) + (+label.value >= 10000000 ? 'M' : 'K')
+    function buildLabel(label, fractionSize, category, isCurrency, symbol) {
+      if (isCurrency)
+        return $filter('currency')(+label.value / category, symbol, fractionSize) + (category === 1000000 ? 'M' : 'K')
+      else {
+        var measure = ''
+        if (symbol)
+          measure = symbol
+        else if (category === 1000000)
+          measure = 'M'
+        else if (category === 1000)
+          measure = 'K'
+        
+        return $filter('number')(+label.value / category, fractionSize) + `${measure}`
+      }
     }
 
     function request (key, params, callback) {
