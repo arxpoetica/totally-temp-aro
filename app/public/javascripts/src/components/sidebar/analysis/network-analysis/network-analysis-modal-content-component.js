f5fa04b77d000dadbaca5d08447c860ad2c0ea8d
dileep <dileep@metronconsulting.com>
2017-09-28 15:44:02 +0530

    refine code

--- app/public/javascripts/src/components/sidebar/analysis/network-analysis/network-analysis-modal-content-component.js
+++ app/public/javascripts/src/components/sidebar/analysis/network-analysis/network-analysis-modal-content-component.js
@@ -1,12 +1,14 @@
 class NetworkAnalysisModalContentController {
 
-  constructor($scope,$rootScope,$document,$http,$filter, state) {
+  constructor($document,$http,$filter, state) {
     this.state = state
     this.$http = $http
-    $scope.plan = null
+    this.$filter = $filter
+    this.$document = $document
+    this.plan = null
 
-    var charts = {}
-    var chartStyles = [   
+    this.charts = {}
+    this.chartStyles = [   
       {
         borderColor: 'rgba(121,127,121,0.5)',
         pointBorderColor: 'rgba(121,127,121,0.8)',
@@ -27,181 +29,173 @@ class NetworkAnalysisModalContentController {
       }
     ]
 
-    $scope.datasets = [
+    this.datasets = [
       { key: 'irr', name: 'IRR' },
       { key: 'npv', name: 'NPV' },
       { key: 'coverage', name: 'Coverage' }
     ]
 
-    $scope.selectedOption = $scope.datasets[0]
+    this.selectedOption = this.datasets[0]
+
+    this.categories = Object.freeze({
+      Normal: 1,
+      Thousand: 1000,
+      Million: 1000000
+    })
 
     state.plan
       .subscribe((plan) => {
-        $scope.plan = plan
+        this.plan = plan
       })
     
     state.showNetworkAnalysisOutput
     .subscribe((show) => {
-      if (_.size(charts) > 0) charts['network-analysis-chart-cash-flow-panel'] && charts['network-analysis-chart-cash-flow-panel'].destroy()
+      if (_.size(this.charts) > 0) this.charts['network-analysis-chart-cash-flow-panel'] && this.charts['network-analysis-chart-cash-flow-panel'].destroy()
       if(show)
         this.showCashFlowChart()
     })
-
-    this.downloadChart = () => {
-      if (!$scope.plan) return
-      window.location.href = `/reports/network_analysis/download/${$scope.plan.id}/optimization_analysis`
-    }
-
-    this.showCashFlowChart = () => {
-      if (!$scope.plan) return
-      request('optimization_analysis', {}, (cashFlow) => {
-        var data = buildChartData(cashFlow, $scope.selectedOption)
-        var categories = Object.freeze({
-          Normal: 1,
-          Thousand: 1000,
-          Million: 1000000
-        })
-        var yAxisCategory = categories.Normal
-        var MaxYVal = Math.max(...data.datasets[0].data.map(val => val.y)) 
-        if(MaxYVal >= 10000000) 
-          yAxisCategory = categories.Million
-        else if(MaxYVal >= 10000 && MaxYVal < 10000000)
-          yAxisCategory = categories.Thousand
-        else
-          yAxisCategory = categories.Normal
-      
-        var tooltips = {}
-        var options = {
-          elements: {
-            line: {
-              fill: false,
-              tension: 0, // disables bezier curves
-            }
-          },
-          tooltips: {},
-          scales: {},
-          showLines: true,
-          responsive: true,
-          maintainAspectRatio: false
-        }
-
-        if ($scope.selectedOption.key === 'irr') {
-          options.scales = { yAxes: [{ ticks: { callback: function (value, index, values) { return buildLabel(value * 100, 0, yAxisCategory, false, '%') },beginAtZero:  true } }] }
-          tooltips = {
-            callbacks: {
-              label: function (tooltipItems, data) {
-                return buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, false, '%')
-              }
-            }
-          }
-        } else if ($scope.selectedOption.key === 'npv') {
-          options.scales = { yAxes: [{ ticks: { callback: function (value, index, values) { return buildLabel(value, 0, yAxisCategory, true, '$') },beginAtZero:  true } }] }
-          tooltips = {
-            callbacks: {
-              label: function (tooltipItems, data) {
-                return buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, true, '$')
-              }
-            }
-          }
-        } else {
-          options.scales = { yAxes: [{ ticks: { callback: function (value, index, values) { return buildLabel(value, 0, yAxisCategory, false) },beginAtZero:  true } }] }
-          tooltips = {
-            callbacks: {
-              label: function (tooltipItems, data) {
-                return buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, false)
-              }
-            }
-          }
-        }
-        
-        options.scales.xAxes = [{ ticks: {
-          userCallback: function(label, index, labels) {
-            var MaxXVal = _.max(labels)
-            var xAxisCategory = categories.Normal
-            if(MaxXVal >= 10000000) 
-              xAxisCategory = categories.Million
-            else if(MaxXVal >= 10000 && MaxXVal < 10000000)
-              xAxisCategory = categories.Thousand
-            else
-              xAxisCategory = categories.Normal
-            return String($filter('number')(+label/xAxisCategory,0) + (xAxisCategory === 1000000 ? 'M' : 'K'))
-          }, autoSkip:true, maxTicksLimit:10 } }]
-        options.tooltips = tooltips
-        showChart(this.target, 'scatter', data, options)
-      })
-    }
-
-    function buildLabel(value, fractionSize, category, isCurrency, symbol) {
-      if (isCurrency)
-        return $filter('currency')(value / category, symbol, fractionSize) + (category === 1000000 ? 'M' : 'K')
-      else {        
-        return $filter('number')(value / category, fractionSize) + (symbol ? symbol : (category === 1000000 ? 'M' : (category === 1000 ? 'K' : '')))
-      }
-    }
-
-    function buildTooltipLabel(value,data, fractionSize, category, isCurrency, symbol) {
-      var tooltip = "CAPEX:" + String($filter('number')(+data.datasets[0].data[value.index].x/1000,0)+'K')
-         + ';' +data.datasets[value.datasetIndex].label +': '
-      if (isCurrency)
-        return tooltip + $filter('currency')(value.yLabel / category, symbol, fractionSize) + (category === 1000000 ? 'M' : 'K')
-      else {        
-        return tooltip + (symbol ? $filter('number')(value.yLabel * 100 / category, fractionSize) : $filter('number')(value.yLabel / category, fractionSize))  
-          + (symbol ? symbol : (category === 1000000 ? 'M' : (category === 1000 ? 'K' : '')))
-      }
-    }
-
-    function request (key, params, callback) {
-      if (!$scope.plan) return
-      var plan_id = $scope.plan.id
-      $http({ url: `/reports/network_analysis/${plan_id}/${key}`, params: params })
-        .then((response) => {
-          callback(response.data)
-        })
-    }
     
-    function buildChartData (result, datasets) {
-      var labels = result.splice(0, 1)
-      result = result.map((row) => _.object(labels[0],row.map((value) => +value)))
-      result = _.sortBy(result,'index')
-      
-      return {
-        datasets: [datasets].map((dataset, i) => Object.assign({
-          label: dataset.name,
-          data: result.map((row) => ({x:row.capex,y:row[dataset.key]}))
-        }, chartStyles[i % chartStyles.length]))
-      }
-    }
-
-    function showChart (id, type, data, options) {
-      charts[id] && charts[id].destroy()
-      var elem = document.getElementById(id)
-      var ctx = elem.getContext('2d')
-      
-      charts[id] = new Chart(ctx, {
-        type: type,
-        data: data,
-        options: options
-      })
-     
-      var legend = document.getElementById(id + '-legend')
-      if (legend) {
-        legend.innerHTML = charts[id].generateLegend()
-      }
-    }
-
     this.showCashFlowChart()    
+  }
+
+  downloadChart() {
+    if (!this.plan) return
+    window.location.href = `/reports/network_analysis/download/${this.plan.id}/optimization_analysis`
+  }
+
+  showCashFlowChart() {
+    if (!this.plan) return 
+    this.request('optimization_analysis', {}, (cashFlow) => {
+      var data = this.buildChartData(cashFlow, this.selectedOption)
+
+      var MaxYVal = Math.max(...data.datasets[0].data.map(val => val.y)) 
+      var yAxisCategory = this.assignCategory(MaxYVal)
+    
+      var tooltips = {}
+      var options = {
+        elements: { line: { fill: false, tension: 0, } }, // disables bezier curves
+        tooltips: {},
+        scales: {},
+        showLines: true,
+        responsive: true,
+        maintainAspectRatio: false
+      }
+
+      if (this.selectedOption.key === 'irr') {
+        options.scales = { yAxes: [{ ticks: { callback: (value, index, values) => { return this.buildLabel(value * 100, 0, yAxisCategory, false, '%') },beginAtZero:  true } }] }
+        tooltips = {
+          callbacks: {
+            label: (tooltipItems, data) => {
+              return this.buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, false, '%')
+            }
+          }
+        }
+      } else if (this.selectedOption.key === 'npv') {
+        options.scales = { yAxes: [{ ticks: { callback: (value, index, values) => { return this.buildLabel(value, 0, yAxisCategory, true, '$') },beginAtZero:  true } }] }
+        tooltips = {
+          callbacks: {
+            label: (tooltipItems, data) => {
+              return this.buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, true, '$')
+            }
+          }
+        }
+      } else {
+        options.scales = { yAxes: [{ ticks: { callback: (value, index, values) => { return this.buildLabel(value, 0, yAxisCategory, false) },beginAtZero:  true } }] }
+        tooltips = {
+          callbacks: {
+            label: (tooltipItems, data) => {
+              return this.buildTooltipLabel(tooltipItems,data, 2, yAxisCategory, false)
+            }
+          }
+        }
+      }
+      
+      options.scales.xAxes = [{ ticks: {
+        userCallback: (label, index, labels) => {
+          var MaxXVal = _.max(labels)
+          var xAxisCategory = this.assignCategory(MaxXVal)
+          return String(this.$filter('number')(+label/xAxisCategory,0) + (xAxisCategory === 1000000 ? 'M' : 'K'))
+        }, autoSkip:true, maxTicksLimit:10 } }]
+      options.tooltips = tooltips
+      this.showChart(this.target, 'scatter', data, options)
+    })
+  }
+
+  assignCategory(maxVal) {
+    var axisCategory = this.categories.Normal
+    if(maxVal >= 10000000) 
+      axisCategory = this.categories.Million
+    else if(maxVal >= 10000 && maxVal < 10000000)
+      axisCategory = this.categories.Thousand
+    else
+      axisCategory = this.categories.Normal
+    return axisCategory
+  }
+
+  buildLabel(value, fractionSize, category, isCurrency, symbol) {
+    if (isCurrency)
+      return this.$filter('currency')(value / category, symbol, fractionSize) + (category === 1000000 ? 'M' : 'K')
+    else {        
+      return this.$filter('number')(value / category, fractionSize) + (symbol ? symbol : (category === 1000000 ? 'M' : (category === 1000 ? 'K' : '')))
+    }
+  }
+
+  buildTooltipLabel(value,data, fractionSize, category, isCurrency, symbol) {
+    var tooltip = "CAPEX:" + String(this.$filter('number')(+data.datasets[0].data[value.index].x/1000,0)+'K')
+       + ';' +data.datasets[value.datasetIndex].label +': '
+    if (isCurrency)
+      return tooltip + this.$filter('currency')(value.yLabel / category, symbol, fractionSize) + (category === 1000000 ? 'M' : 'K')
+    else {        
+      return tooltip + (symbol ? this.$filter('number')(value.yLabel * 100 / category, fractionSize) : this.$filter('number')(value.yLabel / category, fractionSize))  
+        + (symbol ? symbol : (category === 1000000 ? 'M' : (category === 1000 ? 'K' : '')))
+    }
+  }
+
+  request (key, params, callback) {
+    if (!this.plan) return
+    var plan_id = this.plan.id
+    this.$http({ url: `/reports/network_analysis/${plan_id}/${key}`, params: params })
+      .then((response) => {
+        callback(response.data)
+      })
+  }
+
+  buildChartData (result, datasets) {
+    var labels = result.splice(0, 1)
+    result = result.map((row) => _.object(labels[0],row.map((value) => +value)))
+    result = _.sortBy(result,'index')
+    
+    return {
+      datasets: [datasets].map((dataset, i) => Object.assign({
+        label: dataset.name,
+        data: result.map((row) => ({x:row.capex,y:row[dataset.key]}))
+      }, this.chartStyles[i % this.chartStyles.length]))
+    }
+  }
+
+  showChart (id, type, data, options) {
+    this.charts[id] && this.charts[id].destroy()
+    var elem = document.getElementById(id)
+    var ctx = elem.getContext('2d')
+    
+    this.charts[id] = new Chart(ctx, {
+      type: type,
+      data: data,
+      options: options
+    })
+   
   }
 }
 
-NetworkAnalysisModalContentController.$inject = ['$scope', '$rootScope', '$document','$http','$filter', 'state']
+NetworkAnalysisModalContentController.$inject = ['$document','$http','$filter', 'state']
 
 app.component('networkAnalysisContent', {
   template: `
     <div>
       <select class="form-control" style="width: 20%;float: right"
         ng-change="$ctrl.showCashFlowChart()"
-        ng-model="selectedOption"
-        ng-options="item as item.name for item in datasets">
+        ng-model="$ctrl.selectedOption"
+        ng-options="item as item.name for item in $ctrl.datasets">
       </select>
       <div style="position: relative; width: 100%; height: 300px">
         <canvas ng-attr-id= "{{ $ctrl.target }}"></canvas>
