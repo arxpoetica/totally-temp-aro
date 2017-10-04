35ac032050894abdae3d3f8fa48a7822cb3e727a
Parag Patwardhan <parag@metronconsulting.com>
2017-10-04 15:23:18 +0530

    Adding back onHideModal so that the modal can be shown multiple times

--- app/public/javascripts/src/components/sidebar/analysis/network-analysis/network-analysis-modal-component.js
+++ app/public/javascripts/src/components/sidebar/analysis/network-analysis/network-analysis-modal-component.js
@@ -2,13 +2,19 @@ class NetworkAnalysisModalController {
   constructor(state) {
     this.state = state
   }
+
+  onHideModal() {
+    // The modal binds to the value of showNetworkAnalysisOutput. Set it to false so that the
+    // next time it changes to "true" the modal will be shown.
+    this.state.showNetworkAnalysisOutput.next(false)
+  }
 }
 
 NetworkAnalysisModalController.$inject = ['state']
 
 app.component('networkAnalysisModal', {
   template: `
-    <modal visible="$ctrl.state.showNetworkAnalysisOutput.value" backdrop="static">
+    <modal visible="$ctrl.state.showNetworkAnalysisOutput.value" backdrop="static" on-hide="$ctrl.onHideModal()" >
       <modal-header title="Network Analysis"></modal-header>
       <modal-body>
         <network-analysis-output-content></network-analysis-output-content>
