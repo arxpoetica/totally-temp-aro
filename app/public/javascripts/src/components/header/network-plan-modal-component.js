8223f134fbd946212fabb6f910ae1c4606bd39da
Unknown <balu@metronconsulting.com>
2017-10-16 19:46:52 +0530

    Refactor the plan component by removing scope.

--- app/public/javascripts/src/components/header/network-plan-modal-component.js
+++ app/public/javascripts/src/components/header/network-plan-modal-component.js
@@ -1,40 +1,33 @@
 class NetworkPlanModalController {
-  constructor($scope, state) {
-    this.state = state
-    this.newplan = false
+  constructor(state) {
+    this.state    = state
+    this.loadPlan = false
+  }
 
-    $scope.close = () => {
-      state.networkPlanModal.next(false)
-    }
+  close() {
+    this.state.networkPlanModal.next(false)
+  }
 
-    $scope.modalShown = () => {
-      state.networkPlanModal.next(true)
-    }
+  modalShown() {
+    this.state.networkPlanModal.next(true)
+  }
 
-    $scope.modalHide = () => {
-      state.networkPlanModal.next(false)
-    }
-
-    $scope.saveNewPlan = () => {
-      //network-plan-manage-component will save the plan
-      this.newplan = true
-    }
-
+  modalHide() {
+    this.state.networkPlanModal.next(false)
   }
 }
 
-NetworkPlanModalController.$inject = ['$scope', 'state']
+NetworkPlanModalController.$inject = ['state']
 
 app.component('networkPlanModal', {
   template: `
-  <modal visible="$ctrl.state.networkPlanModal.value" backdrop="static" on-show="modalShown()" on-hide="modalHide()" >
+  <modal visible="$ctrl.state.networkPlanModal.value" backdrop="static" on-show="$ctrl.modalShown()" on-hide="$ctrl.modalHide()" >
     <modal-header title="Network Plan"></modal-header>
       <modal-body>
-        <network-plan-manage visible="$ctrl.newplan"></network-plan-manage>
+        <network-plan-manage ng-if="$ctrl.state.networkPlanModal.value"></network-plan-manage>
       </modal-body>
     <modal-footer>
-      <button class="btn btn-primary" ng-click="close()">Close</button>
-      <button class="btn btn-primary" ng-click="saveNewPlan()">Create Plan</button>
+      <button class="btn btn-primary" ng-click="$ctrl.close()">Close</button>
     </modal-footer>
   </modal>
   `,
