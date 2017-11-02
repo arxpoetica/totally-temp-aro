cd383e45ed22dae6f0ea53f348050ea529770eab
Parag Patwardhan <parag@metronconsulting.com>
2017-11-02 17:58:21 +0530

    Only save those ARPU manager entries that have changed

--- app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/arpu-editor-component.js
+++ app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/arpu-editor-component.js
@@ -2,6 +2,7 @@ class ArpuEditorController {
   constructor($http) {
     this.$http = $http
     this.arpuManagerConfiguration = []
+    this.pristineArpuManagerConfiguration = {}
     this.arpuManagerName = ''
   }
 
@@ -19,14 +20,42 @@ class ArpuEditorController {
     })
 
     this.$http.get(`/service/v1/arpu-manager/${this.arpuManagerId}/configuration`)
-    .then((result) => this.arpuManagerConfiguration = result.data)
+    .then((result) => {
+      this.arpuManagerConfiguration = result.data
+      this.pristineArpuManagerConfiguration = {}
+      var copyOfModels = angular.copy(this.arpuManagerConfiguration.arpuModels)
+      copyOfModels.forEach((arpuModel) => {
+        // Create a key from the "id" object
+        var arpuKey = JSON.stringify(arpuModel.id)
+        this.pristineArpuManagerConfiguration[arpuKey] = arpuModel
+      })
+    })
     .catch((err) => console.error(err))
   }
 
   saveConfigurationToServer() {
-    this.$http.put(`/service/v1/arpu-manager/${this.arpuManagerId}/configuration`, this.arpuManagerConfiguration)
-    .then((result) => this.exitEditingMode())
-    .catch((err) => console.error(err))
+
+    // Only save those configurations that have changed
+    var changedModels = []
+    this.arpuManagerConfiguration.arpuModels.forEach((arpuModel) => {
+      var arpuKey = JSON.stringify(arpuModel.id)
+      var pristineModel = this.pristineArpuManagerConfiguration[arpuKey]
+      if (pristineModel) {
+        // Check to see if the model has changed
+        if (JSON.stringify(pristineModel) !== angular.toJson(arpuModel)) {
+          changedModels.push(arpuModel)
+        }
+      }
+    })
+
+    if (changedModels.length > 0) {
+      console.log(changedModels)
+      this.$http.put(`/service/v1/arpu-manager/${this.arpuManagerId}/configuration`, changedModels)
+      .then((result) => this.exitEditingMode())
+      .catch((err) => console.error(err))
+    } else {
+      console.log('ARPU Editor: No models were changed. Nothing to save.')
+    }
   }
 
   exitEditingMode() {
