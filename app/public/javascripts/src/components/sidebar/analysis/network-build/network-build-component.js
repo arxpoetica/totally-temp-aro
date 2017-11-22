c97f4d8bcb9ec5ce3131f28f4572351bdcf52027
dileep <dileep@metronconsulting.com>
2017-11-22 09:45:04 +0530

    Updates to Input formatting

--- app/public/javascripts/src/components/sidebar/analysis/network-build/network-build-component.js
+++ app/public/javascripts/src/components/sidebar/analysis/network-build/network-build-component.js
@@ -54,23 +54,4 @@ app.component('networkBuild', {
     removeServiceArea: '&'
   },
   controller: NetworkBuildController
-})    
-
-app.directive('format', ['$filter', function ($filter) {
-  return {
-      require: '?ngModel',
-      link: function (scope, elem, attrs, ctrl) {
-        if (!ctrl) return;
-
-        ctrl.$formatters.unshift(function (a) {
-          return $filter(attrs.format)(ctrl.$modelValue)
-        });
-
-        ctrl.$parsers.unshift(function (viewValue) {
-          var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
-          elem.val($filter(attrs.format)(+plainNumber));
-          return plainNumber;
-        });
-      }
-  };
-}]);
+})
