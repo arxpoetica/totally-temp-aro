c38d85227b7b228fb214a184890275abf959073d
dileep <dileep@metronconsulting.com>
2017-12-06 15:20:36 +0530

    Remove old code for Network Plan creation,selection, delete

--- app/public/javascripts/src/controllers/navigation_menu_controller.js
+++ app/public/javascripts/src/controllers/navigation_menu_controller.js
@@ -85,17 +85,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
     $scope.selectPlan(null)
   })
 
-  $scope.selectPlan = function (plan) {
-    $scope.plan = plan
-    $('#plan-saving').stop().hide()
-    $('#plan-saved').stop().hide()
-    $('#plan-saving-progress').hide()
-    state.loadPlan(plan.id)
-    $rootScope.$broadcast('plan_selected', plan)
-    $('#select-plan').modal('hide')
-    $('#plan-combo').modal('hide')
-  }
-
   $rootScope.$on('plan_selected', (e, plan) => {
 
     $scope.plan = plan
@@ -174,91 +163,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
     tracker.track('Global customer profile')
   }
 
-  $scope.deletePlan = (plan) => {
-    if (!plan) return
-    tracker.track('Manage Analyses / Delete Analysis')
-
-    swal({
-      title: 'Are you sure?',
-      text: 'You will not be able to recover the deleted plan!',
-      type: 'warning',
-      confirmButtonColor: '#DD6B55',
-      confirmButtonText: 'Yes, delete it!',
-      showCancelButton: true,
-      closeOnConfirm: true
-    }, () => {
-      if ($scope.plan && plan.id === $scope.plan.id) {
-        $scope.plan = null
-        state.loadPlan(null)
-        $rootScope.$broadcast('plan_selected', null)
-        delete $rootScope.currentPlan;
-      }
-      $http.delete(`/service/v1/plan/${plan.id}?user_id=${$scope.user_id}`).then((response) => {
-        $scope.loadPlans()
-      })
-    })
-  }
-
-  var interval = null
-
-  $('#select-plan').on('hidden.bs.modal', () => {
-    clearInterval(interval)
-  })
-
-  $scope.loadPlans = function (page, callback) {
-    clearInterval(interval)
-    $scope.currentPage = page || 1
-    $scope.maxResults = 10
-    if(page > 1) {
-      var start = $scope.maxResults * (page - 1);
-      var end   = start + $scope.maxResults;
-      $scope.plans = $scope.allPlans.slice(start, end);
-      return;
-    }
-
-    var load = (callback) => {
-
-      var planOptions = {
-        url: '/service/v1/plan',
-        method: 'GET',
-        params: {
-          user_id: $scope.user_id,
-          search: $scope.search_text,
-          project_id: $scope.projectId
-        }
-      }
-      $http(planOptions)
-        .then((response) => {
-            $http.get('/optimization/processes').then((running) => {
-              response.data.forEach((plan) => {
-                var info = running.data.find((status) => status.plan.id === +plan.id)
-                if (info) {
-                  var diff = (Date.now() - new Date(info.startDate).getTime()) / 1000
-                  var min = Math.floor(diff / 60)
-                  var sec = Math.ceil(diff % 60)
-                  plan.progressString = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec} Runtime`
-                  plan.progress = info.progress
-                  plan.startDate = info.startDate
-                  plan.optimizationState = info.optimizationState
-                }
-              })
-              $scope.allPlans = response.data
-              $scope.plans = response.data.slice(0, $scope.maxResults);
-              // $scope.pages = response.data.pages
-              $scope.pages = [];
-              var pageSize = Math.floor(response.data.length / $scope.maxResults) + (response.data.length % $scope.maxResults > 0 ? 1 : 0);
-              for (var i = 1; i <= pageSize; i++) {
-                $scope.pages.push(i);
-              }
-
-              callback && callback()
-            })
-        })
-    }
-    load(callback)
-    interval = setInterval(load, 100000)
-  }
-
   // load plan depending on the URL
   var path = $location.path()
   if (path.indexOf('/plan/') === 0) {
@@ -269,48 +173,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
       })
   }
 
-  $scope.showPlans = () => {
-    $scope.loadPlans(1, () => {
-      $('#select-plan').modal('show')
-      tracker.track('Open Analysis')
-
-
-    })
-  }
-
-  $scope.showCombo = () => {
-    $scope.loadPlans(1, () => {
-      $('#plan-combo').modal('show')
-      tracker.track('Open Analysis')
-
-      reloadCurrentLocation();
-    })
-  }
-
-  $scope.showReports = () => {
-    $('#reports').modal('show')
-  }
-
-  $scope.manageNetworkPlans = () => {
-    $scope.loadPlans(1, () => {
-      $('#manage-network-plans').modal('show')
-      tracker.track('Manage Analyses')
-    })
-  }
-
-  $scope.sortBy = (key, descending) => {
-    $scope.sortField = key
-    $scope.sortOrder = descending ? 'DESC' : 'ASC'
-
-    $scope.loadPlans()
-  }
-
-  $scope.new_plan = () => {
-    $('#new-plan').modal('show')
-    // initMap()
-    tracker.track('Create New Analysis')
-  }
-
   $scope.openFinancialProfile = () => {
     map_tools.show('financial_profile')
   }
@@ -318,74 +180,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
   // If we use this more than once it should be more generalized...
   $scope.clear_default_text = () => {
     $scope.new_plan_name = ''
-  }
-
-  $scope.saveNewPlan = () => {
-    var params = {
-      name: $scope.new_plan_name,
-      areaName: $scope.new_plan_area_name,
-      latitude: $scope.new_plan_area_centroid.coordinates[1],
-      longitude: $scope.new_plan_area_centroid.coordinates[0],
-      projectId: $scope.projectId
-    }
-
-    $http.post('/service/v1/plan?user_id=' + $scope.user_id, params).then((response) => {
-      $scope.selectPlan(response.data)
-      $('#new-plan').modal('hide')
-      $('#plan-combo').modal('hide')
-      $scope.loadPlans()
-    })
-  }
-
-  $('#new-plan').on('hidden.bs.modal', () => {
-    $scope.new_plan_name = 'Untitled Analysis'
-    $scope.new_plan_area_name = ''
-    $('#new-plan select').select2('val', '')
-    newPlanMap && newPlanMap.setCenter({lat: -34.397, lng: 150.644})
-  })
-
-  $scope.save_as = () => {
-    $scope.edit_plan_name = $scope.plan.name
-    $('#edit-plan').modal('show')
-  }
-
-  $scope.save_changes = () => {
-    $scope.plan.name = $scope.edit_plan_name
-    $http.post('/network_plan/' + $scope.plan.id + '/save', { name: $scope.plan.name }).then((response) => {
-      $('#edit-plan').modal('hide')
-    })
-  }
-
-  $scope.clearPlan = () => {
-    swal({
-      title: 'Are you sure?',
-      text: 'You will not be able to recover the deleted data!',
-      type: 'warning',
-      confirmButtonColor: '#DD6B55',
-      confirmButtonText: 'Yes, clear it!',
-      showCancelButton: true,
-      closeOnConfirm: true
-    }, () => {
-      $http.post('/network_plan/' + $scope.plan.id + '/clear', { userId: $scope.user_id }).then((response) => {
-        $rootScope.$broadcast('plan_cleared', $scope.plan)
-      })
-    })
-  }
-
-  $scope.exportKmlName = () => {
-    $('#export-plan').modal('show')
-  }
-
-  $scope.export_kml = () => {
-    var params = { name: $scope.kml_file_name }
-    if (!params.name.match(/^[a-zA-Z0-9-_]+$/)) {
-      $('#export-error').show()
-    } else {
-      $('#export-error').hide()
-      $('#export-plan').modal('hide')
-
-      location.href = '/network_plan/' + $scope.plan.id + '/' + params.name + '/export'
-    }
   }
 
   $scope.showSharePlan = (plan) => {
@@ -415,35 +209,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
       })
     })
   }
-
-  $scope.openReport = (plan) => {
-    var modal = null
-    ;['#select-plan', '#plan-combo'].forEach((id) => {
-      if (($(id).data('bs.modal') || {}).isShown) {
-        modal = id
-        $(id).modal('hide')
-      }
-    })
-    $rootScope.$broadcast('open-report', plan, modal)
-  }
-
-  // var nonLinearSlider = document.getElementById('year-slider')
-  // noUiSlider.create(nonLinearSlider, {
-  //   behaviour: 'tap',
-  //   connect: true,
-  //   start: [ 2016, 2019 ],
-  //   range: {
-  //     min: 2016,
-  //     max: 2019,
-  //     '0%': [ 2016, 1 ],
-  //     '100%': [ 2019, 1 ]
-  //   },
-  //   pips: {
-  //     mode: 'values',
-  //     values: [2016, 2017, 2018, 2019],
-  //     density: 0
-  //   }
-  // })
 
   google.charts.load('current', {'packages': ['gantt']})
 
@@ -477,10 +242,6 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
   }
 
   $('#build-sequence').on('shown.bs.modal', drawChart)
-
-  $scope.openReports = () => {
-    $rootScope.$broadcast('open-report', $scope.plan)
-  }
 
   $scope.paneClicked = function (view) {
     $scope.planView = view;
@@ -533,11 +294,10 @@ app.controller('navigation_menu_controller', ['$scope', '$rootScope', '$http', '
     })
   }
 
-
-    $rootScope.$on('show_create_plan_dialog' , function (events) {
-        $scope.showCombo();
-        $("#plan-combo").find("a[href='#plan-combo-create']").trigger('click')
-        $scope.paneClicked('add');
-    })
+  $rootScope.$on('show_create_plan_dialog', function (events) {
+    $scope.showCombo();
+    $("#plan-combo").find("a[href='#plan-combo-create']").trigger('click')
+    $scope.paneClicked('add');
+  })
 
 }])
