c4789240e43a906fd5f55ad08065869dd24b587d
Parag Patwardhan <parag@metronconsulting.com>
2018-03-01 13:42:49 +0530

    Refactoring: Added transaction store

--- app/public/javascripts/src/components/sidebar/view/location-editor-component.js
+++ app/public/javascripts/src/components/sidebar/view/location-editor-component.js
@@ -1,31 +1,74 @@
-var CommandTypes = Object.freeze({
-  ADD_LOCATION: 'ADD_LOCATION',
-  MOVE_LOCATION: 'MOVE_LOCATION'
-})
+class TransactionStore {
+  constructor() {
+    this.commandStack = []    // Stack of all commands executed
+    this.uuidToFeatures = {}  // Map of feature UUID to feature object
+  }
 
+  // Get a UUID. Generating random ones for now. Eventually we need to get these from aro-service
+  getUUID() {
+    // Note that this just returns RANDOM UUIDs, NOT RFC4122 COMPLIANT
+    var s4 = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  // Executes a command in the store
+  executeCommand(command, params) {
+    var result = command.execute(this, params)
+    this.commandStack.push(command)
+    console.log(JSON.stringify(this.uuidToFeatures))
+    return result
+  }
+
+}
 class CommandAddLocation {
-  execute(params) {
-    this.newLocationMarker = new google.maps.Marker({
+  execute(store, params) {
+
+    // Create a new feature object
+    var featureObj = {
+      uuid: store.getUUID(),
+      position: {
+        lat: params.locationLatLng.lat(),
+        lng: params.locationLatLng.lng()
+      },
+      numLocations: params.numLocations
+    }
+    store.uuidToFeatures[featureObj.uuid] = featureObj
+
+    // Create a new google maps marker
+    var newLocationMarker = new google.maps.Marker({
       position: params.locationLatLng,
       icon: '/images/map_icons/aro/households_default.png',
       draggable: true,
       map: params.map,
-      numLocations: params.numLocations,
-      commandParams: params
+      uuid: featureObj.uuid
     })
+
     this.params = params
-    return this.newLocationMarker
+    return newLocationMarker
   }
 }
 
 class CommandMoveLocation {
-  execute(params) {
+  execute(store, params) {
+    // Update the feature object in the store
+    var featureObj = store.uuidToFeatures[params.marker.uuid]
+    featureObj.position = {
+      lat: params.newLocation.lat(),
+      lng: params.newLocation.lng()
+    }
     this.params = params
   }
 }
 
 class CommandEditLocation {
-  execute(params) {
+  execute(store, params) {
+    // Update the feature object in the store
+    var featureObj = store.uuidToFeatures[params.marker.uuid]
+    featureObj.numLocations = params.numLocations
     this.params = params
   }
 }
@@ -42,9 +85,10 @@ class LocationEditorController {
         'Cell tower'
       ],
       selectedType: 'Household',
-      numberOfLocations: 5
+      numLocations: 5
     }
-    this.commandStack = []
+
+    this.store = new TransactionStore()
     this.selectedLocation = null
   }
 
@@ -65,12 +109,13 @@ class LocationEditorController {
       }
       // Create a new marker for the location, only if we are in the right selection mode
       var command = new CommandAddLocation()
-      var newLocationMarker = command.execute({
+      var params = {
         locationLatLng: event.latLng,
-        numLocations: self.addLocationData.numberOfLocations,
+        numLocations: self.addLocationData.numLocations,
         map: self.mapRef
-      })
-      self.commandStack.push(command)
+      }
+      var newLocationMarker = self.store.executeCommand(command, params)
+      self.selectMarker(newLocationMarker)
       self.$timeout() // Trigger change detection
 
       // Monitor events on the marker for dragstart and dragend
@@ -81,15 +126,20 @@ class LocationEditorController {
         self.handleDragEnd(newLocationMarker, event)
       })
       newLocationMarker.addListener('mousedown', (event) => {
-        if (self.selectedLocation) {
-          self.selectedLocation.setIcon('/images/map_icons/aro/households_default.png')
-        }
-        self.selectedLocation = newLocationMarker
-        self.selectedLocation.setIcon('/images/map_icons/aro/households_selected.png')
-        self.addLocationData.numberOfLocations = newLocationMarker.commandParams.numLocations
-        self.$timeout()
+        self.selectMarker(newLocationMarker)
       })
     });
+  }
+
+  selectMarker(marker) {
+    if (this.selectedLocation) {
+      this.selectedLocation.setIcon('/images/map_icons/aro/households_default.png')
+    }
+    this.selectedLocation = marker
+    this.selectedLocation.setIcon('/images/map_icons/aro/households_selected.png')
+    var featureObj = this.store.uuidToFeatures[marker.uuid]
+    this.addLocationData.numLocations = featureObj.numLocations
+    this.$timeout()
   }
 
   handleDragStart(event) {
@@ -98,24 +148,24 @@ class LocationEditorController {
 
   handleDragEnd(marker, event) {
     var command = new CommandMoveLocation()
-    command.execute({
+    var params = {
       marker: marker,
       oldLocation: this.dragStartLatLng,
       newLocation: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
-    })
+    }
+    this.store.executeCommand(command, params)
     this.dragStartLatLng = null
-    this.commandStack.push(command)
     this.$timeout() // Trigger change detection
   }
 
   handleNumLocationsChanged() {
     if (this.selectedLocation) {
-      this.selectedLocation.numLocations = this.addLocationData.numberOfLocations
       var command = new CommandEditLocation()
-      command.execute({
-        numLocations: this.addLocationData.numberOfLocations
-      })
-      this.commandStack.push(command)
+      var params = {
+        marker: this.selectedLocation,
+        numLocations: this.addLocationData.numLocations
+      }
+      this.store.executeCommand(command, params)
     }
   }
 
