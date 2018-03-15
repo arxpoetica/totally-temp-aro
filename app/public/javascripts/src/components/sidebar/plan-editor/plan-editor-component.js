1b94b241b9e08b7d8765c6117b1bba0151282280
Parag Patwardhan <parag@metronconsulting.com>
2018-03-15 09:22:51 +0530

    Added modes for plan editor

--- app/public/javascripts/src/components/sidebar/plan-editor/plan-editor-component.js
+++ app/public/javascripts/src/components/sidebar/plan-editor/plan-editor-component.js
@@ -2,6 +2,18 @@ class PlanEditorController {
   
   constructor(state) {
     this.state = state
+    this.editorModes = Object.freeze({
+      ADD: 'ADD',
+      DELETE: 'DELETE',
+      MOVE: 'MOVE',
+      EDIT_BOUNDARY: 'EDIT_BOUNDARY'
+    })
+    this.selectedEditorMode = this.editorModes.ADD
+  }
+
+  // Sets the editor mode, and subscribes/unsubscribes from map events
+  setEditorMode(newMode) {
+    this.selectedEditorMode = newMode
   }
 }
 
