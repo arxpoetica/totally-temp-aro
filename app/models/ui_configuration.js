35ce9d8d5d412ed7a01270d5d0a68b137ad16f76
Parag Patwardhan <parag@metronconsulting.com>
2018-04-18 15:59:45 +0530

    Bugfix: Configuration was not loading correctly from outside the container.
    Paths must be relative to the source file. Use native nodejs
    "require()".

--- app/models/ui_configuration.js
+++ app/models/ui_configuration.js
@@ -13,26 +13,37 @@ module.exports = class UIConfiguration {
     this.configurations = {}
   }
 
+  // Loads the specified JSON file if it exists. If not, returns defaultValue
+  loadJSONFile(fileWithPath, defaultValue) {
+    var jsonContents = null
+    try {
+      jsonContents = require(fileWithPath)
+    } catch(err) {
+      console.warn(`File ${fileWithPath} could not be located in ui_configuration.js`)
+      console.warn(err)
+      jsonContents = defaultValue
+    }
+    return jsonContents
+  }
+
   // Returns the specified configuration set
   getConfigurationSet (configSet) {
     
     if (!this.configurations[configSet]) {
       // This configuration set has not been loaded. Load it from disk now.
-      
       // merge base config with client config 
-      var baseConfigPath = '/srv/www/aro/conf/base/' + configSet + '.json'
-      var clientConfigPath = '/srv/www/aro/conf/' + process.env.ARO_CLIENT + '/' + configSet + '.json'
+      var baseConfigPath = `../../conf/base/${configSet}.json`
+      var clientConfigPath = `../../conf/${process.env.ARO_CLIENT}/${configSet}.json`
       
-      var baseConfig = fs.existsSync(baseConfigPath) ? JSON.parse( fs.readFileSync(baseConfigPath) ) : null // easier than testing for {}
-      var clientConfig = fs.existsSync(clientConfigPath) ? JSON.parse( fs.readFileSync(clientConfigPath) ) : {}
-      
+      var baseConfig = this.loadJSONFile(baseConfigPath, null) // easier than testing for {}
+      var clientConfig = this.loadJSONFile(clientConfigPath, {})
+
       if (null == baseConfig){
         // if we're just going to copy everything, no need to do it one at a time
         baseConfig = clientConfig 
       }else{
         UIConfiguration.basicDeepObjMerge(baseConfig, clientConfig)
       }
-      
       this.configurations[configSet] = baseConfig
     }
 
