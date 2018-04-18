c76400a1b992656cad4fa1356551cc809838fdda
theUtilitariat <theUtilitariat@gmail.com>
2018-04-17 20:07:08 -0400

    Boundary tag details in info panel when selected, using different column for area information.

--- app/public/javascripts/src/components/sidebar/view/boundary-detail.js
+++ app/public/javascripts/src/components/sidebar/view/boundary-detail.js
@@ -12,10 +12,14 @@ let boundaryDetail = {
     </style>
     <div class="boundary-detail" ng-if="$ctrl.selectedBoundaryInfo !== null">
       <div>Census Block Code: {{$ctrl.selectedBoundaryInfo.tabblock_id}}</div>
-      <div>Land Area(sq. miles): {{$ctrl.selectedBoundaryInfo.aland / (1609.34 * 1609.34) | number: 2}}</div>
-      <div>Water Area(sq. miles): {{$ctrl.selectedBoundaryInfo.awater / (1609.34 * 1609.34) | number: 2}}</div>
+      <div>Area(sq. miles): {{$ctrl.selectedBoundaryInfo.area_meters / (1609.34 * 1609.34) | number: 2}}</div>
+      <div>Area(acres): {{$ctrl.selectedBoundaryInfo.area_meters / 4046.86 | number: 2}}</div>
+      <div>Area(sq. meters): {{$ctrl.selectedBoundaryInfo.area_meters | number: 2}}</div>
       <div>Centroid Latitude: {{$ctrl.selectedBoundaryInfo.centroid.coordinates[1] | number:5}}</div>
       <div>Centroid Longitude: {{$ctrl.selectedBoundaryInfo.centroid.coordinates[0] | number:5}}</div>
+      <div ng-repeat="tag in $ctrl.selectedBoundaryTags" ng-if="undefined != tag.tagInfo">
+        {{tag.censusCatDescription}} : <div class="outlineLegendIcon" style="border-color: {{tag.tagInfo.colourHash}}; background-color: {{tag.tagInfo.colourHash}}33;"></div> {{tag.tagInfo.description}}
+      </div>
     </div>
     `,
     bindings: {},
