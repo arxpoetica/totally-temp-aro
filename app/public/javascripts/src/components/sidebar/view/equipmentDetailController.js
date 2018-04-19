3a14a0d7bc14e9d6c37d693c610b4f48150d1f5b
theUtilitariat <theUtilitariat@gmail.com>
2018-04-19 13:20:10 -0400

    layer order: equipment is above locations

--- app/public/javascripts/src/components/sidebar/view/equipmentDetailController.js
+++ app/public/javascripts/src/components/sidebar/view/equipmentDetailController.js
@@ -16,6 +16,7 @@ class EquipmentDetailController {
         //this.selectedEquipmentInfo = options.equipmentFeatures[0]
         this.getEquipmentInfo(equipmentId)
           .then((equipmentInfo) => {
+            console.log(equipmentInfo)
             this.selectedEquipmentInfo = equipmentInfo
           })
       }
