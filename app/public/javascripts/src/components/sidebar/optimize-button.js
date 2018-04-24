1f57d40273e13035d23e5523bef14fd82b1ea005
Saneesh <b_saneesh@metronconsulting.com>
2018-04-24 12:02:10 +0530

    Config refactoring start

--- app/public/javascripts/src/components/sidebar/optimize-button.js
+++ app/public/javascripts/src/components/sidebar/optimize-button.js
@@ -7,9 +7,9 @@ class OptimizeButtonController {
     var isValid = false
     if ((this.state.selectedLocations.getValue().size > 0 ||
       this.state.selectedServiceAreas.getValue().size > 0) && (
-        this.state.hasLocationType('business') ||
-        this.state.hasLocationType('household') ||
-        this.state.hasLocationType('celltower'))
+        this.state.locationInputSelected('business') ||
+        this.state.locationInputSelected('household') ||
+        this.state.locationInputSelected('celltower'))
     )
       isValid = true
 
