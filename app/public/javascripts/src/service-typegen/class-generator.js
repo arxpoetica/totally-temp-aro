b1b35808fd3f6b148fe179055b634ffa85b57a74
Parag Patwardhan <parag@metronconsulting.com>
2018-04-30 12:09:34 +0530

    Refactoring class generator - no change to functionality

--- app/public/javascripts/src/service-typegen/class-generator.js
+++ app/public/javascripts/src/service-typegen/class-generator.js
@@ -8,14 +8,50 @@ class ClassGenerator {
   constructor() {
   }
 
-  generateClass() {
-    var classDefinition = require('./src/equipment-feature.json')
-    
+  // The main function that generates the source code for our class definitions
+  static generateSourceCode() {
+
+    // Register Handlebars helpers - used to compile class definition into source code
+    this.registerHandlebarsHelpers(Handlebars)
+
+    // Hold a list of class names to compiled strings
     var templateSource = fs.readFileSync('./class-template.handlebars').toString()
+    var typeToSourceCode = {}
+    var handlebarsCompiler = Handlebars.compile(templateSource)
+    var classDefinition = require('./src/equipment-feature.json')
+    this.buildTypeSourceCode(classDefinition, handlebarsCompiler, typeToSourceCode)
+
+    // Dump to console
+    Object.keys(typeToSourceCode).forEach((typeKey) => {
+      console.log('-----------------------------------------------------------------------')
+      console.log(typeToSourceCode[typeKey])
+    })
+
+    // Save to distribution folder
+    this.deleteDistributionFolder()
+      .then(() => {
+        fs.mkdirSync(path.join(__dirname, './dist'))
+        Object.keys(typeToSourceCode).forEach((typeKey) => {
+          const className = typeKey.substr(typeKey.lastIndexOf(':') + 1)
+          const fileName = path.join(__dirname, `./dist/${className}.js`)
+          fs.writeFileSync(fileName, typeToSourceCode[typeKey])
+        })
+      })
+  }
+
+  // Register all helpers for Handlebars, used to generate the source for a single class
+  static registerHandlebarsHelpers(Handlebars) {
     Handlebars.registerHelper('classNameExtractor', (inputString) => {
       return inputString ? inputString.substr(inputString.lastIndexOf(':') + 1) : 'object'
     })
     Handlebars.registerHelper('isNotObject', (input) => input !== 'object')
+    this.registerImportsHelper(Handlebars)
+    this.registerAssignmentHelper(Handlebars)
+  }
+
+  // Register a Handlebars helper that generates the "this.xyz = new Abc()" statements
+  // that are used in the constructor of our class
+  static registerAssignmentHelper(Handlebars) {
     Handlebars.registerHelper('memberDeclaration', (memberName, memberType, objectId) => {
       var result = `this.${memberName} = `
       switch(memberType) {
@@ -45,7 +81,10 @@ class ClassGenerator {
       }
       return new Handlebars.SafeString(result)
     })
+  }
 
+  // Register a Handlebars helper that generates the "import ... from '...'" statements
+  static registerImportsHelper(Handlebars) {
     Handlebars.registerHelper('importDependentClasses', (properties) => {
       var importsString = ''
       Object.keys(properties).forEach((propertyKey) => {
@@ -61,34 +100,9 @@ class ClassGenerator {
       })
       return new Handlebars.SafeString(importsString)
     })
-
-    // Hold a list of class names to compiled strings
-    var typeToSourceCode = {}
-    var handlebarsCompiler = Handlebars.compile(templateSource)
-    // const result = handlebarsCompiler(classDefinition)
-    // console.log(result)
-    this.buildTypeSourceCode(classDefinition, handlebarsCompiler, typeToSourceCode)
-
-    // Dump to console
-    Object.keys(typeToSourceCode).forEach((typeKey) => {
-      console.log('-----------------------------------------------------------------------')
-      console.log(typeToSourceCode[typeKey])
-    })
-
-    // Save to distribution folder
-    this.deleteDistributionFolder()
-      .then(() => {
-        fs.mkdirSync(path.join(__dirname, './dist'))
-        Object.keys(typeToSourceCode).forEach((typeKey) => {
-          const className = typeKey.substr(typeKey.lastIndexOf(':') + 1)
-          const fileName = path.join(__dirname, `./dist/${className}.js`)
-          fs.writeFileSync(fileName, typeToSourceCode[typeKey])
-        })
-      })
-    
   }
 
-  getClassName(classDef) {
+  static getClassName(classDef) {
     if (classDef.id) {
       return classDef.id.substr(classDef.id.lastIndexOf(':') + 1)
     } else {
@@ -96,7 +110,8 @@ class ClassGenerator {
     }
   }
 
-  buildTypeSourceCode(classDefinition, handlebarsCompiler, classToSourceCode) {
+  static buildTypeSourceCode(classDefinition, handlebarsCompiler, classToSourceCode) {
+    // Only generate source code if we haven't generated it before.
     if (classDefinition.type === 'object' && classDefinition.id && !classToSourceCode.hasOwnProperty(classDefinition.id)) {
       // Build the source for this class
       classToSourceCode[classDefinition.id] = handlebarsCompiler(classDefinition)
@@ -109,11 +124,11 @@ class ClassGenerator {
     }
   }
 
-  deleteDistributionFolder() {
+  static deleteDistributionFolder() {
     return new Promise((resolve, reject) => {
       rimraf('./dist', () => resolve())
     })
   }
 }
 
-(new ClassGenerator()).generateClass()
+ClassGenerator.generateSourceCode()
