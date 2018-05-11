1e635389ce08d29afabe6fd8c280a6bc1cccb604
theUtilitariat <theUtilitariat@gmail.com>
2018-05-10 22:36:06 -0400

    coverage info is working! now time to make it pretty.

--- app/public/javascripts/src/components/sidebar/plan-editor/plan-editor.js
+++ app/public/javascripts/src/components/sidebar/plan-editor/plan-editor.js
@@ -14,7 +14,7 @@ class PlanEditorController {
     this.objectIdToMapObject = {}
     this.boundaryIdToEquipmentId = {}
     this.equipmentIdToBoundaryId = {}
-    this.boundaryInfoById = {}
+    this.boundaryCoverageById = {}
     this.objectIdsToHide = new Set()
     this.currentTransaction = null
     this.lastSelectedEquipmentType = 'Generic ADSL'
@@ -38,6 +38,12 @@ class PlanEditorController {
     this.enabledNetworkNodeTypes = [
       'dslam'
     ]
+    
+    this.censusCategories = this.state.censusCategories.getValue()
+    this.state.censusCategories.subscribe((newValue) => {
+      this.censusCategories = newValue
+    })
+    
   }
 
   // Get a list of UUIDs from the server
@@ -241,11 +247,8 @@ class PlanEditorController {
         this.equipmentIdToBoundaryId[equipmentObjectId] = feature.objectId
         this.createMapObjects && this.createMapObjects([feature])
         
-        this.boundaryInfoById[feature.objectId] = {
-          'coverageInfo': result.data.coverageInfo
-        }
-        
-        this.chartBoundaryCoverage(result.data.coverageInfo)
+        this.digestBoundaryCoverage(feature.objectId, result.data)
+        //this.showCoverageChart(feature.objectId)
         
         console.log(result)
       })
@@ -253,13 +256,173 @@ class PlanEditorController {
   }
   
   
-  
-  
-  
-  
-  chartBoundaryCoverage(coverageInfo){
+  digestBoundaryCoverage(objectId, boundaryData){
+    var boundsCoverage = {}
+    boundsCoverage.boundaryData = boundaryData
+    var locations = []
+    var censusBlockCountById = {}
+    var barChartData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
+    for (var localI=0; localI<boundaryData.coverageInfo.length; localI++){
+      var location = boundaryData.coverageInfo[localI]
+      if ("number" != typeof location.distance) continue // skip these 
+      
+      locations.push(location)
+      if (!censusBlockCountById.hasOwnProperty(location.censusBlockId)){
+        censusBlockCountById[location.censusBlockId] = 0
+      }
+      censusBlockCountById[location.censusBlockId]++
+      var dist = location.distance
+      var barIndex = Math.floor(dist / 1000)
+      if (barIndex >= barChartData.length || 'undefined' == typeof barChartData[barIndex]){
+        barChartData[barIndex] = 0
+      }
+      barChartData[barIndex]++
+    }
+    
+    
+    boundsCoverage.boundaryData.coverageInfo = locations
+    boundsCoverage.censusBlockCountById = censusBlockCountById
+    boundsCoverage.barChartData = barChartData
+    
+    this.boundaryCoverageById[objectId] = boundsCoverage
+    
+    this.getCensusTagsForBoundaryCoverage(objectId)
+    
+    console.log(boundsCoverage.censusBlockCountById)
   }
+  
+  getCensusTagsForBoundaryCoverage(objectId){
+    console.log(this.censusCategories)
+    //this.boundaryCoverageById[objectId]
+    
+    var censusBlockIds = Object.keys(this.boundaryCoverageById[objectId].censusBlockCountById)
+    if (censusBlockIds.length > 0){
+      //id eq 61920 or id eq 56829
+      var filter = ''
+      for (var cbI=0; cbI<censusBlockIds.length; cbI++){
+        if (cbI>0){
+          filter += ' or '
+        }
+        filter += 'id eq '+censusBlockIds[cbI]
+      }
+      var entityListUrl = `/service/odata/censusBlocksEntity?$select=id,tagInfo&$filter=${filter}`
+      this.$http.get(entityListUrl).then((results) => {
+        console.log(results)
+        var censusTagsByCat = {}
+        // iterate through each censusblock
+        for (var rowI=0; rowI<results.data.length; rowI++){
+          var row = results.data[rowI]
+          console.log(row)
+          var tagInfo = this.formatCensusBlockData(row.tagInfo)
+          console.log(tagInfo)
+          
+          // iterate through each category of the CB
+          Object.keys(tagInfo).forEach((catId) => {
+            var tagIds = tagInfo[catId]
+            if (!censusTagsByCat.hasOwnProperty(catId)){
+              censusTagsByCat[catId] = {}
+              censusTagsByCat[catId].description = this.censusCategories[catId].description
+              censusTagsByCat[catId].tags = {}
+            }
+            
+            // iterate through each tag of the category 
+            tagIds.forEach((tagId) => {
+              if (!censusTagsByCat[catId].tags.hasOwnProperty(tagId)){
+                censusTagsByCat[catId].tags[tagId] = {}
+                censusTagsByCat[catId].tags[tagId].description = this.censusCategories[catId].tags[tagId].description
+                censusTagsByCat[catId].tags[tagId].count = 0
+              }
+              console.log(this.boundaryCoverageById[objectId].censusBlockCountById[row.id])
+              censusTagsByCat[catId].tags[tagId].count += this.boundaryCoverageById[objectId].censusBlockCountById[row.id]
+            })
+            
+          })
+        }
+        
+        console.log(censusTagsByCat)
+        this.boundaryCoverageById[objectId].censusTagsByCat = censusTagsByCat
+      })
+      
+    }else{
+      this.boundaryCoverageById[objectId].censusTagsByCat = {}
+    }
+  }
+  
+  // ToDo: very similar to the code in tile-data-service.js
+  formatCensusBlockData(tagData){
+    var sepA = ';'
+    var sepB = ':'
+    var kvPairs = tagData.split( sepA )
+    var tags = {}
+    kvPairs.forEach((pair) => {
+      var kv = pair.split( sepB )
+      // incase there are extra ':'s in the value we join all but the first together 
+      if ("" != kv[0]) tags[ ""+kv[0] ] = kv.slice(1)
+    }) 
+    return tags 
+  }
+  
+  showCoverageChart(){
+    var objectId = this.selectedMapObject.objectId
+    //this.boundaryCoverageById[objectId]
+    var ctx = document.getElementById('plan-editor-bounds-dist-chart').getContext('2d');
+    
+    var data = this.boundaryCoverageById[objectId].barChartData
+    var labels = []
+    for (var i=0; i<data.length; i++){
+      labels.push((i+1)*1000)
+    }
+    
+    var settingsData = {
+      labels: labels,
+      datasets: [{
+          label: "residential",
+          backgroundColor: '#76c793',
+          borderColor: '#76c793',
+          data: data
+      }]
+    }
+    
+    var options = {
+      title: {
+        display: true,
+        text: 'Locations by Distance'
+      },
+      legend: {
+        display: true,
+        position: 'bottom'
+      }, 
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'locations'
+          }
+        }], 
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'distance, '+this.configuration.units.length_units, 
+            gridLines: {
+              offsetGridLines: false
+            }
+          }
+        }]
+      }     
+    }
+    
+    console.log(data)
+    var coverageChart = new Chart(ctx, {
+      type: 'bar',
+      data: settingsData,
+      options: options
+    });
+  }
+  
+  
+  
+  // --- //
   
   commitTransaction() {
     if (!this.currentTransaction) {
--- app/public/javascripts/src/models/state.js
+++ app/public/javascripts/src/models/state.js
@@ -1315,7 +1315,8 @@ app.service('state', ['$rootScope', '$http', '$document', '$timeout', 'map_layer
     CensusBlocksEntity: []
   }
   service.loadEntityList = (entityType,filterObj,select,searchColumn) => {
-    
+    // the query is currently causing a DB query that never returns and kills resources. 
+    /*
     var entityListUrl = `/service/odata/${entityType}?$select=${select}&$orderby=id&$top=10`
 
     var filter = ''
@@ -1355,7 +1356,7 @@ app.service('state', ['$rootScope', '$http', '$document', '$timeout', 'map_layer
     .then((results) => {
       service.entityTypeList[entityType] = results.data
     })
-
+    */
   }
 
   return service
