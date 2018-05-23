1f291ff6ee54372c7438b6f9f270e7871ac6e39f
Parag Patwardhan <parag@metronconsulting.com>
2018-05-23 15:40:43 +0530

    Get Administrator flag from the new endpoints

--- app/public/javascripts/src/components/global-settings/global-settings.js
+++ app/public/javascripts/src/components/global-settings/global-settings.js
@@ -1,6 +1,6 @@
 class GlobalSettingsController {
 
-  constructor(state,globalSettingsService) {
+  constructor(state, globalSettingsService, $http) {
     this.state = state
     this.globalSettingsService = globalSettingsService
     this.currentUser = globalUser
@@ -14,6 +14,22 @@ class GlobalSettingsController {
       MANAGE_GROUPS: 5
     })
     this.currentView = this.views.GLOBAL_SETTINGS
+    this.isAdministrator = false
+    var userAdminPermissions = null
+    $http.get('/service/auth/permissions')
+      .then((result) => {
+        // Get the permissions for the name USER_ADMIN
+        userAdminPermissions = result.data.filter((item) => item.name === 'USER_ADMIN')[0].id
+        return $http.get(`/service/auth/acl/SYSTEM/1`)
+      })
+      .then((result) => {
+        // Get the acl entry corresponding to the currently logged in user
+        var userAcl = result.data.resourcePermissions.filter((item) => item.systemActorId === state.getUserId())[0]
+        // The userAcl.rolePermissions is a bit field. If it contains the bit for "userAdminPermissions" then
+        // the logged in user is an administrator.
+        this.isAdministrator = (userAcl.rolePermissions & userAdminPermissions) > 0
+      })
+      .catch((err) => console.error(err))
   }
 
   modalShown() {
@@ -46,7 +62,7 @@ class GlobalSettingsController {
 
 }
 
-GlobalSettingsController.$inject = ['state','globalSettingsService']
+GlobalSettingsController.$inject = ['state', 'globalSettingsService', '$http']
 
 let globalSettings = {
   templateUrl: '/components/global-settings/global-settings.html',
