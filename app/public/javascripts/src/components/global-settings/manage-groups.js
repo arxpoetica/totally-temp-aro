361305656bb394ebd67603d21e0942a0e155c9d4
Parag Patwardhan <parag@metronconsulting.com>
2018-05-23 15:40:43 +0530

    Basic CRUD for groups

--- app/public/javascripts/src/components/global-settings/manage-groups.js
+++ app/public/javascripts/src/components/global-settings/manage-groups.js
@@ -1,23 +1,110 @@
-import MockService from './mockService'
-
 class ManageGroupsController {
 
   constructor($http) {
+    this.$http = $http
+    this.groups = []
+    this.loadGroups()
+  }
 
-    this.groups = [
-      {
-        name: 'Administrators',
-        isAdmin: true
-      },
-      {
-        name: 'CAF Planners',
-        isAdmin: false
-      },
-      {
-        name: 'Ohio CAF PLanners',
-        isAdmin: false
-      }
-    ]
+  loadGroups() {
+    var userAdminPermissions = null
+    this.$http.get('/service/auth/permissions')
+      .then((result) => {
+        // Get the permissions for the name USER_ADMIN
+        userAdminPermissions = result.data.filter((item) => item.name === 'USER_ADMIN')[0].id
+        return Promise.all([
+          this.$http.get(`/service/auth/acl/SYSTEM/1`),
+          this.$http.get('/service/auth/groups')
+        ])
+      })
+      .then((result) => {
+        var acls = result[0].data
+        this.groups = result[1].data
+        var groupIdToGroup = {}
+        this.groups.forEach((group) => groupIdToGroup[group.id] = group)
+        // For each group, we want to determine whether the "Administrator" flag should be set.
+        // The "userAdminPermissions" is a bit flag. When set, then the system actor (user/group) is an administrator.
+        acls.resourcePermissions.forEach((resourcePermission) => {
+          const isAdministrator = (resourcePermission.rolePermissions & userAdminPermissions) > 0
+          if (groupIdToGroup.hasOwnProperty(resourcePermission.systemActorId)) {
+            groupIdToGroup[resourcePermission.systemActorId].isAdministrator = isAdministrator
+          }
+        })
+      })
+      .catch((err) => console.error(err))
+  }
+
+  addGroup() {
+    // Create a group in aro-service and then add it to our groups list. This ensures we will have a valid group id.
+    // Don't do anything with ACL as the default is a non-administrator group
+    this.groups.forEach((group) => group.isEditing = false)
+    this.$http.post('/service/auth/groups', {
+      name: `Group ${Math.round(Math.random() * 10000)}`, // Try to not have a duplicate group name
+      description: 'Group Description'
+    })
+      .then((result) => {
+        var group = result.data
+        group.isEditing = true
+        this.groups.push(group)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  saveGroup(group) {
+    // First save the group name and description
+    group.isEditing = false
+    var userAdminPermissions = null
+    this.$http.put('/service/auth/groups', {
+      id: group.id,
+      name: group.name,
+      description: group.description
+    })
+      .then((result) => {
+        return this.$http.get('/service/auth/permissions')
+      })
+      .then((result) => {
+        // Get the permissions for the name USER_ADMIN
+        userAdminPermissions = result.data.filter((item) => item.name === 'USER_ADMIN')[0].id
+        return this.$http.get('/service/auth/acl/SYSTEM/1')
+      })
+      .then((result) => {
+        var acls = result.data
+        var groupIsInACLList = false
+        acls.resourcePermissions.forEach((item, index) => {
+          var resourcePermission = acls.resourcePermissions[index]
+          if (resourcePermission.systemActorId === group.id) {
+            // This resource permission is for the group being saved
+            groupIsInACLList = true
+            if (group.isAdministrator) {
+              // Set the admin flag. https://stackoverflow.com/a/1436448
+              resourcePermission.rolePermissions |= userAdminPermissions
+            } else {
+              // Remove the admin flag. https://stackoverflow.com/a/1436448
+              resourcePermission.rolePermissions &= (~userAdminPermissions)
+            }
+          }
+        })
+        // In case the group is not in the ACL list at all, AND if the group is an administrator, add it to the list.
+        if (!groupIsInACLList && group.isAdministrator) {
+          acls.resourcePermissions.push({
+            systemActorId: group.id,
+            rolePermissions: userAdminPermissions
+          })
+        }
+        // Our resource permissions may have been modifed. Save the whole lot.
+        return this.$http.put('/service/auth/acl/SYSTEM/1', acls)
+      })
+      .then((result) => this.loadGroups())
+      .catch((err) => {
+        group.isEditing = false
+        console.error(err)
+      })
+  }
+
+  deleteGroup(group) {
+    this.$http.delete(`/service/auth/groups/${group.id}`)
+      .then((result) => this.loadGroups())
+      .catch((err) => console.error(err))
   }
 }
 
