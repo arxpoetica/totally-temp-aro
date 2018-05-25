c18b44d2bc169a8803b744b071ff991d9f28506b
dileep <dileep@metronconsulting.com>
2018-05-25 16:36:41 +0530

    deselect boundaries in plan editing

--- app/public/javascripts/src/components/sidebar/plan-editor/plan-editor.js
+++ app/public/javascripts/src/components/sidebar/plan-editor/plan-editor.js
@@ -86,12 +86,6 @@ class PlanEditorController {
   $onInit() {
     // Select the first boundary in the list
     this.resumeOrCreateTransaction()
-
-    this.state.clearEditingMode.skip(1).subscribe((clear) => {
-      if (clear) {
-        this.state.showSiteBoundary = false
-      }
-    })
   }
 
   resumeOrCreateTransaction() {
