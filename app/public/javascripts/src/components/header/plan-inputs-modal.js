e607e2f352d1d4c0bf15a8425d64a7d54165356c
Parag Patwardhan <parag@metronconsulting.com>
2018-07-24 09:16:59 +0530

    Moved markup to html file

--- app/public/javascripts/src/components/header/plan-inputs-modal.js
+++ app/public/javascripts/src/components/header/plan-inputs-modal.js
@@ -79,45 +79,7 @@ class PlanInputsModalController {
 PlanInputsModalController.$inject = ['state','$element']
 
 let planInputsModal = {
-  template: `
-  <style scoped>
-  .with-margin {
-    margin-bottom: 5px;
-  }
-  </style>
-  <modal id="plan_inputs_modal" visible="$ctrl.state.planInputsModal.value" backdrop="static" on-show="$ctrl.modalShown()" on-hide="$ctrl.modalHide()" >
-    <modal-header title="Plan Inputs"></modal-header>
-      <modal-body>
-        <input class="form-control with-margin" type="text" ng-model="$ctrl.planName" placeholder="Plan Name">
-        <div class="with-margin"> 
-        <edit-plan-tag
-          object-name="Tag"
-          search-list="$ctrl.state.listOfTags"
-          selected-list="$ctrl.state.currentPlanTags"></edit-plan-tag>
-        </div>
-        <edit-plan-tag
-          object-name="Service Area"
-          search-list="$ctrl.state.listOfServiceAreaTags"
-          selected-list="$ctrl.state.currentPlanServiceAreaTags"
-          refresh-tag-list="$ctrl.state.loadListOfSAPlanTags(searchObj)"></edit-plan-tag>
-        <div ng-click="$ctrl.parentPlanSelectorExpanded = !$ctrl.parentPlanSelectorExpanded" style="margin-top: 10px; cursor: pointer;">
-          Parent plan: {{$ctrl.parentPlan ? $ctrl.parentPlan.name : '(undefined)'}}
-          <button class="btn btn-xs btn-default" ng-click="$ctrl.clearParentPlan(); $event.stopPropagation();">Clear</button>
-          <div class="pull-right"><i ng-class="{ 'fa': true, 'fa-plus': !$ctrl.parentPlanSelectorExpanded, 'fa-minus': $ctrl.parentPlanSelectorExpanded }"></i></div>
-        </div>
-        <div ng-style="{ 'margin-left': '30px;', 'display': $ctrl.parentPlanSelectorExpanded ? 'block' : 'none', 'max-height': '300px;' }">
-          <plan-search show-plan-delete-button="false"
-                      show-refresh-plans-on-map-move="false"
-                      on-plan-selected="$ctrl.onParentPlanSelected(plan)">
-          </plan-search>
-        </div>
-      </modal-body>
-    <modal-footer>
-      <button class="btn btn-primary pull-left" ng-click="$ctrl.savePlanAs()">Create Plan</button>
-      <button class="btn btn-danger pull-right" ng-click="$ctrl.close()">Cancel</button>
-    </modal-footer>
-  </modal>
-  `,
+  templateUrl: '/components/header/plan-inputs-modal.html',
   bindings: {},
   controller: PlanInputsModalController
 }
