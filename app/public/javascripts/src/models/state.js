c19f3cf014f953321c9370ea68cfedadc3536a12
Parag Patwardhan <parag@metronconsulting.com>
2018-08-03 17:11:39 +0530

    Fixed service area odata query

--- app/public/javascripts/src/models/state.js
+++ app/public/javascripts/src/models/state.js
@@ -1429,16 +1429,20 @@ app.service('state', ['$rootScope', '$http', '$document', '$timeout', '$sce', 'm
   service.loadAllAssociatedSaPlanTags = (plans) => {
 
     var processedTags = new Set()
-    var filter = "layer/id eq 1"
+    var idFilter = ''
     plans.forEach((plan) => {
-      plan.tagMapping.linkTags.serviceAreaIds.forEach((tag) => {
+      const serviceAreaIds = plan.tagMapping.linkTags.serviceAreaIds
+      serviceAreaIds.forEach((tag) => {
         if (!processedTags.has(tag)) {
-          filter = filter.concat(` and id eq ${tag}`)
+          if (processedTags.size > 0) {
+            idFilter += ' or '
+          }
+          idFilter += `id eq ${tag}`
         }
         processedTags.add(tag)
       })
-    })  
-
+    })
+    const filter = `(layer/id eq 1) and (${idFilter})`
     $http.get(`/service/odata/servicearea?$select=id,code&$filter=${filter}&$top=${MAX_SERVICE_AREAS_FROM_ODATA}`)
       .then((result) => {
         service.listOfServiceAreaTags = service.removeDuplicates(service.listOfServiceAreaTags.concat(result.data), 'id')
