b4d56e313a7156fda4273bcb0b797ead2a364da3
Parag Patwardhan <parag@metronconsulting.com>
2018-08-28 16:49:01 +0530

    Added ability to show additional feature properties

--- app/public/javascripts/src/components/sidebar/debug/view-settings.js
+++ app/public/javascripts/src/components/sidebar/debug/view-settings.js
@@ -1,11 +1,14 @@
 class ViewSettingsController {
 
-  constructor(state, $rootScope) {
+  constructor(state, configuration, $rootScope) {
     this.state = state
+    this.configuration = configuration
     this.$rootScope = $rootScope
 
     this.state.viewSetting.selectedFiberOption = this.state.viewFiberOptions[0]
     this.mapTileOptions
+
+    this.equipmentPropertiesToRender = JSON.stringify(this.configuration.networkEquipment.labelDrawingOptions.properties)
 
     // Map tile settings used for debugging
     this.state.mapTileOptions
@@ -16,6 +19,12 @@ class ViewSettingsController {
     this.$rootScope.$broadcast("map_setting_changed", { type: "fiber_option", setting: this.state.viewSetting.selectedFiberOption });
   }
 
+  saveEquipmentPropertiesToRender() {
+    this.configuration.networkEquipment.labelDrawingOptions.properties = JSON.parse(this.equipmentPropertiesToRender)
+    this.state.viewSettingsChanged.next()
+    this.state.requestMapLayerRefresh.next(null)
+  }
+
   // Take the mapTileOptions defined in $scope and set it on the state
   updateState() {
     var newMapTileOptions = angular.copy(this.mapTileOptions)
@@ -23,7 +32,7 @@ class ViewSettingsController {
   }
 }
 
-ViewSettingsController.$inject = ['state', '$rootScope']
+ViewSettingsController.$inject = ['state', 'configuration', '$rootScope']
 
 let viewSettings = {
   templateUrl: '/components/sidebar/debug/view-settings.html',
