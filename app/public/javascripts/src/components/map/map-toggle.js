231c198db9cf8010ce3b06bf70e2fa077d6381e0
Parag Patwardhan <parag@metronconsulting.com>
2018-10-01 15:20:31 +0530

    Map type was wrong when app was first loaded. Fixed.

--- app/public/javascripts/src/components/map/map-toggle.js
+++ app/public/javascripts/src/components/map/map-toggle.js
@@ -1,8 +1,15 @@
 class MapToggleController {
-  constructor($document, configuration) {
-    this.mapRef = null
+  constructor($document, $rootScope, configuration) {
+    this.mapRefPromise = null
     this.$document = $document
-    this.configuration = configuration
+    if (configuration.mapType) {
+      // Configuration has already been loaded
+      this.configurationPromise = Promise.resolve(configuration)
+    } else {
+      this.configurationPromise = new Promise((resolve, reject) => {
+        $rootScope.$on('configuration_loaded', () => resolve(configuration))
+      })
+    }
     // Hold a map of 'mapTypeId' in state.js to the fontawesome icons
     this.buttonIcons = {
       hybrid: 'fa-globe',
@@ -12,47 +19,63 @@ class MapToggleController {
     this.overridenMapType = null    // Used if the user manually clicks on a map type
   }
 
+  $onInit() {
+    this.ensureMapRefPromiseCreated()
+  }
+
+  ensureMapRefPromiseCreated() {
+    if (!this.mapRefPromise) {
+      this.mapRefPromise = new Promise((resolve, reject) => {
+        this.$document.ready(()=>{
+          if (!this.mapGlobalObjectName) {
+            reject('ERROR: You must specify the name of the global variable that contains the map object.')
+          }
+      
+          // We should have a map variable at this point
+          resolve(window[this.mapGlobalObjectName])
+        })
+      })
+    }
+  }
+
   toggle() {
     this.currentMapType = (this.currentMapType === 'hybrid') ? 'roadmap' : 'hybrid'
     this.overridenMapType = this.currentMapType
-    this.mapRef.setMapTypeId(this.currentMapType)
+    this.mapRefPromise
+      .then((mapRef) => mapRef.setMapTypeId(this.currentMapType))
+      .catch((err) => console.log(err))
   }
 
   updateMapType() {
-    if (!this.mapRef) {
-      return  // Need a map ref object to set the type
-    }
     if (this.overridenMapType) {
       // The user has overriden the map type. Use it.
-      this.mapRef.setMapTypeId(this.overridenMapType)
+      this.mapRefPromise
+        .then((mapRef) => mapRef.setMapTypeId(this.overridenMapType))
+        .catch((err) => console.log(err))
     } else {
       // Depending upon the user perspective, set the map type on the map object
-      this.currentMapType = this.configuration.mapType[this.userPerspective] || this.configuration.mapType.default
-      this.mapRef.setMapTypeId(this.currentMapType)
+      Promise.all([this.mapRefPromise, this.configurationPromise])
+        .then((results) => {
+          const mapRef = results[0]
+          const configuration = results[1]
+          this.currentMapType = configuration.mapType[this.userPerspective] || configuration.mapType.default
+          mapRef.setMapTypeId(this.currentMapType)
+        })
+        .catch((err) => console.log(err))
     }
-  }
-
-  $onInit() {
-    this.$document.ready(()=>{
-      if (!this.mapGlobalObjectName) {
-        console.error('ERROR: You must specify the name of the global variable that contains the map object.')
-      }
-  
-      // We should have a map variable at this point
-      this.mapRef = window[this.mapGlobalObjectName]
-    })
   }
 
   $onChanges(changesObj) {
     if (changesObj && changesObj.userPerspective) {
       // User perspective has changed. Set the overriden configuration to null
       this.overridenMapType = null
+      this.ensureMapRefPromiseCreated()  // In case it has not been created yet
       this.updateMapType()
     }
   }
 }
 
-MapToggleController.$inject = ['$document', 'configuration']
+MapToggleController.$inject = ['$document', '$rootScope', 'configuration']
 
 let mapToggle = {
   template:'<button class="map-toggle" ng-click="$ctrl.toggle()"><i class="fa {{$ctrl.buttonIcons[$ctrl.currentMapType]}}"></i></button>',
