f55e3715d9d1c0e180fed72575bf5bd2e74321fb
Parag Patwardhan <parag@metronconsulting.com>
2018-10-01 15:20:31 +0530

    Initial map type now based on user perspective and ARO_CLIENT

--- app/public/javascripts/src/components/map/map-toggle.js
+++ app/public/javascripts/src/components/map/map-toggle.js
@@ -1,29 +1,48 @@
 class MapToggleController {
-  constructor($document){
-    this.map = map;
-    this.buttonIcon = this.map.mapTypeId === 'hybrid' ? 'fa-globe' : 'fa-road'
+  constructor($document) {
+    this.mapRef = null
+    this.$document = $document
+    // Hold a map of 'mapTypeId' in state.js to the fontawesome icons
+    this.buttonIcons = {
+      hybrid: 'fa-globe',
+      roadmap: 'fa-road'
+    }
   }
 
   toggle(){
-    this.map.mapTypeId === 'hybrid' ? this.toStreets() : this.toStatellite()
+    const newMapTypeId = (this.mapTypeId === 'hybrid') ? 'roadmap' : 'hybrid'
+    this.setMapTypeId && this.setMapTypeId({ mapTypeId: newMapTypeId })
   }
 
-  toStatellite() {
-    this.map.setMapTypeId('hybrid')
-    this.buttonIcon = 'fa-globe'
+  $onInit() {
+    this.$document.ready(()=>{
+      if (!this.mapGlobalObjectName) {
+        console.error('ERROR: You must specify the name of the global variable that contains the map object.')
+      }
+  
+      // We should have a map variable at this point
+      this.mapRef = window[this.mapGlobalObjectName]
+    })
   }
 
-  toStreets(){
-    this.map.setMapTypeId('roadmap')
-    this.buttonIcon = 'fa-road'
+  $onChanges(changesObj) {
+    if (changesObj && changesObj.mapTypeId) {
+      if (this.mapRef) {  // Make sure mapRef has been initialized
+        this.mapRef.setMapTypeId(changesObj.mapTypeId.currentValue)
+      }
+    }
   }
 }
 
 MapToggleController.$inject = ['$document']
 
 let mapToggle = {
-  template:'<button class="map-toggle" ng-click="$ctrl.toggle()"><i class="fa {{$ctrl.buttonIcon}}"></i></button>',
-  bindings: {},
+  template:'<button class="map-toggle" ng-click="$ctrl.toggle()"><i class="fa {{$ctrl.buttonIcons[$ctrl.mapTypeId]}}"></i></button>',
+  bindings: {
+    mapGlobalObjectName: '@',
+    mapTypeId: '<',
+    setMapTypeId: '&'
+  },
   controller:MapToggleController
 };
 
