5934762cd6c728856e09c7bed491047bbb436174
Parag Patwardhan <parag@metronconsulting.com>
2018-10-26 08:25:53 -0400

    Set up initial data for creating coverage reports

--- app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-initializer.js
+++ app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-initializer.js
@@ -1,9 +1,31 @@
 class CoverageInitializerController {
-  constructor() {
+  constructor(state) {
+    this.state = state
+    this.reportTypes = [
+      { id: 'FORM_477', name: 'Form 477' },
+      { id: 'REGULAR', name: 'Regular' }
+    ]
+    this.spatialEdgeTypes = [
+      { id: 'road', name: 'Roads' },
+      { id: 'copper', name: 'Copper' },
+      { id: 'fiber', name: 'Fiber' }
+    ]
+    this.coverageTypes = [
+      { id: 'census_block', name: 'Census Blocks' },
+      { id: 'location', name: 'Locations' }
+    ]
+    this.coveragePlan = {
+      coverageType: 'census_block',
+      name: 'Coverage Plan',
+      distanceThreshold: 20000,
+      useSiteBoundaries: false,
+      spatialEdgeType: 'road'
+    }
+    this.coveragePlan.selectedReportType = this.reportTypes.filter((item) => item.id === 'FORM_477')[0]
   }
 }
 
-CoverageInitializerController.$inject = []
+CoverageInitializerController.$inject = ['state']
 
 let coverageInitializer = {
   templateUrl: '/components/sidebar/analysis/coverage/coverage-initializer.html',
