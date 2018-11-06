b2d85c84cbb365078c35ac438aceca70aec3625f
Parag Patwardhan <parag@metronconsulting.com>
2018-11-06 15:10:07 +0530

    Can select service areas or analysis areas when initializing coverage report

--- app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-initializer.js
+++ app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-initializer.js
@@ -18,6 +18,55 @@ class CoverageInitializerController {
       spatialEdgeType: 'road'
     }
     this.isInitializingReport = false
+    this.serviceAreas = []
+    this.analysisAreas = []
+    this.selectionModeLabels = {}
+    this.selectionModeLabels[state.selectionModes.SELECTED_AREAS] = 'Service Areas'
+    this.selectionModeLabels[state.selectionModes.SELECTED_ANALYSIS_AREAS] = 'Analysis Areas'
+    this.selectionModeLabels[state.selectionModes.SELECTED_LOCATIONS] = 'Locations'
+
+    this.serviceAreasObserver = state.selectedServiceAreas.subscribe((selectedServiceAreas) => {
+      // The selected SA have changed.
+      if (state.optimizationOptions.analysisSelectionMode != state.selectionModes.SELECTED_AREAS) return
+      //console.log(selectedServiceAreas)
+      var serviceAreaIds = Array.from(selectedServiceAreas)
+      $http.post('/network_plan/service_area/addresses', { serviceAreaIds: serviceAreaIds })
+      .then((result) => {
+        this.serviceAreas = result.data
+      })
+      .catch(err => console.error(err))
+    })  
+    
+    this.analysisAreasObserver = state.selectedAnalysisAreas.subscribe((selectedAnalysisAreas) => {
+      // The selected analysis areas have changed.
+      if (state.optimizationOptions.analysisSelectionMode != state.selectionModes.SELECTED_ANALYSIS_AREAS) return
+      var analysisAreaIds = Array.from(selectedAnalysisAreas)
+      $http.post('/network_plan/analysis_area/addresses', { analysisAreaIds: analysisAreaIds })
+      .then((result) => {
+        this.analysisAreas = result.data
+      })
+      .catch(err => console.error(err))
+    })
+  }
+
+  onSelectionTypeChange(selectionType) {
+    this.state.selectionTypeChanged.next(selectionType)
+  } 
+
+  removeServiceAreas(target) {
+    $http.post(`/service_areas/${this.plan.id}/removeServiceAreaTargets`, { serviceAreaIds: target.map((sa) => sa.id) })
+    .then((response) => {
+      this.state.reloadSelectedServiceAreas()
+    })
+    .catch(err => console.error(err))
+  }
+
+  removeAnalysisAreas(target) {
+    $http.post(`/analysis_areas/${this.plan.id}/removeAnalysisAreaTargets`, { analysisAreaIds: target.map((sa) => sa.id) })
+    .then((response) => {
+      this.state.reloadSelectedAnalysisAreas()
+    })
+    .catch(err => console.error(err))
   }
 
   initializeCoverageReport() {
@@ -28,6 +77,22 @@ class CoverageInitializerController {
     serviceCoveragePlan.coverageAnalysisRequest.planId = this.planId
     serviceCoveragePlan.coverageAnalysisRequest.projectTemplateId = this.state.loggedInUser.projectId
     serviceCoveragePlan.coverageAnalysisRequest.distanceThreshold = this.coveragePlan.distanceThreshold * this.state.configuration.units.length_units_to_meters
+    serviceCoveragePlan.coverageAnalysisRequest.analysisSelectionMode = this.state.optimizationOptions.analysisSelectionMode
+    if (this.state.optimizationOptions.analysisSelectionMode === this.state.selectionModes.SELECTED_ANALYSIS_AREAS) {
+      // If we have analysis areas selected, we can have exactly one analysis layer selected in the UI
+      const visibleAnalysisLayers = this.state.boundaries.tileLayers.filter(item => item.visible && (item.type === 'analysis_layer'))
+      if (visibleAnalysisLayers.length !== 1) {
+        const errorMessage = 'You must have exactly one analysis layer selected to initialize the coverage report'
+        swal({
+          title: 'Analysis Layer error',
+          text: errorMessage,
+          type: 'error',
+          closeOnConfirm: true
+        })
+        throw errorMessage
+      }
+      serviceCoveragePlan.coverageAnalysisRequest.analysisLayerId = visibleAnalysisLayers[0].analysisLayerId
+    }
     var createdCoveragePlan = null
     this.isInitializingReport = true
     this.$http.post(`/service/coverage/report`, serviceCoveragePlan)
@@ -42,6 +107,11 @@ class CoverageInitializerController {
         this.$timeout()
       })
       .catch(err => console.error(err))
+  }
+
+  $onDestroy() {
+    this.serviceAreasObserver.unsubscribe()
+    this.analysisAreasObserver.unsubscribe()
   }
 }
 
