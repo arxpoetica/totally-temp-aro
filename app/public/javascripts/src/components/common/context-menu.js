b80d105f27e21fa7ff594fc14f1089b189a6c27b
theUtilitariat <theUtilitariat@gmail.com>
2018-11-13 22:18:42 -0500

    context menu overflow fix, done

--- app/public/javascripts/src/components/common/context-menu.js
+++ app/public/javascripts/src/components/common/context-menu.js
@@ -1,7 +1,8 @@
 
 class ContextMenuController {
   
-  constructor($element, $timeout, contextMenuService){
+  constructor($scope, $element, $timeout, contextMenuService){
+    this.$scope = $scope
     this.$element = $element 
     this.$timeout = $timeout
     this.contextMenuService = contextMenuService
@@ -14,10 +15,19 @@ class ContextMenuController {
       top: '100px',
       left: '100px'
     }
+    this.positionCss = {
+      bottom: 'auto'
+    }
     this.menuItems = []
+    this.isVisible = false
   }
   
   $onInit(){
+    /*
+    this.$timeout(() => {
+      console.log('on time out')
+    }, 0)
+    */
     this.contextMenuService.isMenuVisible.subscribe((isMenuVisible) => {
       if (isMenuVisible){
         this.openMenu()
@@ -30,20 +40,25 @@ class ContextMenuController {
       if ('undefined' == typeof menuXY) return
       if ('undefined' != typeof menuXY.x) this.contextMenuCss.left = `${menuXY.x}px`
       if ('undefined' != typeof menuXY.y) this.contextMenuCss.top = `${menuXY.y}px`
+      this.refreshView()
     })
     
     this.contextMenuService.menuItems.subscribe((menuItems) => {
       this.menuItems = menuItems
+      this.refreshView()
     })
   }
   
   // --- 
   
   openMenu(){
+    this.isVisible = true
     this.contextMenuCss.display = 'block'
+    this.refreshView()
   }
   
   closeMenu(){
+    this.isVisible = false
     this.contextMenuCss.display = 'none'
   }
   
@@ -52,10 +67,28 @@ class ContextMenuController {
     callback(data)
   }
   
+  refreshView(){
+    if (!this.isVisible) return
+    
+    try{
+      if(!this.$scope.$$phase) this.$scope.$digest()
+    }catch(error){
+      // we can silent fail this 
+    }
+    
+    var parentH = this.$element.find('.context-menu-dropdown')[0].offsetHeight
+    var contentH = this.$element.find('.map-object-editor-context-menu-dropdown')[0].offsetHeight
+    if (contentH >= parentH) {
+      this.positionCss.bottom = '0'
+    }else{
+      this.positionCss.bottom = 'auto'
+    }
+  }
+  
 }
 
 
-ContextMenuController.$inject = ['$element', '$timeout', 'contextMenuService']
+ContextMenuController.$inject = ['$scope', '$element', '$timeout', 'contextMenuService']
 
 let contextMenu = {
   templateUrl: '/components/common/context-menu.html',
