5e15c3b113c2473462e753db122bb67c68a5b449
Parag Patwardhan <parag@metronconsulting.com>
2018-11-26 16:10:14 +0530

    Added descriptions to impedance manager mapping

--- app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/impedance-editor.js
+++ app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/impedance-editor.js
@@ -3,6 +3,16 @@ class ImpedanceEditorController {
     this.$http = $http
     this.impedanceManagerConfiguration = []
     this.impedanceManagerName = ''
+    // "mappingLabels" should map from a impedance mapping key (e.g. 0) to a text description of the mapping
+    this.mappingLabels = {
+      '-1': 'Mapping -1',
+      0: 'Mapping 0',
+      1: 'Mapping 1',
+      2: 'Mapping 2',
+      3: 'Mapping 3',
+      4: 'Mapping 4',
+      5: 'Mapping 5'
+    }
   }
 
   $onChanges(changesObj) {
@@ -19,12 +29,25 @@ class ImpedanceEditorController {
     })
 
     this.$http.get(`/service/v1/impedance-manager/${this.impedanceManagerId}/configuration`)
-    .then((result) => this.impedanceManagerConfiguration = result.data)
+    .then((result) => {
+      this.impedanceManagerConfiguration = result.data
+      // The map is a set of key value pairs, we convert it to a sorted array
+      var impedanceMapArray = Object.keys(this.impedanceManagerConfiguration.map)
+                                .map(impKey => { return { key: impKey, value: this.impedanceManagerConfiguration.map[impKey] } })
+                                .sort((a, b) => (a.key < b.key) ? -1 : 1)
+      delete this.impedanceManagerConfiguration.map
+      this.impedanceManagerConfiguration.impedanceMapArray = impedanceMapArray
+    })
     .catch((err) => console.error(err))
   }
 
   saveConfigurationToServer() {
-    this.$http.put(`/service/v1/impedance-manager/${this.impedanceManagerId}/configuration`, this.impedanceManagerConfiguration)
+    // Convert the impedance array to an object that aro-service can use
+    var serviceConfiguration = angular.copy(this.impedanceManagerConfiguration)
+    serviceConfiguration.map = {}
+    this.impedanceManagerConfiguration.impedanceMapArray.forEach((item) => serviceConfiguration.map[item.key] = item.value)
+    delete serviceConfiguration.impedanceMapArray
+    this.$http.put(`/service/v1/impedance-manager/${this.impedanceManagerId}/configuration`, serviceConfiguration)
     .then((result) => this.exitEditingMode())
     .catch((err) => console.error(err))
   }
