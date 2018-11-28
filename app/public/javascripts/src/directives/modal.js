e2832a598dc0652adc1e1d0c52cdcd1df69d86d4
Parag Patwardhan <parag@metronconsulting.com>
2018-11-28 17:08:34 +0530

    Added support for different modal sizes

--- app/public/javascripts/src/directives/modal.js
+++ app/public/javascripts/src/directives/modal.js
@@ -1,10 +1,10 @@
 app.directive('modal', function () {
   return {
-    template: '<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"><div class="modal-dialog modal-lg"><div class="modal-content" ng-transclude><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Modal title</h4></div></div></div></div>',
+    template: '<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content" ng-transclude><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Modal title</h4></div></div></div></div>',
     restrict: 'E',
     transclude: true,
     replace: true,
-    scope: { visible: '=', onShow: '&', onHide: '&' },
+    scope: { visible: '=', modalSize: "<", onShow: '&', onHide: '&' },
     link: function postLink(scope, element, attrs) {
 
       $(element).modal({
@@ -12,13 +12,21 @@ app.directive('modal', function () {
         backdrop: attrs.backdrop
       });
 
-      scope.$watch(function () { return scope.visible; }, function (value) {
+      var currentModalSize = attrs.modalSize || 'modal-md'
+      $(element).find('div').addClass(currentModalSize)
 
+      scope.$watch(function () { return scope.visible; }, function (value) {
         if (value == true) {
           $(element).modal('show');
         } else {
           $(element).modal('hide');
         }
+      });
+
+      scope.$watch(function () { return scope.modalSize; }, function (value) {
+        $(element).find('div').removeClass(currentModalSize)
+        currentModalSize = value
+        $(element).find('div').addClass(currentModalSize)
       });
 
       $(element).on('shown.bs.modal', function () {
