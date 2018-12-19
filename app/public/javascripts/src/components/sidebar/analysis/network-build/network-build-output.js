28a177ff003236d388512a4c4a7271fb355e1537
Parag Patwardhan <parag@metronconsulting.com>
2018-12-19 19:16:04 +0530

    Delete old code (financial profile summary tool)

--- app/public/javascripts/src/components/sidebar/analysis/network-build/network-build-output.js
+++ app/public/javascripts/src/components/sidebar/analysis/network-build/network-build-output.js
@@ -1,24 +1,15 @@
-import Constants from '../../../common/constants'
 class NetworkBuildOutputController {
 
-  constructor(state, map_tools) {
+  constructor(state) {
     this.state = state
-    this.map_tools = map_tools
   }
 
   showDetailedOutput() {
-    var financialProfileConfig = {
-      id: 'financial_profile',
-      name: 'Financial Profile',
-      short_name: 'F',
-      icon: 'fa fa-line-chart fa-2x'
-    }
-    this.map_tools.toggle(financialProfileConfig)
     this.state.showRoicReportsModal = true
   }
 }
 
-NetworkBuildOutputController.$inject = ['state', 'map_tools']
+NetworkBuildOutputController.$inject = ['state']
 
 let networkBuildOutput = {
   templateUrl: '/components/sidebar/analysis/network-build/network-build-output.html',
