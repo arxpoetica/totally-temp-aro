0b43505a7f92f87d45fff7d6ea4b3a3050529fb6
Parag Patwardhan <parag@metronconsulting.com>
2019-01-04 17:38:08 +0530

    Added support for deleting categories

--- app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/rate-reach-distance-editor.js
+++ app/public/javascripts/src/components/sidebar/plan-settings/plan-resource-selection/rate-reach-distance-editor.js
@@ -6,6 +6,32 @@ class RateReachDistanceEditorController {
 
     this.isCategoryInEditMode = {}  // For each category hold a flag that tells us if it is being edited
   }
+
+  addCategory() {
+    // Add a new category and also add placeholder values for the categories
+    const MAX_ID = Math.max.apply(Math, this.categories.map(item => item.id))
+    const newCategory = {
+      id: MAX_ID + 1,
+      name: 'New category',
+      description: 'New category'
+    }
+    this.categories.push(newCategory)
+    Object.keys(this.rateReachGroupMap).forEach(technology => {
+      Object.keys(this.rateReachGroupMap[technology].matrixInMetersMap).forEach(technologyRef => {
+        this.rateReachGroupMap[technology].matrixInMetersMap[technologyRef].push(1000)
+      })
+    })
+  }
+
+  removeCategory(category) {
+    const categoryIndex = this.categories.findIndex(item => item.id === category.id)
+    this.categories.splice(categoryIndex, 1)
+    Object.keys(this.rateReachGroupMap).forEach(technology => {
+      Object.keys(this.rateReachGroupMap[technology].matrixInMetersMap).forEach(technologyRef => {
+        this.rateReachGroupMap[technology].matrixInMetersMap[technologyRef].splice(categoryIndex, 1)
+      })
+    })
+  }
 }
 
 RateReachDistanceEditorController.$inject = ['$http', '$timeout', 'state']
@@ -16,7 +42,7 @@ let rateReachEditor = {
     categories: '=',
     rateReachGroupMap: '=',
     selectedTechnologyType: '<',
-    allowEditableCategory: '<'
+    allowEditableCategories: '<'
   },
   controller: RateReachDistanceEditorController
 }
