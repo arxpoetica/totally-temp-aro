0ab6026f988e52aa48d1e7bdd75108bcff205fdc
Parag Patwardhan <parag@metronconsulting.com>
2019-01-17 17:28:40 +0530

    Added support for Form477 report download

--- app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-report-downloader.js
+++ app/public/javascripts/src/components/sidebar/analysis/coverage/coverage-report-downloader.js
@@ -1,5 +1,3 @@
-import { timestamp } from "rxjs/operator/timestamp";
-
 class CoverageReportDownloaderController {
   constructor($http, $timeout, state, Utils) {
     this.$http = $http
@@ -10,69 +8,40 @@ class CoverageReportDownloaderController {
     this.coverageReport = null
     this.rateReachMatrices = []
     this.selectedRateReachMatrix = null
+    this.coverageReports = []
+  }
+
+  $onInit() {
     // Get the coverage report details
     this.$http.get('/service/rr/matrix')
-      .then((result) => {
-        this.rateReachMatrices = result.data
-        this.selectedRateReachMatrix = result.data[0]
-        this.$timeout()
+    .then((result) => {
+      this.rateReachMatrices = result.data
+      this.selectedRateReachMatrix = result.data[0]
+      return this.$http.get('/service/installed/report/meta-data')
+    })
+    .then(result => {
+      const now = new Date()
+      const timeStamp = `${now.getMonth() + 1}_${now.getDate()}_${now.getFullYear()}_${now.getHours()}_${now.getMinutes()}`
+      this.coverageReports = result.data.filter(item => item.reportType === 'COVERAGE')
+      this.coverageReports.forEach((item, index) => {
+        this.coverageReports[index].downloadUrl = `/report-extended/${item.name}/${this.state.plan.getValue().id}/${item.mediaType}`
+        this.coverageReports[index].downloadFilename = `Coverage_${timeStamp}.csv`
       })
-      .catch(err => console.error(err))
-
-    this.$http.get('/service/installed/report/meta-data')
-      .then(result => {
-        this.coverageReports = result.data.filter(item => item.reportType === 'COVERAGE')
-        this.coverageReports.forEach((item, index) => {
-          this.coverageReports[index].downloadUrl = `/report-extended/${item.name}/${this.state.plan.getValue().id}/${item.mediaType}`
-          const now = new Date()
-          const timeStamp = `${now.getMonth() + 1}_${now.getDate()}_${now.getFullYear()}_${now.getHours()}_${now.getMinutes()}`
-          this.coverageReports[index].downloadFilename = `Coverage_${timeStamp}.csv`
-        })
+      // Manually add the "special" Form 477 report to our list of reports
+      this.coverageReports.push({
+        downloadUrl: `/service/coverage/query/form477/${this.state.coverage.report.reportId}/${this.selectedRateReachMatrix.id}`,
+        name: 'form_477',
+        displayName: 'Form 477',
+        downloadFilename: `Form477_${timeStamp}.csv`
       })
-      .catch(err => console.error(err))
-  }
-
-  $onChanges(changesObj) {
-    if (changesObj && changesObj.coverageReportId) {
-      this.coverageReport = null
-      this.$http.get(`/service/coverage/report/${this.coverageReportId}`)
-        .then(result => {
-          this.coverageReport = result.data
-          this.$timeout()
-        })
-        .catch(err => console.error(err))
-    }
-  }
-
-  downloadReportOld() {
-    this.$http.get(`/service/coverage/query/form477/${this.coverageReportId}/${this.selectedRateReachMatrix.id}`)
-      .then((result) => {
-        const isEmptyResponse = (Object.keys(result.data || {}).length === 0)
-        if (isEmptyResponse) {
-          swal({
-            title: 'Empty coverage report',
-            text: 'We have an empty coverage report with the settings that you have selected',
-            type: 'error',
-            confirmButtonColor: '#DD6B55',
-            confirmButtonText: 'OK',
-            closeOnConfirm: true
-          })
-        } else {
-          this.Utils.downloadCSV(result.data, 'CoverageReport.csv')
-        }
-      })
-      .catch(err => console.error(err))
+      this.$timeout()
+    })
+    .catch(err => console.error(err))
   }
 
   downloadReport(report) {
     this.$http.get(report.downloadUrl)
       .then(result => this.Utils.downloadCSV(result.data, report.downloadFilename))
-      .catch(err => console.error(err))
-  }
-
-  resetReportSettings() {
-    this.$http.delete(`/service/coverage/report/${this.coverageReportId}`)
-      .then(result => this.onReportSettingsReset())
       .catch(err => console.error(err))
   }
 }
@@ -81,10 +50,7 @@ CoverageReportDownloaderController.$inject = ['$http', '$timeout', 'state', 'Uti
 
 let coverageReportDownloader = {
   templateUrl: '/components/sidebar/analysis/coverage/coverage-report-downloader.html',
-  bindings: {
-    coverageReportId: '<',
-    onReportSettingsReset: '&'
-  },
+  bindings: {},
   controller: CoverageReportDownloaderController
 }
 
