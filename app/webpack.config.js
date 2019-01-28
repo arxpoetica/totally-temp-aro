6bb994637ff58ee5d49977d0235784d2f9e80b0e
Parag Patwardhan <parag@metronconsulting.com>
2019-01-28 15:35:54 +0530

    Remove extra webpack config, we are building it all at once

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,66 +1,33 @@
 var path = require('path');
-var glob = require('glob');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = [
-  // Configuration for the AngularJS app
-  {
-    entry: './public/javascripts/src/components/index.js', 
-    output: {
-      path: path.resolve(__dirname, 'public/javascripts/lib'),
-      filename: 'angularjs-bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js|\.jsx$/,
-          exclude: [
-            '/node_modules/',
-            './public/javascripts/src/service-typegen/**',
-            './public/javascripts/src/react/'
-          ],
-          use:{
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"]
-            }
+module.exports = {
+  entry: './public/javascripts/src/components/index.js', 
+  output: {
+    path: path.resolve(__dirname, 'public/javascripts/lib'),
+    filename: 'aro-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: [
+          '/node_modules/',
+          './public/javascripts/src/service-typegen/**'
+        ],
+        use:{
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
-      ]
-    },
-    devtool: 'source-map',
-    plugins: [
-      new UglifyJsPlugin({
-        sourceMap: true
-      })
+      }
     ]
   },
-
-  // Configuration for the ReactJS app
-  {
-    entry: './public/javascripts/src/react/index.js',
-    output: {
-      path: path.resolve(__dirname, 'public/javascripts/lib'),
-      filename: 'reactjs-bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx$/,
-          exclude: [
-            '/node_modules/',
-            './public/javascripts/src/service-typegen/**',
-            './public/javascripts/src/react/'
-          ],
-          use:{
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"]
-            }
-          }
-        }
-      ]
-    },
-  }
-
-];
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
+};
