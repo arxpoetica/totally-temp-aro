04e1511be73baa91fdc1a5dcdd4e052ec579661a
Parag Patwardhan <parag@metronconsulting.com>
2019-02-08 17:15:14 +0530

    Fixed unit tests, they now run as part of "npm run dev"

--- app/public/javascripts/src/react/components/coverage/__tests__/coverage-button.test.js
+++ app/public/javascripts/src/react/components/coverage/__tests__/coverage-button.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
-import sinon from 'sinon'
 import { CoverageButton } from '../coverage-button'
 import CoverageStatusTypes from '../constants'
+import { List } from 'immutable';
 
 // -----------------------------------------------------------------------------
 test('When the user does not have a coverage report', () => {
@@ -34,26 +34,37 @@ test('When a coverage report is running at 10% progress', () => {
 
 // -----------------------------------------------------------------------------
 test('When button is clicked to initialize report', () => {
-  const mockInitializeCoverageReport = sinon.spy()
+  const locationLayers = new List([
+    { plannerKey: 'small', checked: true },
+    { plannerKey: 'small', checked: false }
+  ])
+  const mockInitializeCoverageReport = jest.fn()
   const component = shallow(
     <CoverageButton status={CoverageStatusTypes.UNINITIALIZED}
                     userId={5}
                     planId={22}
                     projectId={7}
+                    locationLayers={locationLayers}
                     activeSelectionModeId={'SELECTED_AREAS'}
                     initializationParams={{ generic: 'Init Params'}}
                     initializeCoverageReport={mockInitializeCoverageReport}></CoverageButton>
   )
 
   component.find('button').simulate('click')
-  expect(mockInitializeCoverageReport.calledOnceWithExactly(
-    5, 22, 7, 'SELECTED_AREAS', ['small', 'medium', 'large'], [], { generic: 'Init Params'})
-  ).toBeTruthy()
+  // We cannot use expect(mockInitializeCoverageReport).toHaveBeenCalledWith() because of the immutable list
+  expect(mockInitializeCoverageReport.mock.calls[0][0]).toEqual(5)
+  expect(mockInitializeCoverageReport.mock.calls[0][1]).toEqual(22)
+  expect(mockInitializeCoverageReport.mock.calls[0][2]).toEqual(7)
+  expect(mockInitializeCoverageReport.mock.calls[0][3]).toEqual('SELECTED_AREAS')
+  const expectedLocationLayers = locationLayers.filter(item => item.checked).map(item => item.plannerKey)
+  expect(mockInitializeCoverageReport.mock.calls[0][4].equals(expectedLocationLayers)).toBeTruthy()
+  expect(mockInitializeCoverageReport.mock.calls[0][5]).toEqual([])
+  expect(mockInitializeCoverageReport.mock.calls[0][6]).toEqual({ generic: 'Init Params'})
 })
 
 // -----------------------------------------------------------------------------
 test('When button is clicked to modify/delete report', () => {
-  const mockModifyCoverageReport = sinon.spy()
+  const mockModifyCoverageReport = jest.fn()
   const component = shallow(
     <CoverageButton status={CoverageStatusTypes.FINISHED}
                     report={{ reportId: 100 }}
@@ -62,5 +73,5 @@ test('When button is clicked to modify/delete report', () => {
   )
 
   component.find('button').simulate('click')
-  expect(mockModifyCoverageReport.calledOnceWithExactly(100)).toBeTruthy()
+  expect(mockModifyCoverageReport).toHaveBeenCalledWith(100)
 })
