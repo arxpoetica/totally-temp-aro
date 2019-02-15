bfe5143954772177d13b6a3527df4ee5c61e1529
Parag Patwardhan <parag@metronconsulting.com>
2019-02-15 15:52:38 +0530

    Minor: source-map instead of inline-source-map

--- app/webpack.dev.js
+++ app/webpack.dev.js
@@ -3,5 +3,5 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'source-map'
 })
