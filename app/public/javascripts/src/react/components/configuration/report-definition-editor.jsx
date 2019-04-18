3ac66d1b95e0438e5aab2a39724b4bf765b07e57
Parag Patwardhan <parag@metronconsulting.com>
2019-04-18 16:21:37 +0530

    Flexbox layout

--- app/public/javascripts/src/react/components/configuration/report-definition-editor.jsx
+++ app/public/javascripts/src/react/components/configuration/report-definition-editor.jsx
@@ -7,8 +7,8 @@ import ConfigurationActions from './configuration-actions'
 // First define the form
 let ReportDefinitionForm = props => {
   const { handleSubmit } = props
-  return <div className='container'>
-    <div className='row'>
+  return <div className='container' style={{ height: '100%' }}>
+    <div className='row' style={{ height: '100%' }}>
       <div className='col-md-2'>
         <ul className='nav nav-pills'>
           <li className='nav-item'>
@@ -16,9 +16,9 @@ let ReportDefinitionForm = props => {
           </li>
         </ul>
       </div>
-      <div className='col-md-10'>
-        <form onSubmit={handleSubmit}>
-          <div className='form-row'>
+      <div className='col-md-10' style={{ height: '100%' }}>
+        <form className='d-flex flex-column' onSubmit={handleSubmit} style={{ height: '100%' }}>
+          <div className='form-row flex-grow-0'>
             <div className='col'>
               <Field name='name' className='form-control' component='input' type='text' />
             </div>
@@ -29,12 +29,29 @@ let ReportDefinitionForm = props => {
               <Field name='queryType' className='form-control' component='input' type='text' />
             </div>
           </div>
-          <div className='form-row' style={{ paddingTop: '10px' }}>
-            <div className='col'>
-              <Field name='query' className='form-control' component='textarea' />
+          <div className='form-row flex-grow-1' style={{ paddingTop: '10px' }}>
+            <div className='col' style={{ height: '100%' }}>
+              <Field name='query' className='form-control' component='textarea' style={{ fontFamily: 'Courier New', fontSize: '10px', height: '100%' }} />
             </div>
           </div>
-          <button type='submit'>Submit1</button>
+          {/* Show an alert if required */}
+          {
+            <div className=' form-row flex-grow 0'>
+              <div className='col'>
+                <div className='alert alert-primary' role='alert'>
+                  This is a primary alert—check it out!
+                </div>
+              </div>
+            </div>
+          }
+          <div className='form-row flex-grow-0'>
+            <div className='col'>
+              <div className='float-right'>
+                <button className='btn btn-light' type='submit'>Test</button>
+                <button className='btn btn-primary' type='submit'>Save Definition</button>
+              </div>
+            </div>
+          </div>
         </form>
       </div>
     </div>
