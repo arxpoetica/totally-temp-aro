b704b3444e3cbb4577efb9558edade9a41cd5d2a
Parag Patwardhan <parag@metronconsulting.com>
2019-04-18 16:33:58 +0530

    Don't run tests that are in lib/ (those are already run from src/)

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupFiles: ['<rootDir>/enzyme.config.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  modulePathIgnorePatterns: ['<rootDir>/public/javascripts/lib'],
   moduleNameMapper: {
     '^.+\\.(css|less)$': '<rootDir>/public/javascripts/src/react/common/CSSStub.js'
   }
