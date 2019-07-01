b6caa971a43c3d798cb10d9db9e10a54bfe2e683
Parag Patwardhan <parag@metronconsulting.com>
2019-07-01 15:48:39 +0530

    Add a pure passthrough server for /service calls

--- app/routes/routes_service.js
+++ app/routes/routes_service.js
@@ -11,26 +11,26 @@ exports.configure = (api, middleware) => {
 
   // Get all requests (POST/GET/DELETE/PUT,etc) that start with /service, and then pass those
   // on to ARO-Service. Do NOT modify any data - this is intended to be a pass-through service
-  api.all('/service/*', (request, response, next) => {
+  // api.all('/service/*', (request, response, next) => {
 
-    // Chop off the prefix on this requests URL, and we get the URL to pass to aro-service
-    var serviceUrl = request.url.substring('/service/'.length)
-    var req = {
-      url: `${config.aro_service_url}/${serviceUrl}`,
-      method: request.method,
-      params: request.params,
-      json: true
-    }
+  //   // Chop off the prefix on this requests URL, and we get the URL to pass to aro-service
+  //   var serviceUrl = request.url.substring('/service/'.length)
+  //   var req = {
+  //     url: `${config.aro_service_url}/${serviceUrl}`,
+  //     method: request.method,
+  //     params: request.params,
+  //     json: true
+  //   }
 
-    // Attach request body if required
-    if (request.method !== 'GET') {
-      req.body = request.body
-    }
+  //   // Attach request body if required
+  //   if (request.method !== 'GET') {
+  //     req.body = request.body
+  //   }
 
-    models.AROService.request(req)
-      .then(jsonSuccess(response, next))
-      .catch(next)
-  })
+  //   models.AROService.request(req)
+  //     .then(jsonSuccess(response, next))
+  //     .catch(next)
+  // })
 
   // Get all requests (POST/GET/DELETE/PUT,etc) that start with /service, and then pass those
   // on to ARO-Service. Do NOT modify any data - this is intended to be a pass-through service
