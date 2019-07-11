97b9e84be0228f2b84c2965ed706dfb66105d5fe
Parag Patwardhan <parag@metronconsulting.com>
2019-07-11 12:56:54 +0530

    Show options when running ring plans

---
+++ app/public/javascripts/src/react/components/ring-edit/ring-options.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import Constants from '../../common/constants'
+
+export class RingOptions extends Component {
+  render () {
+    return <div className='p-2 m-2'>
+      <h4>Options</h4>
+      <form className='d-flex flex-column rfp-options'
+        style={{ height: '100%' }}
+        onSubmit={event => event.preventDefault()}>
+        <table className='table table-sm table-striped'>
+          <tbody>
+            {
+              Object.keys(this.props.initialValues).map(optionKey => (
+                <tr key={optionKey}>
+                  <td>{this.props.initialValues[optionKey].displayName}</td>
+                  <td>
+                    <Field
+                      id={`field_${optionKey}`}
+                      name={`${optionKey}.value`}
+                      className='form-control form-control-sm'
+                      component='input'
+                      type='text' />
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </form>
+    </div>
+  }
+}
+
+let RingOptionsForm = reduxForm({
+  form: Constants.RING_OPTIONS_FORM
+})(RingOptions)
+
+export default RingOptionsForm
