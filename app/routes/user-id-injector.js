967b65901fdc208e4be58862853da3991227735b
Parag Patwardhan <parag@metronconsulting.com>
2019-07-17 17:12:15 +0530

    A little bit of comments to reduce the madness

--- app/routes/user-id-injector.js
+++ app/routes/user-id-injector.js
@@ -5,7 +5,9 @@ const URLSearchParams = require('url').URLSearchParams
 // query parameter to the request URL and returns the URL
 // E.g. for userid=xxx, input url='/service/do_something', returns '/service/do_something?user_id=xxx'
 //      for userid=xxx, input url='/service/do_something?user_id=yyy', returns '/service/do_something?user_id=xxx'
-module.exports = (request, extPrefix, servicePrefix, userId) => {
+// This will also replace parts of the service url. So if you want '/service/v1/test' to become '/svc2/v1/test',
+// send urlPartialToRemove='/service', urlPartialToReplace='/svc2'
+module.exports = (request, urlPartialToRemove, urlPartialToReplace, userId) => {
   // First construct the full url (i.e. including the http(s)://<hostname>)
   const fullUrl = new URL(`${request.protocol}://${request.get('host')}${request.url}`)
 
@@ -18,6 +20,6 @@ module.exports = (request, extPrefix, servicePrefix, userId) => {
   fullUrl.search = searchParams
 
   // Construct the "final" URL by removing the protocol, host, etc so it looks like '/v1/plan?user_id=xxx'
-  const finalUrl = servicePrefix + fullUrl.href.substring(fullUrl.href.indexOf(extPrefix) + extPrefix.length)
+  const finalUrl = urlPartialToReplace + fullUrl.href.substring(fullUrl.href.indexOf(urlPartialToRemove) + urlPartialToRemove.length)
   return finalUrl
 }
