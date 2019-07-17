0f8c498fb640cdf2f7dbdb778bb90f8488454717
Parag Patwardhan <parag@metronconsulting.com>
2019-07-17 16:10:04 +0530

    Inject user id from JWT token into external API requests

---
+++ app/routes/user-id-injector.js
@@ -0,0 +1,23 @@
+const URL = require('url').URL
+const URLSearchParams = require('url').URLSearchParams
+
+// Given a Express request and user id, replaces (if it exists) or appends the user_id
+// query parameter to the request URL and returns the URL
+// E.g. for userid=xxx, input url='/service/do_something', returns '/service/do_something?user_id=xxx'
+//      for userid=xxx, input url='/service/do_something?user_id=yyy', returns '/service/do_something?user_id=xxx'
+module.exports = (request, userId) => {
+  // First construct the full url (i.e. including the http(s)://<hostname>)
+  const fullUrl = new URL(`${request.protocol}://${request.get('host')}${request.url}`)
+
+  // Now extract the existing query parameters
+  const searchParams = new URLSearchParams(fullUrl.searchParams)
+
+  // Overwrite or add the user_id query parameter. (Overwrite so that authenticated clients cannot
+  // impersonate other users). Then set the query parameters back to the original URL.
+  searchParams.set('user_id', userId)
+  fullUrl.search = searchParams
+
+  // Construct the "final" URL by removing the protocol, host, etc so it looks like '/v1/plan?user_id=xxx'
+  const finalUrl = fullUrl.href.substring(fullUrl.href.indexOf('/service/') + '/service/'.length - 1)
+  return finalUrl
+}
