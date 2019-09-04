2d06185f7f2b2d89e80d749edf1796e3cc4740d7
Parag Patwardhan <parag@metronconsulting.com>
2019-09-04 11:14:05 +0530

    Moved aro-search into angularjs component

--- app/public/javascripts/src/components/sidebar/view/view-mode.js
+++ app/public/javascripts/src/components/sidebar/view/view-mode.js
@@ -1,11 +1,35 @@
+import SelectionActions from '../../../react/components/selection/selection-actions'
+
 class ViewModeController {
-  constructor (state) {
+  constructor (state, $http, $ngRedux) {
     this.state = state
+    this.$http = $http
     this.currentUser = state.loggedInUser
+    this.unsubscribeRedux = $ngRedux.connect(this.mapStateToThis, this.mapDispatchToTarget)(this)
+  }
+
+  onSearchResult (selectedLocation) {
+    this.setSelectedLocations([selectedLocation.id])
+  }
+
+  onDestroy () {
+    this.unsubscribeRedux()
+  }
+
+  mapStateToThis (reduxState) {
+    return {
+      dataItems: reduxState.plan.dataItems
+    }
+  }
+
+  mapDispatchToTarget (dispatch) {
+    return {
+      setSelectedLocations: locationIds => dispatch(SelectionActions.setLocations(locationIds))
+    }
   }
 }
 
-ViewModeController.$inject = ['state']
+ViewModeController.$inject = ['state', '$http', '$ngRedux']
 
 let viewMode = {
   templateUrl: '/components/sidebar/view/view-mode.html',
