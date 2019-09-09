81c114ec7fcd9e4ff2c9f1a888a96b2a2c1b586f
Parag Patwardhan <parag@metronconsulting.com>
2019-09-09 12:38:32 +0530

    Show selected location with a green circle around it

--- app/public/javascripts/src/components/sidebar/view/view-mode.js
+++ app/public/javascripts/src/components/sidebar/view/view-mode.js
@@ -5,26 +5,58 @@ class ViewModeController {
     this.state = state
     this.$http = $http
     this.currentUser = state.loggedInUser
-    this.unsubscribeRedux = $ngRedux.connect(this.mapStateToThis, this.mapDispatchToTarget)(this)
+    this.unsubscribeRedux = $ngRedux.connect(this.mapStateToThis, this.mapDispatchToTarget)(this.mergeToTarget.bind(this))
   }
 
   onSearchResult (selectedLocation) {
     this.setSelectedLocations([selectedLocation.id])
   }
 
-  onDestroy () {
+  updateSelectedState (locationId) {
+    // This is legacy code from the old AngularJS location-detail.js. This will show a selection circle
+    // around the selected location.
+    if (!this.state.configuration.perspective) {
+      return
+    }
+    if ((this.state.selectedDisplayMode.getValue() === this.state.displayModes.VIEW) &&
+        (this.state.configuration.perspective.showViewModePanels[this.state.viewModePanels.LOCATION_INFO])) {
+      var newSelection = this.state.cloneSelection()
+      if (locationId) {
+        newSelection.editable.location = {
+          [locationId]: locationId
+        }
+      }
+      this.state.selection = newSelection
+    }
+  }
+
+  $onDestroy () {
     this.unsubscribeRedux()
   }
 
   mapStateToThis (reduxState) {
     return {
-      dataItems: reduxState.plan.dataItems
+      dataItems: reduxState.plan.dataItems,
+      selectedLocations: reduxState.selection.locations
     }
   }
 
   mapDispatchToTarget (dispatch) {
     return {
       setSelectedLocations: locationIds => dispatch(SelectionActions.setLocations(locationIds))
+    }
+  }
+
+  mergeToTarget (nextState, actions) {
+    const currentSelectedLocations = this.selectedLocations
+
+    // merge state and actions onto controller
+    Object.assign(this, nextState)
+    Object.assign(this, actions)
+
+    if (currentSelectedLocations !== nextState.selectedLocations) {
+      const firstLocationId = nextState.selectedLocations.values().next().value
+      this.updateSelectedState(firstLocationId)
     }
   }
 }
