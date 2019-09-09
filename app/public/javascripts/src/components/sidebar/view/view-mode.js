9f8181432af46b231d77cdd19eaaac231fb76a15
Parag Patwardhan <parag@metronconsulting.com>
2019-09-09 12:22:46 +0530

    Removed dead code

--- app/public/javascripts/src/components/sidebar/view/view-mode.js
+++ app/public/javascripts/src/components/sidebar/view/view-mode.js
@@ -5,39 +5,11 @@ class ViewModeController {
     this.state = state
     this.$http = $http
     this.currentUser = state.loggedInUser
-    this.roicPlanSettings = null
     this.unsubscribeRedux = $ngRedux.connect(this.mapStateToThis, this.mapDispatchToTarget)(this)
   }
 
   onSearchResult (selectedLocation) {
     this.setSelectedLocations([selectedLocation.id])
-    this.getLocationInfo(this.state.plan.id, selectedLocation.id)
-  }
-
-  getLocationInfo (planId, id) {
-    return this.$http.get(`/locations/${planId}/${id}/show`)// note: change this for a service endpoint?
-      .then((result) => {
-        var locationIds = []
-        if (result.data.hasOwnProperty('locSourceIds')) {
-          if (result.data.locSourceIds.hasOwnProperty('bizSourceIds') && result.data.locSourceIds.bizSourceIds.object_ids) {
-            locationIds = locationIds.concat(result.data.locSourceIds.bizSourceIds.object_ids)
-          }
-          if (result.data.locSourceIds.hasOwnProperty('hhSourceIds') && result.data.locSourceIds.hhSourceIds.object_ids) {
-            locationIds = locationIds.concat(result.data.locSourceIds.hhSourceIds.object_ids)
-          }
-          if (result.data.locSourceIds.hasOwnProperty('towerSourceIds') && result.data.locSourceIds.towerSourceIds.object_ids) {
-            locationIds = locationIds.concat(result.data.locSourceIds.towerSourceIds.object_ids)
-          }
-        }
-
-        this.roicPlanSettings = {
-          'analysis_type': 'LOCATION_ROIC',
-          'locationIds': locationIds,
-          'planId': planId,
-          'projectTemplateId': 1
-        }
-      })
-      .catch((err) => console.error(err))
   }
 
   onDestroy () {
