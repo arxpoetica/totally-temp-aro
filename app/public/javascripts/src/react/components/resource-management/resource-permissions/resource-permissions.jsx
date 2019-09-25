5e57c886f763e63e06f6779e7ff80918d3aed358
theUtilitariat <theUtilitariat@gmail.com>
2019-09-24 22:37:53 -0400

    resource-permissions react: phase 1

--- app/public/javascripts/src/react/components/resource-management/resource-permissions/resource-permissions.jsx
+++ app/public/javascripts/src/react/components/resource-management/resource-permissions/resource-permissions.jsx
@@ -6,11 +6,88 @@ import { PropTypes } from 'prop-types'
 export class ResourcePermissions extends Component {
   constructor (props) {
     super(props)
-    // more here
+    
+    this.state = {
+      'openRowId': null
+    }
   }
 
   render () {
-    return <div>from react</div>
+    console.log(this.props.dataItems)
+    return <div>
+      <div>
+        <table className="table table-sm ei-table-foldout-striped" style={{'borderBottom': '1px solid #dee2e6'}}>
+          <thead className="thead-dark">
+	          <tr>
+              <th></th>
+              <th className="ei-table-col-head-sortable ng-binding ng-scope" onClick={event => {console.log('reorder')}}>
+                Name
+                <div className="ei-table-col-sort-icon ng-scope">
+                  <i className="fa fa-chevron-down ng-scope" aria-hidden="true"> </i>
+                </div>
+              </th>
+              <th className="ei-table-col-head-sortable ng-binding ng-scope" onClick={event => {console.log('reorder')}}>
+                Data Type
+                <div className="ei-table-col-sort-icon ng-scope">
+                  <i className="fa fa-chevron-down ng-scope" aria-hidden="true"> </i>
+                </div>
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderDataRows()}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  }
+
+  renderDataRows () {
+    var jsx = []
+    Object.keys(this.props.dataItems).forEach((dataKey) => {
+      if (dataKey === 'equipment'){ // check dropdown type val
+        this.props.dataItems[dataKey].allLibraryItems.forEach((dataItem) => {
+          jsx = jsx.concat(this.renderDataRow(dataItem))
+        })       
+      }
+    })
+    return jsx
+  }
+
+  renderDataRow (dataItem) {
+    return [
+      <tr className={this.state.openRowId === dataItem.identifier ? 'ei-foldout-table-open' : ''} key={dataItem.identifier + '_a'}>
+        <td onClick={event => {this.toggleRow(dataItem.identifier)}}>
+          <i className="far fa-minus-square ei-foldout-icon ei-foldout-icon-table-open"></i>
+          <i className="far fa-plus-square ei-foldout-icon ei-foldout-icon-table-closed"></i>
+        </td>
+        <td>
+          {dataItem.name}
+        </td>
+        <td>
+          {dataItem.dataType}
+        </td>
+        <td className="ei-table-cell ei-table-button-cell">
+          <button className="btn btn-sm btn-outline-danger" onClick={event => {console.log('delete')}} data-toggle="tooltip" data-placement="bottom" title="Delete">
+            <i className="fa ei-button-icon fa-trash-alt"></i>
+          </button>
+        </td>
+      </tr>,
+      <tr className='ei-foldout-row' key={dataItem.identifier + '_b'}>
+        <td colSpan='999'>
+          user table
+        </td>
+      </tr>
+    ]
+  }
+
+  toggleRow (rowId) {
+    if (this.state.openRowId === rowId) {
+      rowId = null
+    }
+
+    this.setState({ ...this.state, 'openRowId': rowId })
   }
 
 }
@@ -28,6 +105,8 @@ ResourcePermissions.propTypes = {
 }
 
 const mapStateToProps = (state) => ({
+  dataItems: state.plan.dataItems,
+  uploadDataSources: state.plan.uploadDataSources
   /*
   rings: state.ringEdit.rings,
   selectedRingId: state.ringEdit.selectedRingId,
@@ -39,6 +118,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
+  selectDataItems: (dataItemKey, selectedLibraryItems) => dispatch(PlanActions.selectDataItems(dataItemKey, selectedLibraryItems)),
+  setAllLibraryItems: (dataItemKey, allLibraryItems) => dispatch(PlanActions.setAllLibraryItems(dataItemKey, allLibraryItems))
   /*
   setSelectedRingId: ringId => dispatch(ringActions.setSelectedRingId(ringId)),
   newRing: (planId, userId) => dispatch(ringActions.newRing(planId, userId)),
