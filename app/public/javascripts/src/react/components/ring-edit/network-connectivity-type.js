52b9b7b765d03b9c41642d785a72a06dbb05cb41
Parag Patwardhan <parag@metronconsulting.com>
2019-09-26 22:39:38 +0530

    Minor: Changed display name

--- app/public/javascripts/src/react/components/ring-edit/network-connectivity-type.js
+++ app/public/javascripts/src/react/components/ring-edit/network-connectivity-type.js
@@ -1,5 +1,5 @@
 export default Object.freeze({
   none: { id: 'none', displayName: 'None' },
   snapToEdge: { id: 'snapToEdge', displayName: 'Snap to Edge' },
-  snapToWormhole: { id: 'snapToWormhole', displayName: 'Snap to Location Connector' }
+  snapToWormhole: { id: 'snapToWormhole', displayName: 'Snap to Network Connector' }
 })
