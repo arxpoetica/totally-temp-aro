55219ddccb39331e90dd469239bd1ffa2d6c5d5a
theUtilitariat <theUtilitariat@gmail.com>
2019-10-08 19:19:43 -0400

    searchable dropdown: v1 of search function

--- app/public/javascripts/src/react/components/common/searchable-select.jsx
+++ app/public/javascripts/src/react/components/common/searchable-select.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 export class SearchableSelect extends Component {
   constructor (props) {
@@ -8,15 +8,23 @@ export class SearchableSelect extends Component {
     // props.resultsMax - integer, max length of dropdown results
     this.searchPool = {}
     this.state = {
-      searchResults: {} // group of named arrays
+      searchResults: {}, // group of named arrays
+      newUserId: null,
+      newUserName: ''
     }
   }
 
   render () {
     return (
-      <div className='dropdown'>
+      <div className='btn-group'>
+        <input type='text'
+          onInput={event => this.onSearchInput(event)}
+          placeholder='Search Users'
+          className='form-control'
+          id='dropdownMenu'
+          data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' />
         <button className='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenu' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-          Search Users
+          {this.state.newUserName}
         </button>
         <div className='dropdown-menu' aria-labelledby='dropdownMenu'>
           {this.renderOptions()}
@@ -29,30 +37,48 @@ export class SearchableSelect extends Component {
     var jsx = []
     Object.keys(this.state.searchResults).forEach((key) => {
       if (key) {
-        jsx.push(<h6 key={`search-select-option-${key}`} className='dropdown-header'>-- {key} --</h6>)
-        jsx.push(<div className='dropdown-divider' />)
+        jsx.push(<h6 key={`search-select-header-${key}`} className='dropdown-header'>-- {key} --</h6>)
+        jsx.push(<div className='dropdown-divider' key={`search-select-divider-${key}`} />)
       }
 
       this.state.searchResults[key].forEach(item => {
-        jsx.push(<button key={`search-select-option-${key}-${item.id}`} className='dropdown-item' type='button'>{item.name}</button>)
+        jsx.push(<button onClick={(event) => this.onSelectChange(event, item)} key={`search-select-option-${key}-${item.id}`} className='dropdown-item' type='button'>{item.name}</button>)
       })
     })
 
     return jsx
   }
 
-  onSelectChange (event) {
-    console.log(event.target.value)
+  onSearchInput (event) {
+    var searchTerm = event.target.value
+    var searchResults = {}
+    Object.keys(this.searchPool).forEach((key) => {
+      searchResults[key] = this.searchPool[key].filter(item => { return this.filterItem(item, searchTerm) })
+      if (searchResults[key].length === 0) delete searchResults[key]
+    })
+    this.setState({ ...this.state, 'searchResults': searchResults })
+  }
+
+  onSelectChange (event, item) {
+    this.setState({ ...this.state,
+      newUserId: item.id,
+      newUserName: item.name
+    })
+  }
+
+  filterItem (item, searchTerm) {
+    return (item.name.toLowerCase().includes(searchTerm.toLowerCase())) // optomize: do toLowerCase on search once
   }
 
   componentWillMount () {
-    console.log()
+    // tokenize name here
     var searchResults = {}
     if (Array.isArray(this.props.optionLists)) {
       searchResults = { '': this.props.optionLists } // yes, an empty string can be an object key
     } else {
       searchResults = this.props.optionLists
     }
+    this.searchPool = searchResults
     this.setState({ ...this.state, 'searchResults': searchResults })
   }
 }
