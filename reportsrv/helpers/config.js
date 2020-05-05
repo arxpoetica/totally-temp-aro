2d4b967a4af71ba67b9eab60e92d2f421452c9e1
Nick Vitucci <nvitucci@altvil.com>
2020-05-05 13:49:21 -0400

    Remove reportsrv - moved to separate repo

--- reportsrv/helpers/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // Define the URL where we can access the ARO web app. Ideally, use the internal docker network. So if
-  // your app container is named "app", the base url is 'http://app:8000'
-  APP_BASE_URL: process.env.APP_BASE_URL || 'http://app:8000'
-}
