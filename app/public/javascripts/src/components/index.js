b07c4f7b9a331cb2602d4a281fa8f8fc98cc6bc8
Robert Hall <robert.hall@altmansolon.com>
2021-01-14 20:39:32 -0700

    Removal of legacy code (from long time yore)

--- app/public/javascripts/src/components/index.js
+++ app/public/javascripts/src/components/index.js
@@ -64,7 +64,6 @@ import editPlanTag from './sidebar/view/edit-plan-tag'
 import aroSearch from './sidebar/view/aro-search'
 import aroMultiselectSearch from './sidebar/view/aro-multiselect-search'
 import displayModeButtons from './sidebar/display-mode-buttons'
-import optimizeButton from './sidebar/optimize-button'
 import summaryReports from './sidebar/summary-reports'
 import analysisExpertMode from './sidebar/analysis/analysis-expert-mode'
 import analysisMode from './sidebar/analysis/analysis-mode'
@@ -147,7 +146,6 @@ app.component('boundaryDetail', boundaryDetail)
   .component('aroSearch', aroSearch)
   .component('aroMultiselectSearch', aroMultiselectSearch)
   .component('displayModeButtons', displayModeButtons)
-  .component('optimizeButton', optimizeButton)
   .component('summaryReports', summaryReports)
   .component('analysisExpertMode', analysisExpertMode)
   .component('analysisMode', analysisMode)
