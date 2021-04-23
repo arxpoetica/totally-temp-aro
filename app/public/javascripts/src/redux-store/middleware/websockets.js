a6248baabcc36577e1e5283baa00437302d076c8
Robert Hall <robert.hall@altmansolon.com>
2021-04-23 13:56:00 -0600

    Broadcast fixes, reapplied

--- app/public/javascripts/src/redux-store/middleware/websockets.js
+++ app/public/javascripts/src/redux-store/middleware/websockets.js
@@ -1,18 +1,16 @@
-import io from 'socket.io-client'
 import { toast } from 'react-toastify'
-import Actions from '../../react/common/actions'
 import socketManager from '../../react/common/socket-manager'
 
 const createSocketMiddleware = () => {
   return storeAPI => {
-    // If we get a raw Redux command, dispatch it
+    // Join room for this broadcast
+    socketManager.joinRoom('broadcast', 'broadcast')
     socketManager.subscribe('NOTIFICATION_SHOW', (command) => {
       toast.error(command.payload.subject + ': ' + command.payload.body, {
         position: toast.POSITION.BOTTOM_LEFT,
         className: 'map-canvas',
         autoClose: command.payload.isChecked
       })
-      // storeAPI.dispatch(command)
     })
 
     // next is the following action to be run after this middleware
--- app/sockets/sockets.js
+++ app/sockets/sockets.js
@@ -64,7 +64,8 @@ class Sockets {
 
   // Emit a message to EVERYONE connected via websockets. Use sparingly, if used at all.
   emitToAll (payload) {
-    this.sockets.client.to(`broadcast`).emit('message', payload)
+    // it should be 'this.sockets.broadcast' for broadcast.
+    this.sockets.broadcast.to(`broadcast`).emit('message', payload)
     console.log(`SOCKET EMIT broadcast, payload: ${this.formatPayloadForLogging(payload)}`)
   }
 
