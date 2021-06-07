5fddeb39663f127e7267500eaaa344b29503abb0
Robert Hall <robert.hall@altmansolon.com>
2021-06-07 16:54:20 -0600

    Plan recalculate section

--- app/public/javascripts/src/react/components/plan-editor/plan-editor.jsx
+++ app/public/javascripts/src/react/components/plan-editor/plan-editor.jsx
@@ -3,10 +3,12 @@ import { PropTypes } from 'prop-types'
 import reduxStore from '../../../redux-store'
 import wrapComponentWithProvider from '../../common/provider-wrapped-component'
 import PlanEditorActions from './plan-editor-actions'
+import PlanEditorRecalculate from './plan-editor-recalculate.jsx'
 import EquipmentDragger from './equipment-dragger.jsx'
 import EquipmentMapObjects from './equipment-map-objects.jsx'
 import EquipmentBoundaryMapObjects from './equipment-boundary-map-objects.jsx'
 import BoundaryDrawCreator from './boundary-draw-creator.jsx'
+import './plan-editor.css'
 
 export const PlanEditor = props => {
 
@@ -34,7 +36,7 @@ export const PlanEditor = props => {
   }
 
   return (
-    <div>
+    <div className="aro-plan-editor">
       <div className='text-center'>
         <div className='btn-group '>
           {/* A button to commit the transaction */}
@@ -60,6 +62,8 @@ export const PlanEditor = props => {
       <EquipmentBoundaryMapObjects />
       { /* If we are in "draw boundary mode" for any equipment, render the drawing component */ }
       { isDrawingBoundaryFor ? <BoundaryDrawCreator /> : null }
+
+      <PlanEditorRecalculate/>
     </div>
   )
 
