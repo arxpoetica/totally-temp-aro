9110329c37a5a2139457957fc8a813a9031c1ac1
Robert Hall <robert.hall@altmansolon.com>
2021-06-16 17:43:31 -0600

    Converted equipment dragger to functional comp

--- app/public/javascripts/src/react/components/plan-editor/equipment-dragger.jsx
+++ app/public/javascripts/src/react/components/plan-editor/equipment-dragger.jsx
@@ -1,91 +1,84 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
 import DraggableButton from './draggable-button.jsx'
 import Constants from './constants'
 
-export class EquipmentDragger extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      selectedEquipmentType: null
-    }
-  }
+// TODO: centralize these somewhere...should probably not be hardcoded
+const editableNetworkNodeTypes = [
+  'dslam',
+  'central_office',
+  'fiber_distribution_hub',
+  'fiber_distribution_terminal',
+  'cell_5g',
+  'splice_point',
+  'bulk_distribution_terminal',
+  'loop_extender',
+  'network_anchor',
+  'multiple_dwelling_unit',
+  'network_connector',
+  'location_connector',
+]
 
-  componentDidMount () {
-    this.setState({
-      selectedEquipmentType: this.getEditableEquipmentTypes()[0]
-    })
-  }
+export const EquipmentDragger = props => {
 
-  render () {
-    const editableEquipmentTypes = this.getEditableEquipmentTypes()
-    const selectedEquipmentType = this.state.selectedEquipmentType || editableEquipmentTypes[0]
-    return <div>
-      <div className='d-flex flex-row' style={{ alignItems: 'center' }}>
+  const { visibleEquipmentTypes, equipmentDefinitions } = props
 
-        {/* The button that will be dragged onto the map */}
-        <DraggableButton
-          className='flex-grow-0 m-3'
-          key={this.state.selectedEquipmentType}
-          icon={this.props.equipmentDefinitions[selectedEquipmentType].iconUrl}
-          entityType={Constants.DRAG_DROP_NETWORK_EQUIPMENT}
-          entityDetails={this.props.equipmentDefinitions[selectedEquipmentType].networkNodeType}
-          isBoundary={false}
-        />
+  const [selectedEquipmentType, setSelectedEquipmentType] = useState(null)
+  const [editableEquipmentTypes, setEditableEquipmentTypes] = useState([])
 
-        {/* A dropdown to select the equipment type to drag */}
-        <select
-          className='flex-grow-1 m-2 form-control'
-          value={selectedEquipmentType}
-          onChange={e => this.setState({ selectedEquipmentType: e.target.value })}
-        >
-          {
-            editableEquipmentTypes.map(editableEquipmentType => <option key={editableEquipmentType} value={editableEquipmentType}>
-              {this.props.equipmentDefinitions[editableEquipmentType].label}
-            </option>)
-          }
-        </select>
+  useEffect(() => {
+    const editableEquipmentTypes = visibleEquipmentTypes.filter(type => editableNetworkNodeTypes.includes(type))
+    setEditableEquipmentTypes(editableEquipmentTypes)
+    setSelectedEquipmentType(editableEquipmentTypes[0])
+  }, [])
 
-        {/* Help text */}
-        <div className='flex-shrink-0 m-3'>
-          (drag icon onto map)
-        </div>
+  return equipmentDefinitions && equipmentDefinitions[selectedEquipmentType] && (
+    <div className='d-flex flex-row' style={{ alignItems: 'center' }}>
+      {/* The button that will be dragged onto the map */}
+      <DraggableButton
+        className='flex-grow-0 m-3'
+        key={selectedEquipmentType}
+        icon={equipmentDefinitions[selectedEquipmentType].iconUrl}
+        entityType={Constants.DRAG_DROP_NETWORK_EQUIPMENT}
+        entityDetails={equipmentDefinitions[selectedEquipmentType].networkNodeType}
+        isBoundary={false}
+      />
+
+      {/* A dropdown to select the equipment type to drag */}
+      <select
+        className='flex-grow-1 m-2 form-control'
+        value={selectedEquipmentType}
+        onChange={event => setSelectedEquipmentType(event.target.value)}
+      >
+        {
+          editableEquipmentTypes.map(type => {
+            <option key={type} value={type}>
+              {equipmentDefinitions[type].label}
+            </option>
+          })
+        }
+      </select>
+
+      {/* Help text */}
+      <div className='flex-shrink-0 m-3'>
+        (drag icon onto map)
       </div>
     </div>
-  }
+  ) || null
 
-  getEditableEquipmentTypes () {
-    const editableNetworkNodeTypes = new Set([
-      'dslam',
-      'central_office',
-      'fiber_distribution_hub',
-      'fiber_distribution_terminal',
-      'cell_5g',
-      'splice_point',
-      'bulk_distribution_terminal',
-      'loop_extender',
-      'network_anchor',
-      'multiple_dwelling_unit',
-      'network_connector',
-      'location_connector'
-    ])
-    return this.props.visibleEquipmentTypes.filter(equipmentType => editableNetworkNodeTypes.has(equipmentType))
-  }
 }
 
 EquipmentDragger.propTypes = {
   visibleEquipmentTypes: PropTypes.arrayOf(PropTypes.string),
-  equipmentDefinitions: PropTypes.object
+  equipmentDefinitions: PropTypes.object,
 }
 
 const mapStateToProps = (state) => ({
   visibleEquipmentTypes: (state.configuration.ui.perspective && state.configuration.ui.perspective.networkEquipment.areVisible) || [],
-  equipmentDefinitions: state.mapLayers.networkEquipment.equipments
+  equipmentDefinitions: state.mapLayers.networkEquipment.equipments,
 })
 
-const mapDispatchToProps = dispatch => ({
-})
+const mapDispatchToProps = dispatch => ({})
 
-const EquipmentDraggerComponent = connect(mapStateToProps, mapDispatchToProps)(EquipmentDragger)
-export default EquipmentDraggerComponent
+export default connect(mapStateToProps, mapDispatchToProps)(EquipmentDragger)
