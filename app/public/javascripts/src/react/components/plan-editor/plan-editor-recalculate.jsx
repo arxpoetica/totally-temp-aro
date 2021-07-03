91ee8e815b2920b2127eddcf1a3829d74391259b
Robert Hall <robert.hall@altmansolon.com>
2021-07-02 21:05:34 -0600

    Just some basic layout, in prep for recalc

--- app/public/javascripts/src/react/components/plan-editor/plan-editor-recalculate.jsx
+++ app/public/javascripts/src/react/components/plan-editor/plan-editor-recalculate.jsx
@@ -2,47 +2,81 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import Select from 'react-select'
 import { selectStylesBlue } from '../../common/view-utils'
+import AroHttp from '../../common/aro-http'
 
 const PlanEditorRecalculate = props => {
 
+  const { transaction } = props
+  
   const [options, setOptions] = useState([
     { label: 'Auto assign to a boundary', value: 0 },
     { label: 'Delete this hub', value: 1 },
+    { label: 'Recalculate Entire Plan', value: 2 },
+    { label: 'Recalculate Boundaries', value: 3 },
+    { label: 'Calculate and Place Hubs', value: 4 },
+    { label: 'Calculate and Place Hubs & Terminals', value: 5 },
   ])
 
   const [selectedOption, setSelectedOption] = useState()
-  const handleChange = change => {
-    console.log(change)
+
+  const handleSelectChange = change => {
     setSelectedOption(change)
   }
 
   return (
     <div className="plan-editor-recalculate">
-      <div className="actions">
+
+      {/* TODO: the following will come back when ready */}
+      {/* <div className="actions">
         <h2>Recalculate Plan:</h2>
         <Select
           className="select"
           value={selectedOption}
           options={options}
           placeholder="Select Calculation Type"
-          onChange={handleChange}
+          onChange={handleSelectChange}
           styles={selectStylesBlue}
         />
+      </div> */}
+
+      <div className="plan-editor-buttons">
+
+        <div className="group">
+          <h2>Recalculate Plan:</h2>
+          <div className="group">
+            <button type="button" className="btn btn-outline-secondary">Cancel</button>
+            <button type="button" className="btn btn-primary">Save</button>
+          </div>
+        </div>
+
+        {/* TODO: the following will come back when ready */}
+        {/* <div className="group">
+          <button type="button" className="btn btn-outline-success">Reassign Households</button>
+          <button type="button" className="btn btn-outline-danger">Reassign Connected Hub</button>
+        </div> */}
+        {/* TODO: the following will come back when ready */}
+        {/* <div className="group">
+          <h2>Reassign Households:</h2>
+          <div className="group">
+            <button type="button" className="btn btn-outline-secondary">Cancel</button>
+            <button type="button" className="btn btn-primary">Save</button>
+          </div>
+        </div> */}
       </div>
-      <div className="checkboxes">
-        <label>
-          <input type="checkbox" className="checkboxfill layer-type-checkboxes"/>
-          <span>Sticky assignment</span>
-        </label>
-        <label>
-          <input type="checkbox" className="checkboxfill layer-type-checkboxes"/>
-          <span>Auto recalculate</span>
-        </label>
-      </div>
+
     </div>
   )
 }
 
-const mapStateToProps = state => ({})
-const mapDispatchToProps = dispatch => ({})
+const mapStateToProps = state => ({
+  transaction: state.planEditor.transaction,
+  subnets: state.planEditor.subnets,
+  selectedSubnetId: state.planEditor.selectedSubnetId,
+})
+
+const mapDispatchToProps = dispatch => ({
+  discardTransaction: transactionId => dispatch(PlanEditorActions.discardTransaction(transactionId)),
+  addSubnets: subnetIds => dispatch(PlanEditorActions.addSubnets(subnetIds)),
+})
+
 export default connect(mapStateToProps, mapDispatchToProps)(PlanEditorRecalculate)
--- app/public/javascripts/src/react/components/plan-editor/plan-editor.jsx
+++ app/public/javascripts/src/react/components/plan-editor/plan-editor.jsx
@@ -4,7 +4,6 @@ import reduxStore from '../../../redux-store'
 import wrapComponentWithProvider from '../../common/provider-wrapped-component'
 import PlanEditorActions from './plan-editor-actions'
 import PlanEditorHeader from './plan-editor-header.jsx'
-import PlanEditorButtons from './plan-editor-buttons.jsx'
 import PlanEditorRecalculate from './plan-editor-recalculate.jsx'
 import EquipmentDragger from './equipment-dragger.jsx'
 import EquipmentMapObjects from './equipment-map-objects.jsx'
@@ -17,11 +16,11 @@ export const PlanEditor = props => {
   const {
     planId,
     userId,
-  	transactionId,
-  	isCommittingTransaction,
+    transactionId,
+    isCommittingTransaction,
     resumeOrCreateTransaction,
-  	commitTransaction,
-  	discardTransaction,
+    commitTransaction,
+    discardTransaction,
     isDrawingBoundaryFor,
     features,
     selectedFeatureIds,
@@ -87,7 +86,7 @@ export const PlanEditor = props => {
       { isDrawingBoundaryFor ? <BoundaryDrawCreator /> : null }
 
       <PlanEditorRecalculate />
-      <PlanEditorButtons />
+
       {selectedFeatureIds.map(id => <PlanEditorHeader selectedFeatureId={id}
           onClick={ (event, objectId) => onSelectedClick(event, objectId)}
           onClose={ (event, objectId) => onSelectedClose(event, objectId)}
