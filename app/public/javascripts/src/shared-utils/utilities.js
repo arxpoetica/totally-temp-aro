29311b8a4437974073d0f4e95e4ecf09811ae50a
yashpriyam <yashpriyam74@gmail.com>
2021-08-27 15:43:01 +0530

    create common drawingManagerStateUpdate function

--- app/public/javascripts/src/shared-utils/utilities.js
+++ app/public/javascripts/src/shared-utils/utilities.js
@@ -10,3 +10,22 @@ export function set (obj, path, value) {
         return acc[key]
     }, obj)
 }
+
+export function updateDrawingManagerState (drawingManager, selectedDisplayMode, targetSelectionMode, state, displayModes, mapRef) {
+    if (!drawingManager) {
+      return
+    }
+
+    if (selectedDisplayMode === displayModes.VIEW 
+      && targetSelectionMode === state.targetSelectionModes.POLYGON_EXPORT_TARGET
+      || (
+        (selectedDisplayMode === displayModes.ANALYSIS || selectedDisplayMode === displayModes.VIEW) 
+        && targetSelectionMode === state.targetSelectionModes.POLYGON_PLAN_TARGET)
+      ) {
+      drawingManager.setDrawingMode('polygon')
+      drawingManager.setMap(mapRef)
+    } else {
+      drawingManager.setDrawingMode('marker')
+      drawingManager.setMap(null)
+    }
+  }
