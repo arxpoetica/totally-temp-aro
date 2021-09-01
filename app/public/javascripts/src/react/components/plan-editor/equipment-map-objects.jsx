9b49c937f2f7f46857bc4527640b68a971c4fcf8
Robert Hall <robert.hall@altmansolon.com>
2021-09-01 12:13:49 -0600

    Be able to click central office

--- app/public/javascripts/src/react/components/plan-editor/equipment-map-objects.jsx
+++ app/public/javascripts/src/react/components/plan-editor/equipment-map-objects.jsx
@@ -58,7 +58,6 @@ export class EquipmentMapObjects extends Component {
     //                      (feature.workflow_state_id & WorkflowState.INVALIDATED.id))
     
     const { objectId } = feature
-    
     // ToDo: unhack this 
     //let isLocked = false
     //if (feature.networkNodeType === "central_office") isLocked = true
@@ -70,7 +69,6 @@ export class EquipmentMapObjects extends Component {
         url: this.props.equipmentDefinitions[feature.networkNodeType].iconUrl
       },
       draggable: !feature.locked, // Allow dragging only if feature is not locked
-      clickable: !idle,
       opacity: idle ? 0.4 : 1.0,
       map: this.props.googleMaps,
       zIndex: MAP_OBJECT_Z_INDEX
