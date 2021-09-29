6a1daea0e31b6b838d7ffd148e456f9837547268
Evan Jones <evan@evanjjones.com>
2021-09-29 16:57:47 -0400

    convert to functional component

--- app/public/javascripts/src/react/components/plan-editor/fiber-map-objects.jsx
+++ app/public/javascripts/src/react/components/plan-editor/fiber-map-objects.jsx
@@ -1,34 +1,24 @@
 /* globals google */
-import { Component } from 'react'
+import { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 import WktUtils from '../../../shared-utils/wkt-utils'
 import { constants } from './constants'
 import PlanEditorActions from './plan-editor-actions'
 
-export class FiberMapObjects extends Component {
-  constructor (props) {
-    super(props)
-    this.mapobjects = []
-    this.renderedSubnet = ''
-  }
-  
-  render () {
-    // No UI for this component. It deals with map objects only.
-    return null
-  }
+export const FiberMapObjects = (props) => {
+  const { selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired, setFiberRenderRequired, googleMaps } = props
+  const renderedSubnet = useRef('')
+  const mapObjects = useRef([])
 
-  componentDidUpdate () {
-    const { selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired } = this.props
-    
-    // if it in in subnets it is either a hub or CO, so it has it's own fiber
+  useEffect(() => {
     if (subnets[selectedSubnetId]){
       const { subnetLinks, fiberType } = subnets[selectedSubnetId].fiber
 
       // don'r render if fiber is the same
       // fiber Renderrequired being true means the fiber should be rendered regardless
-      if (this.renderedSubnet !== selectedSubnetId || fiberRenderRequired) {
-        this.renderedSubnet = selectedSubnetId
-        this.renderFiber(subnetLinks, fiberType)
+      if (renderedSubnet.current !== selectedSubnetId || fiberRenderRequired) {
+        renderedSubnet.current = selectedSubnetId
+        renderFiber(subnetLinks, fiberType)
       }
     } 
     else if (subnetFeatures[selectedSubnetId]) {
@@ -36,59 +26,60 @@ export class FiberMapObjects extends Component {
       const { subnetId: parentId } = subnetFeatures[selectedSubnetId]
       const { subnetLinks, fiberType } = subnets[parentId].fiber
 
-      if (this.renderedSubnet !== parentId || fiberRenderRequired) {
-        this.renderedSubnet = parentId
-        this.renderFiber(subnetLinks, fiberType)
+      if (renderedSubnet.current !== parentId || fiberRenderRequired) {
+        renderedSubnet.current = parentId
+        renderFiber(subnetLinks, fiberType)
       }
     }
-    
-  }
 
-  renderFiber (subnetLinks, fiberType) {
-    const { setFiberRenderRequired } = this.props
-    if (this.mapobjects.length) {
-      this.deleteMapObject()
-    }
-    if (subnetLinks){
-      for (const subnetLink of subnetLinks) {
-        const { geometry } = subnetLink
-        if (geometry.type === 'LineString') {
-          const path = WktUtils.getGoogleMapPathsFromWKTLineString(geometry)
-          this.createMapObject(path, fiberType)
-        } else if (geometry.type === 'MultiLineString') {
-          const path = WktUtils.getGoogleMapPathsFromWKTMultiLineString(geometry)
-          this.createMapObject(path, fiberType)
+    function renderFiber(subnetLinks, fiberType) {
+      if (mapObjects.current.length) {
+        deleteMapObject()
+      }
+      if (subnetLinks){
+        for (const subnetLink of subnetLinks) {
+          const { geometry } = subnetLink
+          if (geometry.type === 'LineString') {
+            const path = WktUtils.getGoogleMapPathsFromWKTLineString(geometry)
+            createMapObject(path, fiberType)
+          } else if (geometry.type === 'MultiLineString') {
+            const path = WktUtils.getGoogleMapPathsFromWKTMultiLineString(geometry)
+            createMapObject(path, fiberType)
+          }
         }
+        setFiberRenderRequired(false)
       }
-      setFiberRenderRequired(false)
+      
+    }
+    function createMapObject(path, fiberType) {
+      const mapObject = new google.maps.Polyline({
+        path,
+        clickable: false,
+        map: googleMaps,
+        zIndex: constants.Z_INDEX_MAP_OBJECT,
+        strokeColor: fiberType === 'DISTRIBUTION' ? '#FF0000' : '#1700ff',
+        strokeOpacity: 1.0,
+        strokeWeight: fiberType === 'DISTRIBUTION' ? 2 : 4,
+      })
+      mapObjects.current = [...mapObjects.current, mapObject]
     }
     
-  }
+  }, [selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired, setFiberRenderRequired, googleMaps, deleteMapObject])
 
-  createMapObject (path, fiberType) {
-    const mapObject = new google.maps.Polyline({
-      path,
-      clickable: false,
-      map: this.props.googleMaps,
-      zIndex: constants.Z_INDEX_MAP_OBJECT,
-      strokeColor: fiberType === 'DISTRIBUTION' ? '#FF0000' : '#1700ff',
-      strokeOpacity: 1.0,
-      strokeWeight: fiberType === 'DISTRIBUTION' ? 2 : 4,
-    })
-    this.mapobjects.push(mapObject)
-  }
+  useEffect(() => {
+    // cleanup
+    return deleteMapObject()
+  }, [])
 
-  deleteMapObject () {
-    if (this.mapobjects.length) {
-      this.mapobjects.forEach(mapObject => mapObject.setMap(null))
-      this.mapobjects = []
+  function deleteMapObject() {
+    if (mapObjects.current.length) {
+      mapObjects.current.forEach(mapObject => mapObject.setMap(null))
+      mapObjects.current = []
     }
   }
 
-  componentWillUnmount () {
-    // console.log(this.mapobjects)
-    this.deleteMapObject()
-  }
+  // No ui, only handles mapObjects
+  return null
 }
 
 const mapStateToProps = state => ({
