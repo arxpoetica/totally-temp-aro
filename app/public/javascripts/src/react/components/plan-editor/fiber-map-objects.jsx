c0ee4ddb3655c4006f45a2e9155929572bc6fbed
Evan Jones <evan@evanjjones.com>
2021-09-30 14:55:37 -0400

    fixes and cleanup

--- app/public/javascripts/src/react/components/plan-editor/fiber-map-objects.jsx
+++ app/public/javascripts/src/react/components/plan-editor/fiber-map-objects.jsx
@@ -1,14 +1,15 @@
 /* globals google */
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { connect } from 'react-redux'
 import WktUtils from '../../../shared-utils/wkt-utils'
 import { constants } from './constants'
 import PlanEditorActions from './plan-editor-actions'
 
+let renderedSubnetId = ''
+let mapObjects = []
+
 export const FiberMapObjects = (props) => {
   const { selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired, setFiberRenderRequired, googleMaps } = props
-  const renderedSubnet = useRef('')
-  const mapObjects = useRef([])
 
   useEffect(() => {
     if (subnets[selectedSubnetId]){
@@ -16,8 +17,8 @@ export const FiberMapObjects = (props) => {
 
       // don'r render if fiber is the same
       // fiber Renderrequired being true means the fiber should be rendered regardless
-      if (renderedSubnet.current !== selectedSubnetId || fiberRenderRequired) {
-        renderedSubnet.current = selectedSubnetId
+      if (renderedSubnetId !== selectedSubnetId || fiberRenderRequired) {
+        renderedSubnetId = selectedSubnetId
         renderFiber(subnetLinks, fiberType)
       }
     } 
@@ -26,15 +27,15 @@ export const FiberMapObjects = (props) => {
       const { subnetId: parentId } = subnetFeatures[selectedSubnetId]
       const { subnetLinks, fiberType } = subnets[parentId].fiber
 
-      if (renderedSubnet.current !== parentId || fiberRenderRequired) {
-        renderedSubnet.current = parentId
+      if (renderedSubnetId !== parentId || fiberRenderRequired) {
+        renderedSubnetId = parentId
         renderFiber(subnetLinks, fiberType)
       }
     }
 
     function renderFiber(subnetLinks, fiberType) {
-      if (mapObjects.current.length) {
-        deleteMapObject()
+      if (mapObjects.length) {
+        deleteMapObjects()
       }
       if (subnetLinks){
         for (const subnetLink of subnetLinks) {
@@ -61,20 +62,21 @@ export const FiberMapObjects = (props) => {
         strokeOpacity: 1.0,
         strokeWeight: fiberType === 'DISTRIBUTION' ? 2 : 4,
       })
-      mapObjects.current = [...mapObjects.current, mapObject]
+      mapObjects.push(mapObject)
     }
     
-  }, [selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired, setFiberRenderRequired, googleMaps, deleteMapObject])
+  }, [selectedSubnetId, subnets, subnetFeatures, fiberRenderRequired, setFiberRenderRequired, googleMaps, deleteMapObjects])
 
-  useEffect(() => {
-    // cleanup
-    return deleteMapObject()
-  }, [])
+  // cleanup
+  useEffect(() => () => deleteMapObjects(), [])
 
-  function deleteMapObject() {
-    if (mapObjects.current.length) {
-      mapObjects.current.forEach(mapObject => mapObject.setMap(null))
-      mapObjects.current = []
+  function deleteMapObjects() {
+    if (mapObjects.length) {
+      mapObjects.forEach(mapObject => {
+        mapObject.setMap(null)
+
+      })
+      mapObjects = []
     }
   }
 
