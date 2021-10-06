21e3b74cf2621837287c2761f8ad8bab04a3c2af
Evan Jones <evan@evanjjones.com>
2021-10-06 12:34:27 -0400

    remove unneeded value in textarea

--- app/public/javascripts/src/react/components/common/forms/TextArea.jsx
+++ app/public/javascripts/src/react/components/common/forms/TextArea.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import cx from 'clsx'
 import './TextArea.css'
 
-export const TextArea = ({ value, placeholder, disabled, classes, onClick=()=>{}, onChange=()=>{} }) => {
+export const TextArea = ({ value, placeholder, disabled, classes, onChange=()=>{} }) => {
 
   return (
     <div className={cx('text-area', classes && classes)}>
       <textarea
         value={value}
-        onClick={event => onClick(event)}
         onChange={event => onChange(event)}
         disabled={disabled}
       >
