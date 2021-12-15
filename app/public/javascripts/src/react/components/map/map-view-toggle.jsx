fa3bfce78edb4953780f39058e770d5a8b74d005
muralimetron <murali@metronlabs.io>
2021-12-15 13:33:18 +0530

    map view toggle react migration changes

---
+++ app/public/javascripts/src/react/components/map/map-view-toggle.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import reduxStore from '../../../redux-store'
+import wrapComponentWithProvider from '../../common/provider-wrapped-component'
+
+// Hold a map of 'mapTypeId' in state.js to the fontawesome icons
+const buttonIcons = {
+  hybrid: 'fa-globe',
+  roadmap: 'fa-road'
+}
+let mapRefPromise = null
+
+export const MapViewToggle = (props) => {
+  const [currentMapType, setCurrentMapType] = useState('roadmap')
+  const [overridenMapType, setOverridenMapType] = useState(null) // Used if the user manually clicks on a map type
+
+  const { mapRef, userPerspective, mapType} = props
+
+  useEffect(() => { ensureMapRefPromiseCreated() }, [])
+
+  useEffect(() => { 
+    setOverridenMapType(null)
+    ensureMapRefPromiseCreated() // In case it has not been created yet
+    updateMapType()
+  }, [userPerspective])
+
+  const ensureMapRefPromiseCreated = () => {
+    if (!mapRefPromise) {
+      mapRefPromise = new Promise((resolve, reject) => {
+        if (!mapRef) {
+          reject('ERROR: You must specify the name of the global variable that contains the map object.')
+        }
+        // We should have a map variable at this point
+        resolve(mapRef)
+      })
+    }
+  }
+
+  const updateMapType = () => {
+    if (overridenMapType) {
+      // The user has overriden the map type. Use it.
+      mapRefPromise
+        .then((mapRef) => mapRef.setMapTypeId(overridenMapType))
+        .catch((err) => console.log(err))
+    } else {
+      // Depending upon the user perspective, set the map type on the map object
+      mapRefPromise
+        .then((result) => {
+          const mapRefResult = result
+          let currentMapTypeState = 'roadmap'
+          setCurrentMapType(currentMapTypeState)
+          if (mapType) {
+            const currentMapTypeState = mapType[userPerspective] || mapType.default
+            setCurrentMapType(currentMapTypeState)
+          }
+          mapRefResult.setMapTypeId(currentMapTypeState)
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+
+  const toggle = () => {
+    const currentMapTypeState = (currentMapType === 'hybrid') ? 'roadmap' : 'hybrid'
+    setCurrentMapType(currentMapTypeState)
+    setOverridenMapType(currentMapTypeState)
+    mapRefPromise
+      .then((mapRef) => mapRef.setMapTypeId(currentMapTypeState))
+      .catch((err) => console.log(err))
+  }
+
+  return (
+    <button className="map-toggle" onClick={() => toggle()}>
+      <i className={`fa ${buttonIcons[currentMapType]}`} />
+    </button>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  mapRef: state.map.googleMaps,
+  mapType: state.toolbar.appConfiguration.mapType,
+  userPerspective: state.user.loggedInUser && state.user.loggedInUser.perspective,
+})
+
+export default wrapComponentWithProvider(reduxStore, MapViewToggle, mapStateToProps, null)
