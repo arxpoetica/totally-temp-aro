6c40ce39771691dde7b387d77d93396124a2abfc
theUtilitariat <theUtilitariat@gmail.com>
2022-01-28 16:06:47 -0500

    clean up comments

--- app/public/javascripts/src/models/state.js
+++ app/public/javascripts/src/models/state.js
@@ -1321,7 +1321,6 @@ class State {
       // Set the logged in user in the Redux store
       service.loadAuthPermissionsRedux()
       service.loadAuthRolesRedux()
-      console.log(user)
       service.setLoggedInUserRedux(user)
       service.loadSystemActorsRedux()
       SocketManager.joinRoom('user', user.id)
