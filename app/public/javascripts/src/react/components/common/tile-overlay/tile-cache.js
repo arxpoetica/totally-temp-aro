7e4b2568894aaa97f099eb185ba07800843d2097
theUtilitariat <theUtilitariat@gmail.com>
2022-05-06 18:53:31 -0400

    notes to self

--- app/public/javascripts/src/react/components/common/tile-overlay/tile-cache.js
+++ app/public/javascripts/src/react/components/common/tile-overlay/tile-cache.js
@@ -18,3 +18,26 @@ function deleteCachedImages (tileIds) {
     }
   })
 }
+
+/*
+this will be a proper class keeping state and functions together
+
+there will be a global cache object that will store caches as such
+tileCache: {
+  unbounded: { // view mode / classic VTS
+    ${serviceAreaId}: imageCache,
+  }
+  bounded: { // plan edit / subnets
+    ${subnetId}: imageCache
+  }
+}
+
+there will be a tile cache culling mechanism that will accept multiple lists and a limit
+upon adding an element to any of the lists the culling mechanism will check size and limit and cull if needed
+it will cull the oldest elements
+it keeps track of age with a linked list
+also when a tile comes back on screen it goes back to the end of the line (becoming "young" again) 
+
+We'll probably do two culling objects one for the unbounded set and one for all of the subnets
+
+*/
