aeeb4fcfcde3ab56814c833e348c320b0980c7f1
theUtilitariat <theUtilitariat@gmail.com>
2022-05-10 19:50:39 -0400

    data and refresh in tile overlay component

--- app/public/javascripts/src/react/components/plan-editor/subnet-tile-overlay.jsx
+++ app/public/javascripts/src/react/components/plan-editor/subnet-tile-overlay.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import reduxStore from '../../../redux-store'
 import wrapComponentWithProvider from '../../common/provider-wrapped-component'
+import PlanEditorSelectors from './plan-editor-selectors'
+// global: tileCache.subnets
 
 const _SubnetTileOverlay = props => {
+  const {
+    mapRef,
+    subnetTileData,
+    rootSubnetId,
+  } = props
   const TILE_SIZE = 256
   // set up google maps getTile and releaseTile functions 
   let overlayLayer = {}
@@ -30,14 +37,26 @@ const _SubnetTileOverlay = props => {
   }
   overlayLayer.redrawCachedTiles = (prop) => {console.log(prop)} // called by the OLD VTS
   
-  props.mapRef.overlayMapTypes.push(overlayLayer)
+  mapRef.overlayMapTypes.push(overlayLayer)
 
   useEffect(() => { return () => onDestroy() }, [])
   const onDestroy = () => {
-    let index = props.mapRef.overlayMapTypes.indexOf(overlayLayer)
+    let index = mapRef.overlayMapTypes.indexOf(overlayLayer)
     console.log(index)
-    props.mapRef.overlayMapTypes.removeAt(index)
-  } 
+    mapRef.overlayMapTypes.removeAt(index)
+  }
+
+  useEffect(() => {
+    // if the data changes the cache has been updated so refresh the map
+    console.log(' --- tile refresh --- ')
+    let index = mapRef.overlayMapTypes.indexOf(overlayLayer)
+    mapRef.overlayMapTypes.removeAt(index)
+    mapRef.overlayMapTypes.push(overlayLayer)
+  }, [
+    subnetTileData,
+    rootSubnetId,
+  ])
+
   // No UI for this component. It deals with map objects only.
   return null
 }
@@ -45,6 +64,9 @@ const _SubnetTileOverlay = props => {
 const mapStateToProps = (state) => {
   return {
     mapRef: state.map.googleMaps,
+    subnetTileData: state.subnetTileData, 
+    //selectedSubnetId: state.planEditor.selectedSubnetId,
+    rootSubnetId: PlanEditorSelectors.getRootSubnetIdForSelected(state),
     // tile data, useEffect: on change tell overlayLayer to run getTile on all visible tiles using clearTileCache
     // tileOverlay.clearTileCache();
   }
