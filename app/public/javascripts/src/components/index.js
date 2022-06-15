0907b2da8b1031a86cfd311a81ed6c69759ba852
muralimetron <murali@metronlabs.io>
2022-06-15 13:40:46 +0530

    removed angular legacy code

--- app/public/javascripts/src/components/index.js
+++ app/public/javascripts/src/components/index.js
@@ -78,8 +78,6 @@ import copper from './views/copper'
 import cables from './views/cables'
 import conduits from './views/conduits'
 import networkPlanModal from './header/network-plan-modal'
-import fullScreenContainer from './full-screen/full-screen-container'
-import aroPanel from './common/aro-panel'
 import aroMultiSelect from './common/aro-multiselect'
 import mapObjectEditor from './common/map-object-editor'
 import contextMenu from './common/context-menu/context-menu'
@@ -114,8 +112,6 @@ app.component('boundaries', boundaries)
   .component('conduits', conduits)
   .component('networkPlanModal', networkPlanModal)
   // .component('uiNotification', uiNotification)
-  .component('fullScreenContainer', fullScreenContainer)
-  .component('aroPanel', aroPanel)
   .component('aroMultiSelect', aroMultiSelect)
   .component('mapObjectEditor', mapObjectEditor)
   .component('contextMenu', contextMenu)
