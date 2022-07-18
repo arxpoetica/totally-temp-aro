6c74acce57dc28fae1c2d37908574e0f94a53850
mb <mb@MBP-BOWLER.local>
2022-07-18 16:53:39 -0400

    Changing the update logic from being conditional to being sequential and conditional
    (cherry picked from commit 7fcd20b015a541e2d728cea2ff5e9a48e0bf45e6)

--- app/public/javascripts/src/react/components/map-tools/panels/network.jsx
+++ app/public/javascripts/src/react/components/map-tools/panels/network.jsx
@@ -168,7 +168,7 @@ const NetworkEquipmentPanel = (props) => {
       opacity: networkEquipment.drawingOptions.opacity || 0.5,
       drawingOptions,
       selectable: true,
-      zIndex: networkEquipment.zIndex + (existingOrPlannedzIndex || 0),
+      zIndex: existingOrPlannedzIndex,
       highlightStyle: networkEquipment.highlightStyle,
       subtypes,
     }
