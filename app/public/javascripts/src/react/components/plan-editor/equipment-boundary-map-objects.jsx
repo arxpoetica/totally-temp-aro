0af0d2e85c8b6db743ef7a121ecca3680a7b3e7f
Robert Hall <arxpoetica@users.noreply.github.com>
2022-11-01 12:03:50 -0600

    Changed in Review w/ Evan

--- app/public/javascripts/src/react/components/plan-editor/equipment-boundary-map-objects.jsx
+++ app/public/javascripts/src/react/components/plan-editor/equipment-boundary-map-objects.jsx
@@ -104,7 +104,7 @@ const EquipmentBoundaryMapObjects = props => {
     mapObject.addListener('contextmenu', event => contextMenuClick(event))
     
     mapObject.addListener('click', event => {
-      if (event.vertex || event.vertex == 0) {
+      if (event.vertex || event.vertex === 0) {
         event.domEvent.stopPropagation()
         if (event.domEvent.shiftKey) {
           multiSelectVertices.addOrRemoveMarker(event)
