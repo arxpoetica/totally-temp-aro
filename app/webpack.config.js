c8dbab67c2b1355763338425c53e069f7c61ce29
Robert Hall <robert.hall@altmansolon.com>
2022-11-06 14:18:21 -0700

    Update Webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,10 +1,15 @@
-const path = require('path')
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 const dev = process.env.NODE_ENV === 'development'
 
-module.exports = {
-  entry: path.resolve(__dirname, './public/javascripts/src/components/index.js'),
+// https://stackoverflow.com/a/62892482/209803
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default {
+  entry: resolve(__dirname, './public/javascripts/src/components/index.js'),
   output: {
-    path: path.resolve(__dirname, 'public/javascripts/lib'),
+    path: resolve(__dirname, 'public/javascripts/lib'),
     filename: 'aro-bundle.js',
   },
   mode: dev ? 'development' : 'production',
@@ -14,22 +19,42 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react', {
-                plugins: ['@babel/plugin-proposal-class-properties']
-              }
-            ]
-          }
-        }
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            { plugins: ['@babel/plugin-proposal-class-properties'] }
+          ],
+        },
+        resolve: {
+          fullySpecified: false,
+          extensions: ['', '.js', '.jsx'],
+        },
       },
+      // {
+      //   test: /\.jsx?$/,
+      //   exclude: /node_modules/,
+      //   loader: 'esbuild-loader',
+      //   options: {
+      //     loader: 'jsx',  // Remove this if you're not using JSX
+      //     target: 'es2015'  // Syntax to compile to (see options below for possible values)
+      //   },
+      //   resolve: {
+      //     fullySpecified: false,
+      //     extensions: ['', '.js', '.jsx'],
+      //   },
+      // },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+        ],
+      },
+    ],
+  },
 }
