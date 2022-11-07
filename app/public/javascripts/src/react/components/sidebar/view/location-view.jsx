0bd95d629d48a610fcc83d79806787e6ab7c39e5
theUtilitariat <theUtilitariat@gmail.com>
2022-11-07 18:14:13 -0500

    nearnet right panel contents

--- app/public/javascripts/src/react/components/sidebar/view/location-view.jsx
+++ app/public/javascripts/src/react/components/sidebar/view/location-view.jsx
@@ -7,11 +7,33 @@ const _LocationView = props => {
 
   //useEffect(() => {})
 
+  // FUTURE: replace this with a generic object viewer (I've built that like three times haven't I?)
+  // iterate object props build rows
+  let rows = []
+  let entity = {}
+  if (props.nearnetEntities.length) entity = props.nearnetEntities[0]
+  console.log(entity)
+  for (const [key, value] of Object.entries(entity)) {
+    rows.push(
+      <div className="ei-property-item" key={key}>
+        <div className="ei-property-label" style={{fontWeight: 'bold'}}>{key}</div>
+        <div className="ei-property-value" style={{textAlign: 'right'}}>
+          {""+value}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <pre>
-        {JSON.stringify(props.nearnetEntities, undefined, 2)}
-      </pre>
+    <div className="ei-items-contain object-editor" style={{paddingRight: '16px'}}>
+      <div className="ei-header ei-no-pointer ei-header-with-icon">
+        Title Here
+      </div>
+      <div className="ei-gen-level ei-internal-level">
+        <div className="ei-items-contain">
+          {rows}
+        </div>
+      </div>
     </div>
   )
 }
