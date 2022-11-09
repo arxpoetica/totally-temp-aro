2770337e8f858025823178473423c848ffc1534c
Evan Jones <evan@evanjjones.com>
2022-11-09 12:40:29 -0500

    format

--- app/public/javascripts/src/react/components/acl/resource-permissions/permissions-table.jsx
+++ app/public/javascripts/src/react/components/acl/resource-permissions/permissions-table.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux'
 import aclActions from '../acl-actions.js'
 import { Select, Group, Button } from '@mantine/core'
 
-const sortableColumns = { 'NAME': 'name', 'PERMISSIONS': 'permissions' }
+const sortableColumns = { NAME: 'name', PERMISSIONS: 'permissions' }
 
 export const PermissionsTable = (props) => {
-  const [selectedColumn, setSelectedColumn] = useState(sortableColumns.PERMISSIONS)
+  const [selectedColumn, setSelectedColumn] = useState(
+    sortableColumns.PERMISSIONS,
+  )
   const [sortedRows, setSortedRows] = useState([])
   const [isOrderDesc, setIsOrderDesc] = useState(false)
   const [selectedActorId, setSelecedActorId] = useState('')
@@ -22,7 +24,7 @@ export const PermissionsTable = (props) => {
   useEffect(() => {
     const createRows = () => {
       const rows = props.acl.slice(0)
-      
+
       rows.sort((a, b) => {
         var aVal = ''
         var bVal = ''
@@ -30,8 +32,12 @@ export const PermissionsTable = (props) => {
           aVal = getActorNameById(a.systemActorId)
           bVal = getActorNameById(b.systemActorId)
         } else if (selectedColumn === sortableColumns.PERMISSIONS) {
-          aVal = props.authRolesByPermission[a.rolePermissions] ? props.authRolesByPermission[a.rolePermissions].displayName : ''
-          bVal = props.authRolesByPermission[b.rolePermissions] ? props.authRolesByPermission[b.rolePermissions].displayName : ''
+          aVal = props.authRolesByPermission[a.rolePermissions]
+            ? props.authRolesByPermission[a.rolePermissions].displayName
+            : ''
+          bVal = props.authRolesByPermission[b.rolePermissions]
+            ? props.authRolesByPermission[b.rolePermissions].displayName
+            : ''
         }
         if (isOrderDesc) {
           var holder = aVal
@@ -50,23 +56,27 @@ export const PermissionsTable = (props) => {
   // creates format to be used in select for adding
   useEffect(() => {
     const formatSystemActors = () => {
-      const newList = Object.values(props.systemActors).filter((actor) => {
-        const index = props.acl.findIndex((permission) => permission.systemActorId === actor.id)
-        return index === -1 ? true : false
-      }).map((actor) => {
-        const newActor = {}
-        newActor.value = String(actor.id); //convert to string for mantine
-        newActor.group = actor.type
-        if (actor.type === 'group') {
-          newActor.label = actor.name
-          
+      const newList = Object.values(props.systemActors)
+        .filter((actor) => {
+          const index = props.acl.findIndex(
+            (permission) => permission.systemActorId === actor.id,
+          )
+          return index === -1 ? true : false
+        })
+        .map((actor) => {
+          const newActor = {}
+          newActor.value = String(actor.id) //convert to string for mantine
+          newActor.group = actor.type
+          if (actor.type === 'group') {
+            newActor.label = actor.name
+
+            return newActor
+          }
+          newActor.label = actor.firstName + ' ' + actor.lastName
+
           return newActor
-        }
-        newActor.label = actor.firstName + ' ' + actor.lastName
-        
-        return newActor
-      })
-  
+        })
+
       setActorList(newList)
     }
 
@@ -81,42 +91,63 @@ export const PermissionsTable = (props) => {
     return jsx
   }
 
-  const renderDataRow =(dataItem) => {
+  const renderDataRow = (dataItem) => {
     const systemActor = props.systemActors[dataItem.systemActorId]
     if (!systemActor) return
-    if (!systemActor.hasOwnProperty('name')) systemActor.name = `${systemActor.firstName} ${systemActor.lastName}`
-    return <tr key={dataItem.systemActorId}>
-      <td>
-        {systemActor.name}
-      </td>
-      <td>
-        {props.isOwner
-          ? (
-            <select className='form-control' onChange={event => { onSelectRoll(event, dataItem.systemActorId) }} value={dataItem.rolePermissions}>
+    if (!systemActor.hasOwnProperty('name'))
+      systemActor.name = `${systemActor.firstName} ${systemActor.lastName}`
+    return (
+      <tr key={dataItem.systemActorId}>
+        <td>{systemActor.name}</td>
+        <td>
+          {props.isOwner ? (
+            <select
+              className="form-control"
+              onChange={(event) => {
+                onSelectRoll(event, dataItem.systemActorId)
+              }}
+              value={dataItem.rolePermissions}
+            >
               {Object.values(props.filteredAuthRoles).map((role) => (
-                <option key={`data-item-${dataItem.systemActorId}-dropdown-option-${role.id}`} value={role.permissions}>{role.displayName}</option>
+                <option
+                  key={`data-item-${dataItem.systemActorId}-dropdown-option-${role.id}`}
+                  value={role.permissions}
+                >
+                  {role.displayName}
+                </option>
               ))}
             </select>
-          )
-          : Object.values(props.filteredAuthRoles).filter(role => role.permissions === dataItem.rolePermissions)[0].displayName
-        }
-      </td>
-      <td className='ei-table-cell ei-table-button-cell'>
-        <button className='btn btn-sm btn-outline-danger'
-          type='button'
-          onClick={event => { deleteAuthItem(event, dataItem.systemActorId) }}
-          data-toggle='tooltip' data-placement='bottom' title='Delete'
-          disabled={(props.isOwner ? null : 'disabled')}>
-          <i className='fa ei-button-icon fa-trash-alt' />
-        </button>
-      </td>
-    </tr>
+          ) : (
+            Object.values(props.filteredAuthRoles).filter(
+              (role) => role.permissions === dataItem.rolePermissions,
+            )[0].displayName
+          )}
+        </td>
+        <td className="ei-table-cell ei-table-button-cell">
+          <button
+            className="btn btn-sm btn-outline-danger"
+            type="button"
+            onClick={(event) => {
+              deleteAuthItem(event, dataItem.systemActorId)
+            }}
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title="Delete"
+            disabled={props.isOwner ? null : 'disabled'}
+          >
+            <i className="fa ei-button-icon fa-trash-alt" />
+          </button>
+        </td>
+      </tr>
+    )
   }
 
   const getActorNameById = (id) => {
     const systemActor = props.systemActors[id]
     if (!systemActor) return ''
-    return systemActor.name || `${systemActor.firstName} ${systemActor.lastName}`
+    return (
+      systemActor.name || `${systemActor.firstName} ${systemActor.lastName}`
+    )
   }
   // --- //
 
@@ -139,59 +170,96 @@ export const PermissionsTable = (props) => {
 
   const addAuthItem = (systemActorId) => {
     var permissionsBit = props.authRoles['RESOURCE_VIEWER'].permissions
-    props.setUserAcl(props.resource.identifier, Number(systemActorId), permissionsBit)
+    props.setUserAcl(
+      props.resource.identifier,
+      Number(systemActorId),
+      permissionsBit,
+    )
   }
-
-
 
   return (
     <>
-      <table className='table table-sm ei-table-striped' style={{ 'borderBottom': '1px solid #dee2e6' }}>
+      <table
+        className="table table-sm ei-table-striped"
+        style={{ borderBottom: '1px solid #dee2e6' }}
+      >
         <thead>
           <tr>
-            <th className='ei-table-col-head-sortable ng-binding ng-scope' onClick={event => { onSortClick(sortableColumns.NAME) }} style={{'cursor': 'pointer'}}>
+            <th
+              className="ei-table-col-head-sortable ng-binding ng-scope"
+              onClick={(event) => {
+                onSortClick(sortableColumns.NAME)
+              }}
+              style={{ cursor: 'pointer' }}
+            >
               Name
-              {selectedColumn === sortableColumns.NAME
-                ? <div className='ei-table-col-sort-icon ng-scope'>
-                    <i className={'fa' + (isOrderDesc ? ' fa-chevron-up' : ' fa-chevron-down')} aria-hidden='true'> </i>
-                  </div>
-                : ''
-              }
+              {selectedColumn === sortableColumns.NAME ? (
+                <div className="ei-table-col-sort-icon ng-scope">
+                  <i
+                    className={
+                      'fa' +
+                      (isOrderDesc ? ' fa-chevron-up' : ' fa-chevron-down')
+                    }
+                    aria-hidden="true"
+                  >
+                    {' '}
+                  </i>
+                </div>
+              ) : (
+                ''
+              )}
             </th>
-            <th className='ei-table-col-head-sortable ng-binding ng-scope' onClick={event => { onSortClick(sortableColumns.PERMISSIONS) }} style={{'cursor': 'pointer'}}>
+            <th
+              className="ei-table-col-head-sortable ng-binding ng-scope"
+              onClick={(event) => {
+                onSortClick(sortableColumns.PERMISSIONS)
+              }}
+              style={{ cursor: 'pointer' }}
+            >
               Role Permissions
-              {selectedColumn === sortableColumns.PERMISSIONS
-                ? <div className='ei-table-col-sort-icon ng-scope'>
-                    <i className={'fa' + (isOrderDesc ? ' fa-chevron-up' : ' fa-chevron-down')} aria-hidden='true'> </i>
-                  </div>
-                : ''
-              }
+              {selectedColumn === sortableColumns.PERMISSIONS ? (
+                <div className="ei-table-col-sort-icon ng-scope">
+                  <i
+                    className={
+                      'fa' +
+                      (isOrderDesc ? ' fa-chevron-up' : ' fa-chevron-down')
+                    }
+                    aria-hidden="true"
+                  >
+                    {' '}
+                  </i>
+                </div>
+              ) : (
+                ''
+              )}
             </th>
             <th />
           </tr>
         </thead>
-        <tbody>
-          {renderDataRows()}
-        </tbody>
+        <tbody>{renderDataRows()}</tbody>
       </table>
       <div>
-        {props.isOwner
-          ? <Group>
-              <Select
-                data={actorList}
-                value={selectedActorId}
-                onChange={(value) => setSelecedActorId(value)}
-                searchable
-                nothingFound="No Matches"
-                clearable
-              />
-              <Button type='button' onClick={() => {
+        {props.isOwner ? (
+          <Group>
+            <Select
+              data={actorList}
+              value={selectedActorId}
+              onChange={(value) => setSelecedActorId(value)}
+              searchable
+              nothingFound="No Matches"
+              clearable
+            />
+            <Button
+              type="button"
+              onClick={() => {
                 addAuthItem(selectedActorId)
                 setSelecedActorId('')
-              }}>Add</Button> 
-            </Group>
-          : null
-        }
+              }}
+            >
+              Add
+            </Button>
+          </Group>
+        ) : null}
       </div>
     </>
   )
@@ -202,16 +270,22 @@ export const PermissionsTable = (props) => {
 const mapStateToProps = (state, ownProps) => {
   var acl = []
   // acl may not be loaded
-  if (state.acl.aclByType.hasOwnProperty(ownProps.resourceType) &&
-    state.acl.aclByType[ownProps.resourceType].hasOwnProperty(ownProps.resource.identifier)) {
-    acl = state.acl.aclByType[ownProps.resourceType][ownProps.resource.identifier]
+  if (
+    state.acl.aclByType.hasOwnProperty(ownProps.resourceType) &&
+    state.acl.aclByType[ownProps.resourceType].hasOwnProperty(
+      ownProps.resource.identifier,
+    )
+  ) {
+    acl =
+      state.acl.aclByType[ownProps.resourceType][ownProps.resource.identifier]
   }
   var filteredAuthRoles = []
   var authRolesByPermission = {}
-  Object.keys(state.user.authRoles).forEach(key => {
+  Object.keys(state.user.authRoles).forEach((key) => {
     if (key.slice(0, 9) === 'RESOURCE_') {
       filteredAuthRoles.push(state.user.authRoles[key])
-      authRolesByPermission[state.user.authRoles[key].permissions] = state.user.authRoles[key]
+      authRolesByPermission[state.user.authRoles[key].permissions] =
+        state.user.authRoles[key]
     }
   })
   return {
@@ -219,14 +293,28 @@ const mapStateToProps = (state, ownProps) => {
     systemActors: state.user.systemActors,
     authRoles: state.user.authRoles,
     filteredAuthRoles: filteredAuthRoles,
-    authRolesByPermission: authRolesByPermission
+    authRolesByPermission: authRolesByPermission,
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  getAcl: (resourceId, doForceUpdate = false) => dispatch(aclActions.getAcl(ownProps.resourceType, resourceId, doForceUpdate)),
-  setUserAcl: (resourceId, userId, permissionsBit) => dispatch(aclActions.setUserAcl(ownProps.resourceType, resourceId, userId, permissionsBit)),
-  deleteUserAcl: (resourceId, userId) => dispatch(aclActions.deleteUserAcl(ownProps.resourceType, resourceId, userId))
+  getAcl: (resourceId, doForceUpdate = false) =>
+    dispatch(
+      aclActions.getAcl(ownProps.resourceType, resourceId, doForceUpdate),
+    ),
+  setUserAcl: (resourceId, userId, permissionsBit) =>
+    dispatch(
+      aclActions.setUserAcl(
+        ownProps.resourceType,
+        resourceId,
+        userId,
+        permissionsBit,
+      ),
+    ),
+  deleteUserAcl: (resourceId, userId) =>
+    dispatch(
+      aclActions.deleteUserAcl(ownProps.resourceType, resourceId, userId),
+    ),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PermissionsTable)
